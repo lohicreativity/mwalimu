cf0e35aa36f5406630b0b818de4a616b0f83a78e
Amani Ghachocha <scholardreamtanzania@gmail.com>
2021-11-24 18:41:49 +0300

    Added elective deadlines

--- resources/views/dashboard/student/modules.blade.php
+++ resources/views/dashboard/student/modules.blade.php
@@ -79,6 +79,8 @@
                            @if(!App\Utils\Util::collectionContains($options,$assignment))
                             @if(count($semester->electiveDeadlines) != 0) @if(strtotime(now()->format('Y-m-d')) > strtotime($semester->electiveDeadlines[0]->deadline))
                             <a href="{{ url('student/module/'.$assignment->id.'/opt') }}" class="btn btn-primary"><i class="fa fa-check-circle"></i> Opt</a>
+                            @else
+                            <p class="ss-no-margin">OPTIONAL</p>
                             @endif
                             @endif
                           @else
