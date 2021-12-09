262449f2082a8c8448cc7a34a39cc428241e3420
Amani Ghachocha <scholardreamtanzania@gmail.com>
2021-12-09 14:39:50 +0300

    Refined exam processing

--- resources/views/dashboard/academic/staff-assigned-modules.blade.php
+++ resources/views/dashboard/academic/staff-assigned-modules.blade.php
@@ -51,7 +51,7 @@
                     <select name="study_academic_year_id" class="form-control" required>
                        <option value="">Select Study Academic Year</option>
                        @foreach($study_academic_years as $year)
-                       <option value="{{ $year->id }}">{{ $year->academicYear->year }}</option>
+                       <option value="{{ $year->id }}" @if($year->status == 'ACTIVE') selected="selected" @endif>{{ $year->academicYear->year }}</option>
                        @endforeach
                     </select>
                      
