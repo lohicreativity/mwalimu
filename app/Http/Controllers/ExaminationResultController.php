5c21c957616dcdd2f4153c1e9be0bb5297d22de6
Amani Ghachocha <scholardreamtanzania@gmail.com>
2021-12-15 02:24:51 +0300

    Added ACL

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -78,7 +78,6 @@ class ExaminationResultController extends Controller
     	         })->whereHas('programModuleAssignment.campusProgram',function($query) use($campus_program){
 	    	    	$query->where('program_id',$campus_program->program->id);
 	    	    })->with('module.ntaLevel','programModuleAssignment.campusProgram.program','studyAcademicYear')->where('study_academic_year_id',$request->get('study_academic_year_id'))->get();
-       return $module_assignments;
     	 $annual_module_assignments = $module_assignments;
 
     	if($request->get('semester_id') != 'SUPPLEMENTARY'){
@@ -91,6 +90,7 @@ class ExaminationResultController extends Controller
     	        })->with('module.ntaLevel','programModuleAssignment.campusProgram.program','studyAcademicYear')->where('study_academic_year_id',$request->get('study_academic_year_id'))->get();
          }
         	
+        return $module_assignments;
       if(count($module_assignments) == 0){
           DB::rollback();
           return redirect()->back()->with('error','No results to process');
