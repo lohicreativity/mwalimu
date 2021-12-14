79df926d1f259445d3d09ea04b39ba6949dae754
Amani Ghachocha <scholardreamtanzania@gmail.com>
2021-12-14 23:11:35 +0300

    Added ACL

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -677,7 +677,7 @@ class ModuleAssignmentController extends Controller
               fclose($file_handle);
               foreach($line_of_text as $ln){
                    $line = explode(',', $ln);
-                   return dd($line[1]);
+                   return dd(trim($line[1]));
                    if(floatval(str_replace(' ', '', $line[1])) < 0 || floatval(str_replace(' ', '', $line[1])) > 100){
                      $validationStatus = false;
                    }
