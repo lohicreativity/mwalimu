831806a2949d7123c38f5ab03a1d856be683a65e
Amani Ghachocha <scholardreamtanzania@gmail.com>
2021-12-15 06:24:24 +0300

    Added ACL

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -983,7 +983,7 @@ class ExaminationResultController extends Controller
         }
         $grading_policies = GradingPolicy::where('nta_level_id',$campus_program->program->nta_level_id)->where('study_academic_year_id',$request->get('study_academic_year_id'))->orderBy('grade')->get();
 
-        return $campus_program;
+        return $request->get('study_academic_year_id');
 
         $modules = [];
 
