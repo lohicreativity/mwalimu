1c4f2969bcbd1f507a44a29c9384de37c69fda43
Amani Ghachocha <scholardreamtanzania@gmail.com>
2021-12-27 17:37:17 +0300

    Added graduants

--- app/Http/Controllers/ProgramModuleAssignmentController.php
+++ app/Http/Controllers/ProgramModuleAssignmentController.php
@@ -26,9 +26,7 @@ class ProgramModuleAssignmentController extends Controller
            'study_academic_years'=>StudyAcademicYear::with('academicYear')->get(),
            'study_academic_year'=>$request->has('study_academic_year_id')? StudyAcademicYear::with('academicYear')->find($request->get('study_academic_year_id')) : null,
            'campuses'=>Campus::all(),
-           'campus'=>Campus::with(['campusPrograms.program'=>function($query) use ($staff){
-               $query->where('department_id',$staff->department_id);
-           },'campusPrograms.programModuleAssignments.module'])->find($request->get('campus_id')),
+           'campus'=>Campus::with(['campusPrograms.program','campusPrograms.programModuleAssignments.module'])->find($request->get('campus_id')),
            'staff'=>$staff,
            'request'=>$request
     	];
