4a1dbdae98f07d01e09147dfce12eb49018efb01
Amani Ghachocha <scholardreamtanzania@gmail.com>
2021-12-27 14:29:57 +0300

    Added graduants

--- resources/views/dashboard/academic/non-graduants-list.blade.php
+++ resources/views/dashboard/academic/non-graduants-list.blade.php
@@ -49,7 +49,7 @@
               </div>
               <!-- /.card-header -->
               <div class="card-body">
-                 {!! Form::open(['url'=>'academic/examination-policies','class'=>'ss-form-processing','method'=>'GET']) !!}
+                 {!! Form::open(['url'=>'academic/excluded-graduants','class'=>'ss-form-processing','method'=>'GET']) !!}
                    
                    <div class="form-group">
                     <select name="study_academic_year_id" class="form-control" required>
