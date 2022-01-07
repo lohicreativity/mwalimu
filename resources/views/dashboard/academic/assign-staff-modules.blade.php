096933dffd7d0189aeabd9f2a18f237cdd1bfebd
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-01-07 16:06:09 +0300

    Added assessment plan delete message

--- resources/views/dashboard/academic/assign-staff-modules.blade.php
+++ resources/views/dashboard/academic/assign-staff-modules.blade.php
@@ -233,15 +233,11 @@
                            {!! Form::input('hidden','campus_program_id',$assign->campus_program_id) !!}
 
                          @can('request-module-facilitator')
-                         @if(count($assign->programModuleAssignmentRequests) != 0)
-                            <i>Facilitator Requested</i>
-                         @else
                          <button type="submit" class="btn btn-info btn-sm" href="">
                               <i class="fas fa-plus">
                               </i>
                               Request Facilitator
                          </button>
-                         @endif
                          @endcan
                          {!! Form::close() !!}
                          @endif
