d752833185ea6ed99ecd090713ce8d39046c1567
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-01-21 16:53:13 +0300

    Reprocessed results

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -766,10 +766,10 @@ class ModuleAssignmentController extends Controller
                       $invalid_students[] = $up_stud;
                    }
                 }
-                if(count($invalid_students) != 0){
-                    session()->flash('invalid_students',$invalid_students);
-                    return redirect()->back()->with('error','Uploaded students do not exists');
-                }
+                // if(count($invalid_students) != 0){
+                //     session()->flash('invalid_students',$invalid_students);
+                //     return redirect()->back()->with('error','Uploaded students do not exists');
+                // }
               }
 
               
