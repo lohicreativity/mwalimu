f50ccfe40a452000ed51cb0da5a3db26968db5d6
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-01-23 12:38:11 +0300

    Comments

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -77,7 +77,6 @@ class LevelController extends Controller
     {
         try{
             $level = Level::with('awards')->findOrFail($id);
-            return $level->awards;
             if(count($level->awards) != 0){
                return redirect()->back()->with('message','Level cannot be deleted because it has awards');
             }
