6a305eb3b197cc90d67d16aad26a2a64d8b06292
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-01-23 22:12:28 +0300

    Comments

--- app/Http/Controllers/ModuleAssignmentRequestController.php
+++ app/Http/Controllers/ModuleAssignmentRequestController.php
@@ -25,6 +25,7 @@ class ModuleAssignmentRequestController extends Controller
            'staffs'=>Staff::with(['campus'])->where('department_id',$staff->department_id)->get(),
            'staff'=>$staff
     	];
+        return $data['requests'];
     	return view('dashboard.academic.module-assignment-requests',$data)->withTitle('Module Assignments Requests');
     }
 
