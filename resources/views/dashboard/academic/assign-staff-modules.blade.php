d63a0fd663e900234dd2c3f22001e606d26ee8a0
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-01-23 17:51:14 +0300

    Comments

--- resources/views/dashboard/academic/assign-staff-modules.blade.php
+++ resources/views/dashboard/academic/assign-staff-modules.blade.php
@@ -264,7 +264,7 @@
                                         <option value="">Select Facilitator</option>
                                         @foreach($staffs as $stf)
                                         @if($stf->department_id == $staff->department_id)
-                                        <option value="{{ $stf->id }}">{{ $stf->title }} {{ $stf->first_name }} {{ $stf->surname }} - {{ $stf->designation->name }} ($stf->campus->name)</option>
+                                        <option value="{{ $stf->id }}">{{ $stf->title }} {{ $stf->first_name }} {{ $stf->surname }} - {{ $stf->designation->name }} ({{ $stf->campus->name }})</option>
                                         @endif
                                         @endforeach
                                       </select>
