73ec11b4c5a9dee6a7aaf2c18267bb2529e4bf5b
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-01-24 19:23:09 +0300

    Comments

--- resources/views/dashboard/academic/reports/final-program-results-second-semester.blade.php
+++ resources/views/dashboard/academic/reports/final-program-results-second-semester.blade.php
@@ -273,16 +273,13 @@
 
                       @foreach($module_assignments as $assignment)
 
-                          
+                          @php
+                            $results_present = false;
+                          @endphp
 
                       
                           @foreach($student->examinationResults as $result)
-
-                            @php
-                            $results_present = false;
-                            @endphp
-                            
-                            @if($result->module_assignment_id == $assignment->id)
+                            @if($result->module_assignment_id == $assignment->id)  
 
                             @php
                               $results_present = true;
