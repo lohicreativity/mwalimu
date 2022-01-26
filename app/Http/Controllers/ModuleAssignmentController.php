c9a63696a3b4322a3a50ac9d088917207ff3a1bd
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-01-26 12:01:05 +0300

    Comments

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -251,7 +251,7 @@ class ModuleAssignmentController extends Controller
 
              $students_with_supplemetary_count = ExaminationResult::where('module_assignment_id',$module_assignment->id)->whereNotNull('supp_score')->count();
 
-             $students_passed_count = $students_with_supplemetary_count = ExaminationResult::where('module_assignment_id',$module_assignment->id)->where('final_remark','!=','FAIL')->where('exam_type','FINAL')->count();
+             $students_passed_count = ExaminationResult::where('module_assignment_id',$module_assignment->id)->where('final_remark','!=','FAIL')->where('exam_type','FINAL')->count();
              $supp_cases_count = ExaminationResult::where('module_assignment_id',$module_assignment->id)->whereNotNull('final_uploaded_at')->where('final_exam_remark','FAIL')->count();
              $students_with_no_supplementary_count = $students_with_final_marks_count - $students_with_supplemetary_count;
              $students_with_abscond_count = ExaminationResult::where('module_assignment_id',$module_assignment->id)->where('final_uploaded_at','!=',null)->where('course_work_remark','INCOMPLETE')->orWhere('final_remark','INCOMPLETE')->count();
