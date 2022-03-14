f60daaa69535f16b0b09c5280294115406569585
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-03-14 11:26:55 +0300

    Comments

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -219,6 +219,8 @@ class StudentController extends Controller
          	 $query->where('study_academic_year_id',$ac_yr_id)->where('year_of_study',$yr_of_study);
          },'moduleAssignment.module'])->where('student_id',$student->id)->get();
 
+         return $results;
+
          $core_programs = ProgramModuleAssignment::with(['module'])->where('study_academic_year_id',$ac_yr_id)->where('year_of_study',$yr_of_study)->where('category','COMPULSORY')->where('campus_program_id',$student->campus_program_id)->get();
          $optional_programs = ProgramModuleAssignment::whereHas('students',function($query) use($student){
          	   $query->where('id',$student->id);
