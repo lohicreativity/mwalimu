b5dc12cf1696d98afe927e3a70dc365c0d4499d1
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-03-17 11:51:53 +0300

    Comments

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -1958,8 +1958,6 @@ class ExaminationResultController extends Controller
             }
         }
 
-        return dd($report);
-
         $data = [
             'report'=>$report,
             'study_academic_year'=>StudyAcademicYear::with('academicYear')->find($request->get('study_academic_year_id')),
