01f29f25b872aa01f572dc6a4a1e03b70588e88f
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-03-23 11:10:01 +0300

    Comments

--- app/Exports/GraduantsExport.php
+++ app/Exports/GraduantsExport.php
@@ -4,7 +4,7 @@ namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
-use App\Domain\Academic\Models\Program;
+use App\Domain\Academic\Models\CampusProgram;
 use App\Exports\Sheets\GraduantsPerProgramSheet;
 
 class GraduantsExport implements WithMultipleSheets
