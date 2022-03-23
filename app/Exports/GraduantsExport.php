080e04dcdc559961cd305039d20d6a861b6ea26d
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-03-23 10:13:45 +0300

    Comments

--- app/Exports/GraduantsExport.php
+++ app/Exports/GraduantsExport.php
@@ -4,10 +4,11 @@ namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+use Maatwebsite\Excel\Concerns\WithMapping;
 use App\Domain\Academic\Models\Program;
 use App\Exports\Sheets\GraduantsPerProgramSheet;
 
-class GraduantsExport implements WithMultipleSheets
+class GraduantsExport implements WithMultipleSheets, WithMapping
 {
     use Exportable;
 
@@ -32,5 +33,14 @@ class GraduantsExport implements WithMultipleSheets
         }
 
         return $sheets;
+    }
+
+    public function map($graduant): array
+    {
+        return [
+            $graduant->student->first_name.' '.$graduant->student->middle_name.' '.$graduant->student->surname,
+            $graduant->student->gender,
+            $graduant->student->registration_number,
+        ];
     }
 }
