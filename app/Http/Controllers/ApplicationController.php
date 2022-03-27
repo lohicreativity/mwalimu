0421e2e66175e8674b6e4a3c901e8dcd959138d1
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-03-27 11:59:11 +0300

    Comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -402,7 +402,7 @@ class ApplicationController extends Controller
             $query->where('application_window_id',$request->get('application_window_id'));
         })->get();
 
-        $o_level_grades = ['A'=>5,'B+'=>4,'B'=>3,'C'=>2,'D'=>1,'F'=>0];
+        $o_level_grades = ['A'=>5,'B+'=>4,'B'=>3,'C'=>2,'D'=>1,'E'=>0.5,'F'=>0];
 
         $diploma_grades = ['A'=>5,'B+'=>4,'B'=>3,'C'=>2,'D'=>1,'F'=>0];
         
