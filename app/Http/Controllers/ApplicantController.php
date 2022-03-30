41f95d6bf83e28d4a8364007ab3a8a1cd611eba6
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-03-30 17:11:06 +0300

    Comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -328,7 +328,7 @@ class ApplicantController extends Controller
             'first_name'=>'required',
             'surname'=>'required',
             'birth_date'=>'required',
-            'phone'=>'required|min:12|max:12',
+            'phone'=>'required|min:12|max:12|numeric',
             'address'=>'required',
             'nationality'=>'required',
             'street'=>'required'
