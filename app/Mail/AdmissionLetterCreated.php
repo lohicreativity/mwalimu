7003fd8111ff451961ca781f085cbe1510d870d4
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-04-04 10:42:11 +0300

    Comments

--- app/Mail/AdmissionLetterCreated.php
+++ app/Mail/AdmissionLetterCreated.php
@@ -39,7 +39,7 @@ class AdmissionLetterCreated extends Mailable
     public function build()
     {
         // $file_name = 'Admission-Letter.pdf';
-        $this->pdf->save('/uploads/Admission-Letter-'.$this->applicant->first_name.'-'.$this->applicant->surname.'.pdf');
+        $this->pdf->save(public_path().'/uploads/Admission-Letter-'.$this->applicant->first_name.'-'.$this->applicant->surname.'.pdf');
         // file_put_contents($file_name, $this->pdf->output());
         $file_name = public_path().'/uploads/Admission-Letter-'.$this->applicant->first_name.'-'.$this->applicant->surname.'.pdf';
         $attachments = AdmissionAttachment::all();
