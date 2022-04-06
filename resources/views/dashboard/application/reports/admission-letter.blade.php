9be667ce05817439586cf64ef763e7dee13ac337
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-04-06 16:37:55 +0300

    Comments

--- resources/views/dashboard/application/reports/admission-letter.blade.php
+++ resources/views/dashboard/application/reports/admission-letter.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-  <title>{{ Config::get('constants.SITE_NAME') }}</title>
+  <title></title>
   <style type="text/css">
       
       body{
