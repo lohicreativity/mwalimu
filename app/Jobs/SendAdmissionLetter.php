fb10e4e720f2de24b1c203daab6d8d1d418f3376
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-04-15 14:15:41 +0300

    Comments

--- app/Jobs/SendAdmissionLetter.php
+++ app/Jobs/SendAdmissionLetter.php
@@ -64,14 +64,14 @@ class SendAdmissionLetter implements ShouldQueue
 
         foreach($applicants as $applicant){
            // try{
-               // $ac_year = date('Y',strtotime($applicant->applicationWindow->end_date));
-               // $ac_year += 1;
-               // $study_academic_year = StudyAcademicYear::whereHas('academicYear',function($query) use($ac_year){
-               //        $query->where('year','LIKE','%'.$ac_year.'%');
-               //  })->first();
-               // if(!$study_academic_year){
-               //     return redirect()->back()->with('error','Admission study academic year not created');
-               // }
+               $ac_year = date('Y',strtotime($applicant->applicationWindow->end_date));
+               $ac_year += 1;
+               $study_academic_year = StudyAcademicYear::whereHas('academicYear',function($query) use($ac_year){
+                      $query->where('year','LIKE','%'.$ac_year.'%');
+                })->first();
+               if(!$study_academic_year){
+                   return redirect()->back()->with('error','Admission study academic year not created');
+               }
 
                // $program_fee = ProgramFee::where('study_academic_year_id',$study_academic_year->id)->where('campus_program_id',4)->first();
 
