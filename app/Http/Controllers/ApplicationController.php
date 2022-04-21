275dbeb4829d87cf24a140e6b0d99f25bd53d546
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-04-21 11:01:08 +0300

    Comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -310,7 +310,9 @@ class ApplicationController extends Controller
                  $query->where('id',$request->get('application_window_id'));
             })->with(['nextOfKin.region','region','district','intake','selections.campusProgram.program','nectaResultDetails'])->where('program_level_id',$request->get('program_level_id'))->where('campus_id',$staff->campus_id)->get();
 
-        if(ApplicantProgramSelection::where('application_window_id',$request->get('application_window_id'))->where('status','APPROVING')->count() == 0){
+        if(ApplicantProgramSelection::whereHas('applicant',function($query) use($request,$staff){
+             $query->where('campus_id',$staff->campus_id)->where('program_level_id',$request->get('program_level_id'));
+        })->where('application_window_id',$request->get('application_window_id'))->where('status','APPROVING')->count() == 0){
              return redirect()->back()->with('error','Applicants selection has not been run yet');
         }
 
