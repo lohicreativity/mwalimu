8553567c22726089fe51fd8fc24ee4239081e35a
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-04-25 22:27:56 +0300

    Comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -1372,9 +1372,9 @@ class ApplicationController extends Controller
             })->with(['intake','selections.campusProgram.program'])->where('campus_id',$staff->campus_id)->where(function($query) use($request){
                    $query->where('first_name','LIKE','%'.$request->get('query').'%')->orWhere('middle_name','LIKE','%'.$request->get('query').'%')->orWhere('surname','LIKE','%'.$request->get('query').'%');
                  })->where('application_window_id',$application_window->id)->where(function($query){
-                     $query->where('confirmation_status','!=','CANCELLED')->orWhere('confirmation_status','!=','TRANSFERED');
+                     $query->where('confirmation_status','!==','CANCELLED')->orWhere('confirmation_status','!==','TRANSFERED')->orWhereNull('confirmation_status');
                    })->where(function($query){
-                     $query->where('admission_confirmation_status','!=','NOT CONFIRMED')->orWhereNull('admission_confirmation_status');
+                     $query->where('admission_confirmation_status','!==','NOT CONFIRMED')->orWhereNull('admission_confirmation_status');
                    })->where('status','ADMITTED')->get();
               if(count($applicants) == 0){
                   return redirect()->back()->with('error','No applicant with searched name');
