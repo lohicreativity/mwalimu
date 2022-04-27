34641a06c625a39db50c2c593d6c5df8d0a8ce36
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-04-27 15:36:07 +0300

    Comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -1006,7 +1006,7 @@ class ApplicationController extends Controller
 
         $applicants = Applicant::whereHas('selections',function($query) use($request){
             $query->where('application_window_id',$request->get('application_window_id'));
-        })->with(['selections','nectaResultDetails.results','nacteResultDetails.results'])->where('program_level_id',$request->get('award_id'))->get();
+        })->with(['selections','nectaResultDetails.results','nacteResultDetails.results'])->where('program_level_id',$request->get('award_id'))->whereNull('is_tamisemi')->get();
 
 
         $o_level_grades = ['A'=>5,'B+'=>4,'B'=>3,'C'=>2,'D'=>1,'E'=>0.5,'F'=>0];
@@ -2736,6 +2736,7 @@ class ApplicationController extends Controller
                 $applicant->payment_complete_status = 1;
                 $applicant->intake_id = $application_window->intake->id;
                 $applicant->index_number = $form4index;
+                $applicant->admission_year = $applyr;
                 $applicant->entry_mode = 'DIRECT';
                 $applicant->nationality = 'Tanzanian';
                 $applicant->birth_date = $student->date_of_birth;
