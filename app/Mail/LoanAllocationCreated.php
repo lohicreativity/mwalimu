eb1d4eb2768d145e7579f5746dbea96a3f41c1ec
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-05-04 20:07:32 +0300

    Comments

--- app/Mail/LoanAllocationCreated.php
+++ app/Mail/LoanAllocationCreated.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use App\Models\User;
 
 class LoanAllocationCreated extends Mailable
 {
