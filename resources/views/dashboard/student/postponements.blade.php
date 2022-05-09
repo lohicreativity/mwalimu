3dc7913b06db52a7f905bc2b1584cdd959c73233
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-05-09 12:34:09 +0300

    Comments

--- resources/views/dashboard/student/postponements.blade.php
+++ resources/views/dashboard/student/postponements.blade.php
@@ -133,7 +133,7 @@
                       
                       @if($post->status == 'POSTPONED' && $is_renewal && $post->is_renewal == 1)
                       <a href="{{ url('student/postponement/'.$post->id.'/resume') }}" class="btn btn-primary">Resume</a>
-                      @elseif($post->status == 'POSTPONED' && !$is_renewal && && $post->is_renewal == null)
+                      @elseif($post->status == 'POSTPONED' && !$is_renewal && $post->is_renewal == null)
                       <a href="{{ url('student/postponement/'.$post->id.'/resume') }}" class="btn btn-primary">Resume</a>
                       @endif
 
