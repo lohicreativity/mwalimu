35b2bd672cc3879eb51e336e8fcb6035f786b604
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-05-10 22:54:17 +0300

    Comments

--- app/Http/Controllers/EntryRequirementController.php
+++ app/Http/Controllers/EntryRequirementController.php
@@ -117,11 +117,11 @@ class EntryRequirementController extends Controller
     {
         $staff = User::find(Auth::user()->id)->staff;
         $application_window = ApplicationWindow::where('campus_id',$staff->campus_id)->latest()->offset(1)->first();
-        return $application_window;
         if(!$application_window){
             return redirect()->back()->with('error','No previous application window');
         }
         $reqs = EntryRequirement::where('application_window_id',$application_window->id)->get();
+        return $reqs;
         foreach($reqs as $req){
             $requirement = new EntryRequirement;
             $requirement->campus_program_id = $req->campus_program_id;
