fe82c0bbf69dd5eef162f1e1b16ad75f7c3bd68d
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-05-10 12:03:22 +0300

    Comments

--- app/Domain/Registration/Models/Registration.php
+++ app/Domain/Registration/Models/Registration.php
@@ -4,6 +4,7 @@ namespace App\Domain\Registration\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Domain\Academic\Models\StudyAcademicYear;
 
 class Registration extends Model
 {
@@ -17,6 +18,14 @@ class Registration extends Model
     public function student()
     {
     	return $this->belongsTo(Student::class,'student_id');
+    }
+
+    /**
+     * Establish one to many relationship with study academic years
+     */
+    public function studyAcademicYear()
+    {
+    	return $this->belongsTo(StudyAcademicYear::class,'study_academic_year_id');
     }
 
 }
