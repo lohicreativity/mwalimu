9acfbf8f9e9436e321c44d9211e5d6c2b5b61a00
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-05-12 12:05:00 +0300

    Comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -103,8 +103,8 @@ class ApplicantController extends Controller
             session(['applicant_campus_id'=>$request->get('campus_id')]);
             
             if(!Applicant::where('user_id',Auth::user()->id)->where('campus_id',$request->get('campus_id'))->first()){
-                $app = Applicant::where('user_id',Auth::user()->id)->where('campus_id','0')->first();
-                return $app;
+                $app = Applicant::where('user_id',Auth::user()->id)->where('campus_id',0)->first();
+                return Auth::user();
                 if($app){
                     $applicant = $app;
                     $applicant->user_id = Auth::user()->id;
