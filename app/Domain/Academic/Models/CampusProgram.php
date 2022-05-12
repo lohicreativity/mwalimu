f12276a268c2972fb5950d4a00ca127005a181f1
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-05-12 10:10:32 +0300

    Comments

--- app/Domain/Academic/Models/CampusProgram.php
+++ app/Domain/Academic/Models/CampusProgram.php
@@ -104,6 +104,57 @@ class CampusProgram extends Model
     }
 
     /**
+     * @param string $relation - The relation to create the query for
+     * @param string|null $overwrite_table - In case if you want to overwrite the table (join as)
+     * @return Builder
+     */
+    public static function RelationToJoin(string $relation, $overwrite_table = false) {
+        $instance = (new self());
+        if(!method_exists($instance, $relation))
+            throw new \Error('Method ' . $relation . ' does not exists on class ' . self::class);
+        $relationData = $instance->{$relation}();
+        if(gettype($relationData) !== 'object')
+            throw new \Error('Method ' . $relation . ' is not a relation of class ' . self::class);
+        if(!is_subclass_of(get_class($relationData), Relation::class))
+            throw new \Error('Method ' . $relation . ' is not a relation of class ' . self::class);
+        $related = $relationData->getRelated();
+        $me = new self();
+        $query = $relationData->getQuery()->getQuery();
+        switch(get_class($relationData)) {
+            case HasOne::class:
+                $keys = [
+                    'foreign' => $relationData->getForeignKeyName(),
+                    'local' => $relationData->getLocalKeyName()
+                ];
+            break;
+            case BelongsTo::class:
+                $keys = [
+                    'foreign' => $relationData->getOwnerKeyName(),
+                    'local' => $relationData->getForeignKeyName()
+                ];
+            break;
+            default:
+                throw new \Error('Relation join only works with one to one relationships');
+        }
+        $checks = [];
+        $other_table = ($overwrite_table ? $overwrite_table : $related->getTable());
+        foreach($keys as $key) {
+            array_push($checks, $key);
+            array_push($checks, $related->getTable() . '.' . $key);
+        }
+        foreach($query->wheres as $key => $where)
+            if(in_array($where['type'], ['Null', 'NotNull']) && in_array($where['column'], $checks))
+                unset($query->wheres[$key]);
+        $query = $query->whereRaw('`' . $other_table . '`.`' . $keys['foreign'] . '` = `' . $me->getTable() . '`.`' . $keys['local'] . '`');
+        return (object) [
+            'query' => $query,
+            'table' => $related->getTable(),
+            'wheres' => $query->wheres,
+            'bindings' => $query->bindings
+        ];
+    }
+
+    /**
      * @param Builder $builder
      * @param string $relation - The relation to join
      */
