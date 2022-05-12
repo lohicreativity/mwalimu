7ebcd1bc5f6b1b69a18046b72475fbf249b9f898
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-05-12 20:36:02 +0300

    Comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -676,7 +676,7 @@ class ApplicationController extends Controller
                  $selection->save();
 
                  $select_count = ApplicantProgramSelection::where('applicant_id',$request->get('applicant_id'))->count();
-                 if($select_count == 1 && $request->get('choice') == 1){
+                 if($request->get('choice') == 1){
                     $applicant = Applicant::find($request->get('applicant_id'));
                     $applicant->programs_complete_status = 1;
                     $applicant->save();
