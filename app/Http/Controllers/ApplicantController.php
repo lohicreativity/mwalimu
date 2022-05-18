bd79a63202aad72cf65b000068a571dc678c62fe
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-05-18 11:15:21 +0300

    Comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -705,35 +705,35 @@ class ApplicantController extends Controller
                        $equivalent_must_subjects_count = 0;
                        $nacte_gpa = null;
 
-                       if(unserialize($program->entryRequirements[0]->equivalent_majors) != ''){
-                           foreach(unserialize($program->entryRequirements[0]->equivalent_majors) as $sub){
-                             if(str_contains($program->name,$sub)){
-                                 $has_major = true;
-                             }
-                           }
-                       }else{
-                          if(unserialize($program->entryRequirements[0]->equivalent_must_subjects) != ''){
-                              foreach($applicant->nacteResultDetails as $detail){
-                                  foreach($detail->results as $result){
-                                      if(in_array($result->subject,unserialize($program->entryRequirements[0]->equivalent_must_subjects))){
-                                          $equivalent_must_subjects_count += 1;
-                                      }
-                                  }
-                                  $nacte_gpa = $detail->diploma_gpa;
-                              }
-                          }
-                       }
-                        if($o_level_pass_count >= $program->entryRequirements[0]->pass_subjects && $has_major && $detail->diploma_gpa >= $program->entryRequirements[0]->equivalent_gpa){
+                       // if(unserialize($program->entryRequirements[0]->equivalent_majors) != ''){
+                       //     foreach(unserialize($program->entryRequirements[0]->equivalent_majors) as $sub){
+                       //       if(str_contains($program->name,$sub)){
+                       //           $has_major = true;
+                       //       }
+                       //     }
+                       // }else{
+                       //    if(unserialize($program->entryRequirements[0]->equivalent_must_subjects) != ''){
+                       //        foreach($applicant->nacteResultDetails as $detail){
+                       //            foreach($detail->results as $result){
+                       //                if(in_array($result->subject,unserialize($program->entryRequirements[0]->equivalent_must_subjects))){
+                       //                    $equivalent_must_subjects_count += 1;
+                       //                }
+                       //            }
+                       //            $nacte_gpa = $detail->diploma_gpa;
+                       //        }
+                       //    }
+                       // }
+                       //  if($o_level_pass_count >= $program->entryRequirements[0]->pass_subjects && $has_major && $detail->diploma_gpa >= $program->entryRequirements[0]->equivalent_gpa){
                             
-                           $programs[] = $program;
-                        }
+                       //     $programs[] = $program;
+                       //  }
                        
-                        if(unserialize($program->entryRequirements[0]->equivalent_must_subjects) != ''){
-                            if($o_level_pass_count >= $program->entryRequirements[0]->pass_subjects && $equivalent_must_subjects_count >= count(unserialize($program->entryRequirements[0]->equivalent_must_subjects)) && $detail->diploma_gpa >= $program->entryRequirements[0]->equivalent_gpa){
+                       //  if(unserialize($program->entryRequirements[0]->equivalent_must_subjects) != ''){
+                       //      if($o_level_pass_count >= $program->entryRequirements[0]->pass_subjects && $equivalent_must_subjects_count >= count(unserialize($program->entryRequirements[0]->equivalent_must_subjects)) && $detail->diploma_gpa >= $program->entryRequirements[0]->equivalent_gpa){
                                 
-                               $programs[] = $program;
-                            }
-                        }
+                       //         $programs[] = $program;
+                       //      }
+                       //  }
 
 
                         $exclude_out_subjects_codes = unserialize($program->entryRequirements[0]->open_exclude_subjects); //['OFC 017','OFP 018','OFP 020'];
@@ -752,7 +752,7 @@ class ApplicantController extends Controller
                             }
                             
                             if(unserialize($program->entryRequirements[0]->equivalent_must_subjects) != ''){
-                                if($out_pass_subjects_count >= 3 && $detail->gpa >= 3 && $nacte_gpa >= 2){
+                                if($out_pass_subjects_count >= 3 && $detail->gpa >= 3 && $equivalent_must_subjects_count >= count(unserialize($program->entryRequirements[0]->equivalent_must_subjects)) && $nacte_gpa >= 2){
                                         $programs[] = $program;
                                 }
                             }
