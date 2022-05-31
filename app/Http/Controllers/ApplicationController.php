0a3646d0317cf2c85bb8dadf10bf23dc1641a5ac
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-05-31 17:27:06 +0300

    Comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -295,7 +295,7 @@ class ApplicationController extends Controller
             $list = Applicant::whereHas('intake.applicationWindows',function($query) use($request){
                  $query->where('id',$request->get('application_window_id'));
             })->whereHas('selections',function($query) use($request){
-                 $query->where('status','APPROVING')->orWhere('status','SELECTED')->orWhere('status','PENDING');
+                 $query->where('status','APPROVING');
             })->with(['nextOfKin','intake','selections.campusProgram.program','nectaResultDetails.results','nacteResultDetails.results'])->where('program_level_id',$request->get('program_level_id'))->where('campus_id',$staff->campus_id)->get();
          }
 
