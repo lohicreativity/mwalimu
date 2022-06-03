363d7d236ca856fc1a6fda34b19bfbf8d0aa19dd
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-06-03 17:03:52 +0300

    Comments

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -278,10 +278,10 @@ class RegistrationController extends Controller
         $pdf = PDF::loadView('dashboard.registration.print-id-card-bulk',$data,[],[
                'format'=>'A7',
                'mode' => 'utf-8',
-               'margin_top'=>0,
-               'margin_bottom'=>0,
-               'margin_left'=>0,
-               'margin_right'=>0,
+               // 'margin_top'=>0,
+               // 'margin_bottom'=>0,
+               // 'margin_left'=>0,
+               // 'margin_right'=>0,
                'orientation'=>'L',
                'display_mode'=>'fullpage',
                // 'format'=>[500,400]
