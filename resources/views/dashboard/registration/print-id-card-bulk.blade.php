848fbe230b191f23a692683ee5d25261a93a023a
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-06-07 13:16:41 +0300

    Comments

--- resources/views/dashboard/registration/print-id-card-bulk.blade.php
+++ resources/views/dashboard/registration/print-id-card-bulk.blade.php
@@ -228,19 +228,20 @@
         </div>
         <div class="col-md-9">
            <h3 style="margin-top: 0px;">THE MWALIMU NYERERE MEMORIAL ACADEMY</h3>
-        </div>
-     </div>
-     <div class="row">
-        <div class="col-md-3" style="text-align: center;">
-          <img src="{{ asset('img/user-avatar.png')}}" class="ss-logo" style="text-align: center; width: 150px;">
-        </div>
-        <div class="col-md-9">
            <h5 style="margin: 0px 0px 0px 20px;">REGNO: <span style="font-style: italic;">{{ $student->student->registration_number }}</span></h5>
            <h5 style="margin: 0px 0px 0px 20px;">NAME: <span style="font-style: italic;">{{ $student->student->first_name }} {{ $student->student->middle_name }} {{ $student->student->surname }}</span></h5>
            <h5 style="margin: 0px 0px 0px 20px;">MOBILE: <span style="font-style: italic;">{{ $student->student->phone }}</i></h5>
            <h5 style="margin: 0px 0px 0px 20px;">VALID TO: <span style="font-style: italic;">{{ App\Utils\DateMaker::toStandardDate($study_academic_year->end_date) }}</span></h5>
            <h5 style="margin: 0px 0px 0px 20px;">SIGNATURE:
            <img src="{{ asset('signatures/'.$student->signature) }}" style="width: 100px; height: auto; margin-top: 20px;"></h5>
+        </div>
+     </div>
+     <div class="row">
+        <div class="col-md-3" style="text-align: center;">
+          <img src="{{ asset('avatars/'.$student->image)}}" class="ss-logo" style="text-align: center; width: 150px;">
+        </div>
+        <div class="col-md-9">
+           
         </div>
      </div>
      <div class="row">
