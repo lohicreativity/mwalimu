a1e4906d433ae56eb47e1d3434ecb9dd15bbe9d1
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-06-22 19:18:40 +0300

    Comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -515,7 +515,6 @@ class ApplicantController extends Controller
                                  }
                               }
                            }
-                           return $o_level_pass_count;
                          }elseif($detail->exam_id == 2){
                            $other_advance_must_subject_ready = false;
                            $other_advance_subsidiary_ready = false;
@@ -558,6 +557,7 @@ class ApplicantController extends Controller
                                  }
                               }
                            }
+                           return $a_level_principle_pass_count;
                          }
                          
                          if($o_level_pass_count >= $program->entryRequirements[0]->pass_subjects && ($a_level_subsidiary_pass_count >= 1 && $a_level_principle_pass_count >= 1)){
