3fcbb2c1aa8e000f89e2d818a49ec24905720cbf
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-06-23 22:25:47 +0300

    Comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -766,7 +766,6 @@ class ApplicantController extends Controller
                                 }
                                 $nacte_gpa = $detail->diploma_gpa;
                             }
-                            return $equivalent_must_subjects_count;
                         }
                         if(unserialize($program->entryRequirements[0]->equivalent_majors) != ''){
                             if($o_level_pass_count >= $program->entryRequirements[0]->pass_subjects && $has_major && $nacte_gpa >= $program->entryRequirements[0]->equivalent_gpa){
