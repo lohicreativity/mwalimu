681d1aa5a28b6d20e6e0650177a5368cb80d2e7c
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-06-26 17:27:51 +0300

    Comments

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -469,7 +469,7 @@ class ModuleAssignmentController extends Controller
             }
               $headers = [
                       'Cache-Control'       => 'must-revalidate, post-check=0, pre-check=0',   
-                      'Content-type'        => 'text/csv',
+                      'Content-type'        => 'application/csv',
                       'Content-Disposition' => 'attachment; filename='.$module_assignment->module->code.'_'.$module_assignment->studyAcademicYear->academicYear->year.'.csv',
                       'Expires'             => '0',
                       'Pragma'              => 'public'
