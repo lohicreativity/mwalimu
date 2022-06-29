493c98d34d4d047def42baadbd858e10cf9533ac
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-06-29 21:54:39 +0300

    Comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -1844,7 +1844,7 @@ class ApplicationController extends Controller
     public function sendAdmissionLetter(Request $request)
     {   
         $job = new SendAdmissionLetter($request->all());
-        dispatch($job);
+        $this->dispatch($job);
 
         return dd($job->getResponse());
 
