cfef51c69d5690dc60fea95173f9f524447bb295
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-06-30 21:32:46 +0300

    Comments

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get('test',function(){
 	$acpac = new ACPACService;
 	$acpac->query("INSERT INTO receipts (BANK,BANKNAME,RCPNUMBER,RCPDATE,RCPDESC,IDCUST,NAMECUST,INVOICE,AMTAPPLIED,IMPORTED,IMPDATE) VALUES
    ('J','CRDB','REC03','10','TF','MNMA003','TEST','INV003','100.0','C','10')");
-	$results = $acpac->query('SELECT * FROM customers');
+	$results = $acpac->query('SELECT * FROM invoices');
     while ($row = sqlsrv_fetch_array($results)) {
     	print_r($row);
     }
