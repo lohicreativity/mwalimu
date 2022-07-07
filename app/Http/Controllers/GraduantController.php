8fbd2b71ef522bd4d0195d8faed93cc4cbc398fa
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-07-07 17:27:23 +0300

    Comments

--- app/Http/Controllers/GraduantController.php
+++ app/Http/Controllers/GraduantController.php
@@ -66,6 +66,8 @@ class GraduantController extends Controller
           	$campus_program = CampusProgram::with('program')->find($request->get('campus_program_id'));
           	$students = Student::whereHas('studentshipStatus',function($query){
                 $query->where('name','ACTIVE');
+            })->whereHas('academicStatus',function($query){
+                $query->where('name','PASS');
             })->with(['annualRemarks','overallRemark'])->whereHas('campusProgram',function($query) use ($program, $request){
                  $query->where('program_id',$program->id)->where('campus_id',$request->get('campus_id'));
             })->where('year_of_study',$program->min_duration)->get();
