8ec0445b81e61b58463baa4a2d9fc20028c287a0
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-07-10 12:21:32 +0300

    Comments

--- resources/views/dashboard/academic/resumptions.blade.php
+++ resources/views/dashboard/academic/resumptions.blade.php
@@ -21,12 +21,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>{{ __('Postponements') }}</h1>
+            <h1>{{ __('Resumptions') }}</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">{{ __('Postponements') }}</li>
+              <li class="breadcrumb-item active">{{ __('Resumptions') }}</li>
             </ol>
           </div>
         </div>
