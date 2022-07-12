1ab9db0fbfc4c73f0388137be2ecd989a5f8b0d2
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-07-12 21:07:00 +0300

    Comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2799,7 +2799,7 @@ class ApplicationController extends Controller
      */
     public function submitInternalTransfer(Request $request)
     {
-        $student = Student::with(['applicant.selections.campusProgram','nectaResultDetails.results','applicant.nacteResultDetails.results','applicant.programLevel'])->find($request->get('student_id'));
+        $student = Student::with(['applicant.selections.campusProgram','applicant.nectaResultDetails.results','applicant.nacteResultDetails.results','applicant.programLevel'])->find($request->get('student_id'));
 
         $award = $student->applicant->programLevel;
         $applicant = $student->applicant;
