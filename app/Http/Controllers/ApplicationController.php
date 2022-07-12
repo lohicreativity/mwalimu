7b06a21912caca7ce96ad5a14498e4c2df4ffd0d
Amani Ghachocha <scholardreamtanzania@gmail.com>
2022-07-12 20:32:13 +0300

    Comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -1351,7 +1351,7 @@ class ApplicationController extends Controller
 
         $selection = ApplicantProgramSelection::with('campusProgram.program')->where('applicant_id',$request->get('applicant_id'))->where('status','SELECTED')->first();
 
-        $studentship_status = ($applicant->is_postponed == 1)? StudentshipStatus::where('name','POSTPONED')->first() : StudentshipStatus::where('name','ACTIVE')->first();
+        $studentship_status = ($applicant->has_postponed == 1)? StudentshipStatus::where('name','POSTPONED')->first() : StudentshipStatus::where('name','ACTIVE')->first();
         $academic_status = AcademicStatus::where('name','FRESHER')->first();
         $semester = Semester::where('status','ACTIVE')->first();
         $last_student = DB::table('students')->select(DB::raw('SUBSTRING(REVERSE(registration_number),0,7) AS last_number'))->first();
