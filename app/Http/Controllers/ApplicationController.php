e40f9c83cbf5a8a4c1474db23a19793a0e2016f5
Amani Ghachocha <amanighachocha@gmail.com>
2022-07-17 12:06:39 +0300

    Comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -1360,7 +1360,7 @@ class ApplicationController extends Controller
         $studentship_status = ($applicant->has_postponed == 1)? StudentshipStatus::where('name','POSTPONED')->first() : StudentshipStatus::where('name','ACTIVE')->first();
         $academic_status = AcademicStatus::where('name','FRESHER')->first();
         $semester = Semester::where('status','ACTIVE')->first();
-        $last_student = DB::table('students')->select(DB::raw('MAX(SUBSTRING(REVERSE(registration_number),0,7)) AS last_number'))->where('campus_program_id',$selection->campusProgram->id'campus_pro')->first();
+        $last_student = DB::table('students')->select(DB::raw('MAX(SUBSTRING(REVERSE(registration_number),0,7)) AS last_number'))->where('campus_program_id',$selection->campusProgram->id)->first();
         //Student::where('campus_program_id',$selection->campusProgram->id)->max();
         if(!empty($last_student->last_number)){
            $code = sprintf('%04d',explode('/', $last_student->last_number)[2] + 1);
