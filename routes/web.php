70cb0307acb95a34a9794f00c57ae2844d9a2a50
Amani Ghachocha <amanighachocha@gmail.com>
2022-07-18 21:12:33 +0300

    Comments

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::get('test',function(){
 	// $acpac->query("DELETE FROM customer");
 	// $acpac->query("DELETE FROM invoices");
 	// $acpac->query("DELETE FROM receipts");
-	$results = $acpac->query('SELECT * FROM invoices');
+	$results = $acpac->query('SELECT * FROM customer');
      while ($row = sqlsrv_fetch_array($results)) {
      	print_r($row);
      }
