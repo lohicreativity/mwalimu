f3ce92be2f8a63a86c15718a0508e3db7aaa21fa
Amani Ghachocha <amanighachocha@gmail.com>
2022-07-24 15:31:29 +0300

    Comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -3307,7 +3307,7 @@ class ApplicationController extends Controller
            }])->where('campus_id',$applicant->campus_id)->get() : [];
         
 
-        $award = $applicant->programLevel;
+        $award = $applicant? $applicant->programLevel : null;
         $programs = [];
 
         $o_level_grades = ['A'=>5,'B+'=>4,'B'=>3,'C'=>2,'D'=>1,'E'=>0.5,'F'=>0];
