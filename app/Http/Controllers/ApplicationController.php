ff2753577fe32426eef48c6e573b07c01cca4f04
Amani Ghachocha <amanighachocha@gmail.com>
2022-07-27 19:42:25 +0300

    Comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4954,6 +4954,8 @@ class ApplicationController extends Controller
         $json = json_encode($xml_response);
         $array = json_decode($json,TRUE);
 		
+		return dd($array);
+		
 		if($array['Response']['ResponseParameters']['StatusCode'] == 200){
 		   $trans = InternalTransfer::find($transfer->id);
 		   $trans->status = 'SUBMITTED';
