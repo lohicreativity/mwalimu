cc60d8a009ddca66ff72f1cffcfe0af67ad45224
Amani Ghachocha <amanighachocha@gmail.com>
2022-07-27 18:07:42 +0300

    Comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4679,7 +4679,7 @@ class ApplicationController extends Controller
 				return redirect()->back()->with('error','Programme does not have capacity to accomodate the transfer');
 			}
 		
-			$last_student = DB::table('students')->select(DB::raw('MAX(SUBSTRING(REVERSE(registration_number),0,7)) AS last_number'))->where('campus_program_id',$selection->campusProgram->id)->first();
+			$last_student = DB::table('students')->select(DB::raw('MAX(SUBSTRING(REVERSE(registration_number),0,7)) AS last_number'))->where('campus_program_id',$transfer_program->id)->first();
 			//Student::where('campus_program_id',$selection->campusProgram->id)->max();
 			if(!empty($last_student->last_number)){
 			   $code = sprintf('%04d',explode('/', $last_student->last_number)[2] + 1);
@@ -4715,7 +4715,7 @@ class ApplicationController extends Controller
 			
 			$student = Student::find($student->id);
 			$student->registration_number = 'MNMA/'.$program_code.'/'.$code.'/'.$year;
-			$student->campus_program_id = $selection->campusProgram->id;
+			$student->campus_program_id = $transfer_program->id;
 			
 			
 			$user = new User;
