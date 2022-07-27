ee98ee8420f36e10becef1c2122a351282860132
Amani Ghachocha <amanighachocha@gmail.com>
2022-07-27 19:23:11 +0300

    Comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4682,7 +4682,7 @@ class ApplicationController extends Controller
 			$last_student = DB::table('students')->select(DB::raw('MAX(SUBSTRING(REVERSE(registration_number),1,7)) AS last_number'))->where('campus_program_id',$transfer_program->id)->first();
 			//Student::where('campus_program_id',$selection->campusProgram->id)->max();
 			if(!empty($last_student->last_number)){
-			   $code = sprintf('%04d',explode('/', $last_student->last_number)[2] + 1);
+			   $code = sprintf('%04d',explode('/', $last_student->last_number)[1] + 1);
 			}else{
 			   $code = sprintf('%04d',1);
 			}
