016bb2d832e17fc2c8a4eafe77db8b9bc78ec46a
Amani Ghachocha <amanighachocha@gmail.com>
2022-07-31 17:11:17 +0300

    Comments

--- resources/views/dashboard/settings/gpa-classifications.blade.php
+++ resources/views/dashboard/settings/gpa-classifications.blade.php
@@ -188,14 +188,14 @@
                     <td>{{ $class->max_gpa }}</td>
                     <td>
 
-                      <a class="btn btn-info btn-sm" href="#" data-toggle="modal" data-target="#ss-edit-level-{{ $level->id }}">
+                      <a class="btn btn-info btn-sm" href="#" data-toggle="modal" data-target="#ss-edit-level-{{ $class->id }}">
                               <i class="fas fa-pencil-alt">
                               </i>
                               Edit
                        </a>
                       
 
-                       <div class="modal fade" id="ss-edit-level-{{ $level->id }}">
+                       <div class="modal fade" id="ss-edit-level-{{ $class->id }}">
                         <div class="modal-dialog modal-lg">
                           <div class="modal-content">
                             <div class="modal-header">
@@ -276,15 +276,13 @@
                         <!-- /.modal-dialog -->
                       </div>
                       <!-- /.modal -->
-                      @can('delete-nta-level')
-                      <a class="btn btn-danger btn-sm" href="#" @if(count($level->programs) == 0) data-toggle="modal" data-target="#ss-delete-level-{{ $level->id }}" @else disabled="disabled" @endif>
+                      <a class="btn btn-danger btn-sm" href="#" data-toggle="modal" data-target="#ss-delete-level-{{ $class->id }}">
                               <i class="fas fa-trash">
                               </i>
                               Delete
                        </a>
-                       @endcan
 
-                       <div class="modal fade" id="ss-delete-level-{{ $level->id }}">
+                       <div class="modal fade" id="ss-delete-level-{{ $class->id }}">
                         <div class="modal-dialog modal-lg">
                           <div class="modal-content">
                             <div class="modal-header">
@@ -297,10 +295,10 @@
                               <div class="row">
                                 <div class="col-12">
                                     <div id="ss-confirmation-container">
-                                       <p id="ss-confirmation-text">Are you sure you want to delete this level from the list?</p>
+                                       <p id="ss-confirmation-text">Are you sure you want to delete this gpa classification from the list?</p>
                                        <div class="ss-form-controls">
                                          <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                                         <a href="{{ url('settings/nta-level/'.$level->id.'/destroy') }}" class="btn btn-danger">Delete</a>
+                                         <a href="{{ url('settings/gpa-classification/'.$class->id.'/destroy') }}" class="btn btn-danger">Delete</a>
                                          </div><!-- end of ss-form-controls -->
                                       </div><!-- end of ss-confirmation-container -->
                                   </div><!-- end of col-md-12 -->
