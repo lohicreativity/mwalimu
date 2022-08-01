948719fed3b5b5369b03b20c61e850c636ad11d2
Amani Ghachocha <amanighachocha@gmail.com>
2022-08-01 17:32:10 +0100

    Comments

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -895,6 +895,7 @@ class StudentController extends Controller
 
                    // Diploma
                    if(str_contains($award->name,'Diploma')){
+                       return $award;
                        $o_level_pass_count = 0;
                        $a_level_principle_pass_count = 0;
                        $a_level_subsidiary_pass_count = 0;
