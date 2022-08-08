e368ba0ee7a8f5ed95d0cd94691dc79eec487d6c
Amani Ghachocha <amanighachocha@gmail.com>
2022-08-08 12:26:20 +0300

    Comments

--- resources/views/dashboard/application/edit-applicant-details.blade.php
+++ resources/views/dashboard/application/edit-applicant-details.blade.php
@@ -156,6 +156,8 @@
                    <button type="submit" class="btn btn-primary">{{ __('Save Changes') }}</button>
 
                    <a href="{{ url('application/reset-applicant-password-default?user_id='.$applicant->user_id.'&applicant_id='.$applicant->id) }}" class="btn btn-primary">Reset Password</a>
+
+                   <a href="#" data-token="{{ session()->token() }}" data-applicant-id="{{ $applicant->id }}" class="btn btn-primary">Reset Control Number</a>
                   </div>
 
                    {!! Form::close() !!}
