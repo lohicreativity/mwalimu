c055253cfec7d980cec275847f83983f5ddf6519
Amani Ghachocha <amanighachocha@gmail.com>
2022-08-10 10:26:26 +0300

    Comments

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -907,7 +907,6 @@ class ModuleAssignmentController extends Controller
 
                 $invalid_students = [];
                 foreach($uploaded_students as $up_stud){
-                    return $up_stud;
                    if(Student::whereHas('registrations',function($query) use($module_assignment){
                      $query->where('year_of_study',$module_assignment->programModuleAssignment->year_of_study)->where('semester_id',$module_assignment->programModuleAssignment->semester_id)->where('study_academic_year_id',$module_assignment->programModuleAssignment->study_academic_year_id);
                 })->where('campus_program_id',$module_assignment->programModuleAssignment->campus_program_id)->where('registration_number',$up_stud->registration_number)->count() == 0){
