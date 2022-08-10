5b45809f24f740d741c7f63ed2e75120080e7f23
Amani Ghachocha <amanighachocha@gmail.com>
2022-08-10 18:35:56 +0300

    Comments

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -982,11 +982,11 @@ class ModuleAssignmentController extends Controller
               $csvFile = $destination.$csvFileName;
               $file_handle = fopen($csvFile, 'r');
               while (!feof($file_handle)) {
-                  $line_of_text[] = fgetcsv($file_handle, 0, ',');
+                  $line_of_text_2[] = fgetcsv($file_handle, 0, ',');
               }
               fclose($file_handle);
               $invalidEntries = [];
-              foreach($line_of_text as $line){
+              foreach($line_of_text_2 as $line){
                    if((floatval(trim($line[1])) < 0 || floatval(trim($line[1])) > 100 || !is_numeric(trim($line[1]))) && !empty($line[1])){
                      $validationStatus = false;
                      $invalidEntries[] = trim($line[0]);
@@ -1020,7 +1020,7 @@ class ModuleAssignmentController extends Controller
                   $line_of_text[] = fgetcsv($file_handle, 0, ',');
               }
               fclose($file_handle);
-              return dd($line_of_text);
+
               foreach($line_of_text as $line){
 				  //whereHas('registrations',function($query) use($module_assignment){
                    //  $query->where('year_of_study',$module_assignment->programModuleAssignment->year_of_study)->where('semester_id',$module_assignment->programMo//duleAssignment->semester_id)->where('study_academic_year_id',$module_assignment->programModuleAssignment->study_academic_year_id);
@@ -1217,6 +1217,7 @@ class ModuleAssignmentController extends Controller
                           $result->uploaded_by_user_id = Auth::user()->id;
                           $result->save();
                   }else{
+                      return $request->get('assessment_plan_id');
                       $result_log = new CourseWorkResultLog;
                       $result_log->module_assignment_id = $request->get('module_assignment_id');
                       $result_log->assessment_plan_id = $plan->id;
