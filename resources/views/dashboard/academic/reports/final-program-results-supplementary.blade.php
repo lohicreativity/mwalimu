46952778e22849204d3d6a11dfcd5d6a2dfb286b
Amani Ghachocha <amanighachocha@gmail.com>
2022-08-21 17:26:21 +0300

    Comments

--- resources/views/dashboard/academic/reports/final-program-results-supplementary.blade.php
+++ resources/views/dashboard/academic/reports/final-program-results-supplementary.blade.php
@@ -266,12 +266,13 @@
                           @foreach($mods as $assignment)
                             @foreach($student->examinationResults as $result)
                                  @if($result->module_assignment_id == $assignment->id)
-                                    <h1>{{ $result->module_assignment_id }}</h1>
                                     @if(!is_null($result->supp_score))
+                                        <h1>{{ $result->supp_score }}</h1>
                                         @php $display_student = true; @endphp
                                     @else
                                       @if(count($result->moduleAssignment->specialExams) != 0) 
                                         @php $display_student = true; @endphp
+                                        <h1>{{ count($result->moduleAssignment->specialExams) }}</h1>
                                       @endif
                                     @endif
                                  @endif
