dd9b0fb2074e4bd6acfa0a2f29e78586b76c825a
Amani Ghachocha <amanighachocha@gmail.com>
2022-08-21 18:57:01 +0300

    Comments

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -725,9 +725,9 @@ class ExaminationResultController extends Controller
                               
                               $overall_gpa = $credits != 0? bcdiv($points/$credits, 1,1) : null;
                               $gpa_class = GPAClassification::where('nta_level_id',$student->campusProgram->program->nta_level_id)->where('study_academic_year_id',$request->get('study_academic_year_id'))->where('min_gpa','<=',bcdiv($overall_gpa,1,1))->where('max_gpa','>=',bcdiv($overall_gpa,1,1))->first();
-                              // if(!$gpa_class){
-                               //  return redirect()->back()->with('error','GPA classification not defined');
-                              // }
+                              if(!$gpa_class && $key == 7){
+                                return redirect()->back()->with('error','GPA classification not defined');
+                              }
                               if($gpa_class && $student_buffer[$key]['year_of_study'] == $student->year_of_study && str_contains($semester->name,'2')){
                                  $overall_remark = $gpa_class->name;
 
