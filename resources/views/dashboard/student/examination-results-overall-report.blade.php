c4679bee90507ab21e746ea24226887d68678a56
Amani Ghachocha <amanighachocha@gmail.com>
2022-08-31 19:12:30 +0300

    comments

--- resources/views/dashboard/student/examination-results-overall-report.blade.php
+++ resources/views/dashboard/student/examination-results-overall-report.blade.php
@@ -21,7 +21,8 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>{{ __('Examination Results') }}</h1>
+            <h1>{{ __('Examination Results') }}
+            </h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
