c1fcd2664e76097e6e19187bfb2f9b75b19de2ba
Amani Ghachocha <amanighachocha@gmail.com>
2022-09-12 23:42:54 +0300

    Comments

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -2709,9 +2709,7 @@ class ExaminationResultController extends Controller
          $core_program_modules = ModuleAssignment::whereHas('programModuleAssignment',function($query) use ($ac_yr_id,$yr_of_study){
                    $query->where('study_academic_year_id',$ac_yr_id)->where('year_of_study',$yr_of_study)->where('category','COMPULSORY');
                  })->get();
-            $opt_program_modules = ModuleAssignment::whereHas('programModuleAssignment.students',function($query) use($student){
-                     //$query->where('id',$student->id);
-                 })->whereHas('programModuleAssignment',function($query) use($ac_yr_id,$yr_of_study){
+            $opt_program_modules = ModuleAssignment::whereHas('programModuleAssignment',function($query) use($ac_yr_id,$yr_of_study){
                      $query->where('study_academic_year_id',$ac_yr_id)->where('year_of_study',$yr_of_study)->where('category','OPTIONAL');
                 })->get();
 
