70e79dcaf23d9f016e90b0d9c902bc0dd359dad6
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-09-26 16:14:21 +0300

    comments

--- resources/views/dashboard/application/run-selection.blade.php
+++ resources/views/dashboard/application/run-selection.blade.php
@@ -98,7 +98,7 @@
                </div>
                <div class="card-footer">
                   <button type="submit" class="btn btn-primary">{{ __('Run Selection') }}</button>
-				  <a id="resetLink" href="{{ url('application/reset-selections?application_window_id='.$application_window->id) }}" class="btn btn-primary">Reset Selection</a>
+				  <a id="resetLink" href="{{ url('application/reset-selections?application_window_id='.$application_window->id.'&program_level_id='val()) }}" class="btn btn-primary">Reset Selection</a>
                 </div>
               {!! Form::close() !!}
             </div>
@@ -112,7 +112,8 @@
   <script>
     function val () {
       selectValue = document.getElementById('awards').value;
-      document.getElementById('resetLink').setAttribute("href", "{{ url('application/reset-selections?application_window_id='.$application_window->id.'\\&program_level_id='.\" + selectValue + ") }}");
+      return selectValue;
+      // document.getElementById('resetLink').setAttribute("href", "{{ url('application/reset-selections?application_window_id='.$application_window->id.'\\&program_level_id='.\" + selectValue + ") }}");
     }
   </script>
   <!-- /.content-wrapper -->
