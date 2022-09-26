4d3d9a56e30726535bc3e2170292316c6352118f
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-09-26 17:13:37 +0300

    comments

--- resources/views/dashboard/application/run-selection.blade.php
+++ resources/views/dashboard/application/run-selection.blade.php
@@ -98,7 +98,7 @@
                </div>
                <div class="card-footer">
                   <button type="submit" class="btn btn-primary">{{ __('Run Selection') }}</button>
-				  <a id="resetLink" onclick="reset()" href="{{ url('application/reset-selections?application_window_id='.$application_window->id.'&program_level_id=pid') }}" class="btn btn-primary">Reset Selection</a>
+				  <a id="resetLink"  href="{{ url('application/reset-selections?application_window_id='.$application_window->id.'&program_level_id=pid') }}" class="btn btn-primary">Reset Selection</a>
                 </div>
               {!! Form::close() !!}
             </div>
@@ -119,7 +119,8 @@
     function reset () {
       selectValue = document.getElementById('awards').value;
       urlString = document.getElementById('resetLink').getAttribute('href');
-      document.getElementById('resetLink').setAttribute("href", urlString.replace(/1|2|4|5/gi, selectValue));
+      strLength = "program_level_id=".length;
+      document.getElementById('resetLink').setAttribute("href", , selectValue));
     }
   </script>
   <!-- /.content-wrapper -->
