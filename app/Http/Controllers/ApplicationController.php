8553aa957ed212828d9da02b78bb273eab6d4597
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-09-26 14:47:22 +0300

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -183,27 +183,30 @@ class ApplicationController extends Controller
           ApplicantProgramSelection::whereHas('applicant',function($query) use($staff, $request){
          return ApplicantProgramSelection::whereHas('applicant',function($query) use($staff, $request){
 
+            
 
               
 */
-         return ApplicantProgramSelection::whereHas('applicant',function($query) use($staff, $request){
-             $query->where('campus_id',$staff->campus_id)
-                   ->where('application_window_id',$request->get('application_window_id'))
-                   ->where('program_level_id',$request->get('program_level_id'))
-                   ->where(function ($q){
-                     $q->where('status','!=','ADMITTED')
-                       ->orWhere('status','!=','SUBMITTED');
-                    });
+        //  return ApplicantProgramSelection::whereHas('applicant',function($query) use($staff, $request){
+        //      $query->where('campus_id',$staff->campus_id)
+        //            ->where('application_window_id',$request->get('application_window_id'))
+        //            ->where('program_level_id',$request->get('program_level_id'))
+        //            ->where(function ($q){
+        //              $q->where('status','!=','ADMITTED')
+        //                ->orWhere('status','!=','SUBMITTED');
+        //             });
 
-              })->update(['statussasa'=>'ELIGIBLE']);
+        //       })->update(['status'=>'ELIGIBLE']);
 
-		 Applicant::where('application_window_id',$request->get('application_window_id'))
-         ->where('campus_id',$staff->campus_id)
-         ->where('program_level_id',$request->get('program_level_id'))
-         ->where(function ($y) {
-               $y->where('status','!=','ADMITTED')
-                 ->orWhere('status','!=','SUBMITTED');
-            })->update(['status'=>null]);
+		//  Applicant::where('application_window_id',$request->get('application_window_id'))
+        //  ->where('campus_id',$staff->campus_id)
+        //  ->where('program_level_id',$request->get('program_level_id'))
+        //  ->where(function ($y) {
+        //        $y->where('status','!=','ADMITTED')
+        //          ->orWhere('status','!=','SUBMITTED');
+        //     })->update(['status'=>null]);
+
+            echo $request->get('award_id');
 
 		 return redirect()->back()->with('message','Selections reset successfully');
 	 }
