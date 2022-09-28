b23e590ced7a93a9061f4d17f3d66adddb87fc34
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-09-28 16:38:33 +0300

    comments

--- app/Http/Controllers/AppealController.php
+++ app/Http/Controllers/AppealController.php
@@ -123,6 +123,7 @@ class AppealController extends Controller
               $file_handle = fopen($csvFile, 'r');
               while (!feof($file_handle)) {
                   $line_of_text[] = fgetcsv($file_handle, 0, ',');
+                  return print_r($line_of_text);
               }
               fclose($file_handle);
               foreach($line_of_text as $line){
