e03e9c79e3ef34bf0cfccdfb7b6f541f6b1a60a7
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-09-29 16:53:48 +0300

    comments

--- resources/views/dashboard/student/special-exams.blade.php
+++ resources/views/dashboard/student/special-exams.blade.php
@@ -75,9 +75,59 @@
                 </div>
                 <div class="row">
 
+                @foreach($module_assignments as $assign)
+<!-- {{ $ex->moduleAssignment->module->id }} <br> -->
+                @if(count($special_exam_requests) != 0)
                    @foreach($special_exam_requests as $exl)
                      @foreach($exl->exams as $ex)
-                     {{ $ex->moduleAssignment->module->id }} <br>
+                        @if($assign->programModuleAssignment->category == 'OPTIONAL' && $opted_module[0]->module_id == $assign->module_id && $assign->module_id == $ex->moduleAssignment->module->id)
+                        <div class="col-3">
+                          <div class="checkbox">
+                            <label>
+                                {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id, true) !!}
+                                {{ $assign->module->name }}
+                            </label>
+                          </div>
+                        </div>
+                        @elseif($assign->programModuleAssignment->category == 'COMPULSORY' && $assign->module_id == $ex->moduleAssignment->module->id)
+                        <div class="col-3">
+                          <div class="checkbox">
+                            <label>
+                                {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
+                                {{ $assign->module->name }}
+                            </label>
+                          </div>
+                        </div>
+                        @endif
+                     @endforeach
+                   @endforeach
+                @endif
+
+
+                  @if($assign->programModuleAssignment->category == 'OPTIONAL' && $opted_module[0]->module_id == $assign->module_id)
+                    <div class="col-3">
+                      <div class="checkbox">
+                        <label>
+                            {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id, true) !!}
+                            {{ $assign->module->name }}
+                        </label>
+                      </div>
+                    </div>
+                    @elseif($assign->programModuleAssignment->category == 'COMPULSORY')
+                    <div class="col-3">
+                      <div class="checkbox">
+                        <label>
+                            {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
+                            {{ $assign->module->name }}
+                        </label>
+                      </div>
+                    </div>
+                    @endif
+                  @endforeach
+
+                   @foreach($special_exam_requests as $exl)
+                     @foreach($exl->exams as $ex)
+                     <!-- {{ $ex->moduleAssignment->module->id }} <br> -->
                      @endforeach
                    @endforeach
                 </div>
