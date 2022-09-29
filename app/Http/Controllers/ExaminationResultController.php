2ef52e254d0f8613312a1b9d202f14e0c41b22c3
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-09-29 12:36:27 +0300

    comments

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -1057,7 +1057,6 @@ class ExaminationResultController extends Controller
      */
     public function update(Request $request)
     {
-      return 123;
         try{
             $validation = Validator::make($request->all(),[
                 'final_score'=>'numeric|min:0|max:100',
