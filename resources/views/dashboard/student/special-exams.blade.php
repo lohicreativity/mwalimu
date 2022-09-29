d422b154b171296b1780354df7a71e096c50ef3c
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-09-29 15:40:12 +0300

    comments

--- resources/views/dashboard/student/special-exams.blade.php
+++ resources/views/dashboard/student/special-exams.blade.php
@@ -75,7 +75,7 @@
                 </div>
                 <div class="row">
                    @foreach($module_assignments as $assign)
-                   {{ $opted_module[0]->module_id }} : {{ $assign->module_id}} : {{ $assign->category }} <br>
+                   {{ $opted_module[0]->module_id }} : {{ $assign->module_id}} : {{ $assign->program_module_assignment->category }} <br>
                    @endforeach
                 </div>
                 </div>
