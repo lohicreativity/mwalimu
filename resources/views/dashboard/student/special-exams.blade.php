ef4157dedb987f9e314c408a0d779867e6277c07
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-09-29 18:05:38 +0300

    comments

--- resources/views/dashboard/student/special-exams.blade.php
+++ resources/views/dashboard/student/special-exams.blade.php
@@ -96,7 +96,26 @@
                                   </label>
                                 </div>
                               </div>    
-                                           
+                   @else
+                   @if($assign->programModuleAssignment->category == 'OPTIONAL' && $opted_module[0]->module_id == $assign->module_id && $assign->module_id == $ex->moduleAssignment->module->id)
+                              <div class="col-3">
+                                <div class="checkbox">
+                                  <label>
+                                      {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id, true, array('disabled')) !!}
+                                      {{ $assign->module->name }}
+                                  </label>
+                                </div>
+                              </div>
+                              @elseif($assign->programModuleAssignment->category == 'COMPULSORY' && $assign->module_id == $ex->moduleAssignment->module->id)
+                              <div class="col-3">
+                                <div class="checkbox">
+                                  <label>
+                                      {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id, true, array('disabled')) !!}
+                                      {{ $assign->module->name }}
+                                  </label>
+                                </div>
+                              </div> 
+                              @endif
                               @endif
                           @endforeach
                         @endforeach
