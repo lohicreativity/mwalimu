7c998b5793f3c8dcc6ab3189448bd072d67e8c8a
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-09-30 10:43:32 +0300

    comments

--- resources/views/dashboard/student/special-exams.blade.php
+++ resources/views/dashboard/student/special-exams.blade.php
@@ -80,7 +80,7 @@
                     @if(count($special_exam_requests) != 0)
                       @foreach($special_exam_requests as $exl)
                         @foreach($exl->exams as $ex)
-                          @if($assign->module_id != $ex->moduleAssignment->module->id)
+                          @if($assign->module_id !== $ex->moduleAssignment->module->id)
                             <div class="col-3">
                               <div class="checkbox">
                                 <label>
