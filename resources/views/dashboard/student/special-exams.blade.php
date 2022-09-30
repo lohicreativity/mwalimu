9e988d776ee8ab6a4c2b3ee30746bed7fba90491
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-09-30 11:21:47 +0300

    comments

--- resources/views/dashboard/student/special-exams.blade.php
+++ resources/views/dashboard/student/special-exams.blade.php
@@ -77,14 +77,7 @@
 
                 @foreach($module_assignments as $assign)
                   @if(sizeof($opted_module) == 0 && $assign->programModuleAssignment->category == 'OPTIONAL')
-                    <div class="col-3">
-                      <div class="checkbox">
-                        <label>
-                          {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
-                          {{ $assign->module->name }}
-                        </label>
-                      </div>
-                    </div>                    
+                                    
                   @elseif($assign->programModuleAssignment->category == 'OPTIONAL' && $opted_module[0]->module_id == $assign->module_id)
                     <div class="col-3">
                       <div class="checkbox">
