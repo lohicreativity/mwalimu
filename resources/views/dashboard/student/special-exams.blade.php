c622b6e8f667f973baf4efef37899921f350bee8
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-09-30 09:08:03 +0300

    comments

--- resources/views/dashboard/student/special-exams.blade.php
+++ resources/views/dashboard/student/special-exams.blade.php
@@ -96,7 +96,7 @@
                                       </label>
                                     </div>
                                   </div>  
-                                  @elseif($assign->module_id != $ex->moduleAssignment->module->id)
+                                  @else
                                           @if($assign->programModuleAssignment->category == 'OPTIONAL' && $opted_module[0]->module_id == $assign->module_id)
                                                 <div class="col-3">
                                                   <div class="checkbox">
