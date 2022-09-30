ebbd1dc7e9aa060d0d810a714beb2eede7dcddf3
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-09-30 10:02:21 +0300

    comments

--- resources/views/dashboard/student/special-exams.blade.php
+++ resources/views/dashboard/student/special-exams.blade.php
@@ -77,32 +77,15 @@
 
                 @foreach($module_assignments as $assign)
                   @if($assign->programModuleAssignment->category == 'OPTIONAL' && $opted_module[0]->module_id == $assign->module_id)
-                    @if(count($special_exam_requests) != 0)
-                      @foreach($special_exam_requests as $exl)
-                        @foreach($exl->exams as $ex)
-                          @if($assign->module_id == $ex->moduleAssignment->module->id)
-                            <div class="col-3">
-                              <div class="checkbox">
-                                <label>
-                                  {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id, true, array('disabled')) !!}
-                                  {{ $assign->module->name }}
-                                </label>
-                              </div>
-                            </div>
-                          @elseif($assign->module_id != $ex->moduleAssignment->module->id) 
-                            <div class="col-3">
-                              <div class="checkbox">
-                                <label>
-                                  {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id, true, array('disabled')) !!}
-                                  {{ $assign->module->name }}
-                                </label>
-                              </div>
-                            </div>
-                          @endif
-                        @endforeach
-                      @endforeach
-                    @endif                     
-                  @endif 
+                    <div class="col-3">
+                      <div class="checkbox">
+                        <label>
+                          {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id, true, array('disabled')) !!}
+                          {{ $assign->module->name }}
+                        </label>
+                      </div>
+                    </div>
+                  @endif
                 @endforeach
                 
                 </div>
