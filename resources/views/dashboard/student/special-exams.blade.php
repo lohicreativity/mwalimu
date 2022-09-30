17239b238324067bfbaaf17ff916ce43526d4ba2
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-09-30 11:01:55 +0300

    comments

--- resources/views/dashboard/student/special-exams.blade.php
+++ resources/views/dashboard/student/special-exams.blade.php
@@ -77,24 +77,23 @@
 
                 @foreach($module_assignments as $assign)
                   @if($assign->programModuleAssignment->category == 'OPTIONAL' && $opted_module[0]->module_id == $assign->module_id)
-                    @if(count($special_exam_requests) != 0)
-                      @foreach($special_exam_requests as $exl)
-                        @foreach($exl->exams as $ex)
-                          @if($assign->module_id == $ex->moduleAssignment->module->id)
-                          {{ (($assign->module_id == $ex->moduleAssignment->module->id) + 1) }}
-                            <div class="col-3">
-                              <div class="checkbox">
-                                <label>
-                                  {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
-                                  {{ $assign->module->name }}
-                                </label>
-                              </div>
-                            </div>                          
-                          @endif
-                        @endforeach
-                      @endforeach
-                      
-                    @endif
+                    <div class="col-3">
+                      <div class="checkbox">
+                        <label>
+                          {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
+                          {{ $assign->module->name }}
+                        </label>
+                      </div>
+                    </div> 
+                  @elseif(empty($opted_module))
+                    <div class="col-3">
+                      <div class="checkbox">
+                        <label>
+                          {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
+                          {{ $assign->module->name }}
+                        </label>
+                      </div>
+                    </div>
                   @elseif($assign->programModuleAssignment->category == 'COMPULSORY')
                     <div class="col-3">
                       <div class="checkbox">
