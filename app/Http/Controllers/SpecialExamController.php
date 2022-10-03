57b83f4901030d53e1e9d293fb4655f5840d9807
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-03 16:42:40 +0300

    comments

--- app/Http/Controllers/SpecialExamController.php
+++ app/Http/Controllers/SpecialExamController.php
@@ -90,7 +90,7 @@ class SpecialExamController extends Controller
         ];
     
 
-        return $data['special_exam_requests'];
+        // return $data['special_exam_requests'];
         return view('dashboard.student.special-exams',$data)->withTitle('Exam Postponement');
     }
 
