893af2c05882da8747b214a6cb7097fed432ce53
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-03 11:36:52 +0300

    comments

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -3061,7 +3061,7 @@ class ExaminationResultController extends Controller
               ->select('number_of_options')
               ->get();
 
-              return $num_options->pluck('number_of_options');
+              return $num_options->number_of_options;
 
          $data = [
          	'semesters'=>$semesters,
