31dbdd3f3bdc1d6a748706f6d905f7c3541052e9
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-04 12:11:23 +0300

    comments

--- resources/views/dashboard/student/special-exams.blade.php
+++ resources/views/dashboard/student/special-exams.blade.php
@@ -118,11 +118,40 @@
                                 </label>
                               </div>
                             </div>
-                             
+                             @elseif($ex->moduleAssignment->module->name != $assign->module->name)
+                             @if(sizeof($opted_module) == 0 && $assign->programModuleAssignment->category == 'OPTIONAL')
+                      <div class="col-3">
+                        <div class="checkbox">
+                          <label>
+                            {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
+                            {{ $assign->module->name }}
+                          </label>
+                        </div>
+                      </div>   
+                    @elseif($assign->programModuleAssignment->category == 'OPTIONAL' && $opted_module[0]->module_id == $assign->module_id)
+                      <div class="col-3">
+                        <div class="checkbox">
+                          <label>
+                            {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
+                            {{ $assign->module->name }}
+                          </label>
+                        </div>
+                      </div>
+                    @elseif($assign->programModuleAssignment->category == 'COMPULSORY')
+                      <div class="col-3">
+                        <div class="checkbox">
+                          <label>
+                            {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
+                            {{ $assign->module->name }}
+                          </label>
+                        </div>
+                      </div>
+                    @endif
                             @endif
                           @endforeach
                         @endforeach
                         @else
+
                         @if(sizeof($opted_module) == 0 && $assign->programModuleAssignment->category == 'OPTIONAL')
                       <div class="col-3">
                         <div class="checkbox">
@@ -151,6 +180,7 @@
                         </div>
                       </div>
                     @endif
+
                       @endif
                   @endforeach
 
