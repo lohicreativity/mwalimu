0732062e8c32c534d25736e4655dd7df7a6f3bd7
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-06 15:58:02 +0300

    comments

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -699,7 +699,7 @@ class ExaminationResultController extends Controller
 
 
                           $stud = Student::find($key);
-                        //   return $status.'   '.$status->id;
+                          return $key;
                           $stud->academic_status_id = $status->id;
                           $stud->save();
                         
