286820ceab264fb790ed3b240b94e221f50a2f4c
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-10 10:30:09 +0300

    comments

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -1517,7 +1517,7 @@ class ExaminationResultController extends Controller
             $student_buffer[$student->id]['opt_prog'] = 0;
             $student_buffer[$student->id]['opt_prog_status'] = true;
 
-            return $results;
+            // return $results;
 
             foreach($results as $key=>$result){          
               
@@ -1542,7 +1542,7 @@ class ExaminationResultController extends Controller
                         $processed_result = ExaminationResult::find($result->retakeHistory->retakeHistory->retakableResults[0]->id);
                     }elseif($result->carryHistory && isset($result->carryHistory->carrableResults[0])){
                         $processed_result = ExaminationResult::find($result->carryHistory->carrableResults[0]->id);
-
+                        return $processed_result;
                     }else{
                         $processed_result = ExaminationResult::find($result->id);
                     }
