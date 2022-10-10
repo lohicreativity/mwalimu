986c57d6c96ed7ba3de48cf0d720b7b8d82c033e
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-10 11:23:49 +0300

    comments

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -1612,7 +1612,7 @@ class ExaminationResultController extends Controller
                           if(Util::stripSpacesUpper($assignment->module->ntaLevel->name) == Util::stripSpacesUpper('NTA Level 7')){
                                   if($assignment->programModuleAssignment->year_of_study == 1){
                                        if($processed_result->retakable_id != null){
-                                          return $assignment->programModuleAssignment;
+                                          return $assignment->programModuleAssignment.'<br><br><br><br>'.$processed_result;
                                           //   return $assignment->programModuleAssignment->module_id.'<br><br>'. $processed_result->carryHistory->module_assignment_id;
                                           // if ($assignment->programModuleAssignment->id == $processed_result->carryHistory->module) {
                                           //    # code...
