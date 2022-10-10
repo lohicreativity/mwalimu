4c5f6f4fe39e26322367c1d9a4132dc92b8c9541
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-10 09:19:17 +0300

    comments

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -1535,6 +1535,8 @@ class ExaminationResultController extends Controller
                    }
 
                    $student_buffer[$student->id]['total_credit'] = $student_buffer[$student->id]['opt_credit'] + $total_credit;
+                   
+                  //   return $result->carryHistory[0];
 
                     if($result->retakeHistory && isset($result->retakeHistory->retakeHistory->retakableResults[0])){
                         $processed_result = ExaminationResult::find($result->retakeHistory->retakeHistory->retakableResults[0]->id);
