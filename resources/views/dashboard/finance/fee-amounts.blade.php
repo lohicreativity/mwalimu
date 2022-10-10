efe999dd3bf95970a92886842c463d8513093161
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-10 14:18:50 +0300

    comments

--- resources/views/dashboard/finance/fee-amounts.blade.php
+++ resources/views/dashboard/finance/fee-amounts.blade.php
@@ -119,7 +119,7 @@
                   </tr>
                   </thead>
                   <tbody>
-                  @foreach($amounts as $amount)
+                  @foreach($amounts->sortBy(['created_at', 'desc']) as $amount)
                   <tr>
                     <td>{{ $amount->feeItem->name }}</td>
                     <td>{{ number_format($amount->amount_in_tzs,2) }}</td>
