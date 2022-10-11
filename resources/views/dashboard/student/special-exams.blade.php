f6d7b1a4b66aa7150940add327b10d829e4ef5b6
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-11 13:00:03 +0300

    comments

--- resources/views/dashboard/student/special-exams.blade.php
+++ resources/views/dashboard/student/special-exams.blade.php
@@ -126,7 +126,38 @@
                         @endforeach
                       @endif 
                       @foreach($check_special_exam as $cse)
-                      {{ $cse }}
+                        @if($cse == $assign->module->name)
+                        @break
+                        @else 
+                        @if(sizeof($opted_module) == 0 && $assign->programModuleAssignment->category == 'OPTIONAL')
+                        <div class="col-3">
+                          <div class="checkbox">
+                            <label>
+                              {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
+                              {{ $assign->module->name }}
+                            </label>
+                          </div>
+                        </div>   
+                        @elseif($assign->programModuleAssignment->category == 'OPTIONAL' && $opted_module[0]->module_id == $assign->module_id)
+                        <div class="col-3">
+                          <div class="checkbox">
+                            <label>
+                              {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
+                              {{ $assign->module->name }}
+                            </label>
+                          </div>
+                        </div>
+                        @elseif($assign->programModuleAssignment->category == 'COMPULSORY')
+                        <div class="col-3">
+                          <div class="checkbox">
+                            <label>
+                              {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
+                              {{ $assign->module->name }}
+                            </label>
+                          </div>
+                        </div>
+                      @endif 
+                        @endif
                       @endforeach
 
                      
