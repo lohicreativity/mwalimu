8c568d07770335582473aa04508e60e25ce41f2c
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-11 10:26:51 +0300

    comments

--- resources/views/dashboard/student/special-exams.blade.php
+++ resources/views/dashboard/student/special-exams.blade.php
@@ -123,35 +123,39 @@
                         @endforeach
                       @endif 
 
-
-                      @if(sizeof($opted_module) == 0 && $assign->programModuleAssignment->category == 'OPTIONAL')
-                        <div class="col-3">
-                          <div class="checkbox">
-                            <label>
-                              {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
-                              {{ $assign->module->name }}
-                            </label>
-                          </div>
-                        </div>   
-                        @elseif($assign->programModuleAssignment->category == 'OPTIONAL' && $opted_module[0]->module_id == $assign->module_id)
-                        <div class="col-3">
-                          <div class="checkbox">
-                            <label>
-                              {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
-                              {{ $assign->module->name }}
-                            </label>
-                          </div>
-                        </div>
-                        @elseif($assign->programModuleAssignment->category == 'COMPULSORY')
-                        <div class="col-3">
-                          <div class="checkbox">
-                            <label>
-                              {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
-                              {{ $assign->module->name }}
-                            </label>
-                          </div>
-                        </div>
-                      @endif                                  
+                     @foreach($special_exam_requests as $exam) 
+                      @foreach($exam->exams as $ex)
+                          @if($ex->moduleAssignment->module->name != $assign->module->name && sizeof($opted_module) == 0 && $assign->programModuleAssignment->category == 'OPTIONAL')
+                            <div class="col-3">
+                              <div class="checkbox">
+                                <label>
+                                  {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
+                                  {{ $assign->module->name }}
+                                </label>
+                              </div>
+                            </div>   
+                            @elseif($ex->moduleAssignment->module->name != $assign->module->name && $assign->programModuleAssignment->category == 'OPTIONAL' && $opted_module[0]->module_id == $assign->module_id)
+                            <div class="col-3">
+                              <div class="checkbox">
+                                <label>
+                                  {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
+                                  {{ $assign->module->name }}
+                                </label>
+                              </div>
+                            </div>
+                            @elseif($ex->moduleAssignment->module->name != $assign->module->name && $assign->programModuleAssignment->category == 'COMPULSORY')
+                            <div class="col-3">
+                              <div class="checkbox">
+                                <label>
+                                  {!! Form::checkbox('mod_assign_'.$assign->id,$assign->id) !!}
+                                  {{ $assign->module->name }}
+                                </label>
+                              </div>
+                            </div>
+                          @endif
+                      @endforeach
+                     @endforeach
+                                                       
                   @endforeach
                 @endif
                 
