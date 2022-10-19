d5f9234652f7b1715ff6b3d9d8b9df0f34797bac
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-19 15:13:21 +0300

    comments

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -128,8 +128,7 @@ class ModuleAssignmentController extends Controller
            'semesters'=>Semester::all(),
            'assignments'=>$staff? ModuleAssignment::whereHas('studyAcademicYear.moduleAssignments',function($query) use ($request){
                   $query->where('id',$request->get('study_academic_year_id'))
-                  ->orderBy('year_of_study', 'asc')
-                  ->orderBy('semester_id', 'asc');
+                  ->orderBy('year_of_study', 'asc');
              })->with(['studyAcademicYear.academicYear','module','programModuleAssignment.campusProgram.program','programModuleAssignment.campusProgram.campus','programModuleAssignment.semester'])
              ->where('staff_id',$staff->id)
              ->where('confirmed',1)
