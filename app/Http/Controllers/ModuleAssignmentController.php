bdd11050fd06ae960a301f63ce4286306f7b31fa
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-20 11:17:00 +0300

    comments

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -121,7 +121,6 @@ class ModuleAssignmentController extends Controller
                    $query->where('status','ACTIVE');
             }])->where('user_id',Auth::user()->id)->first();
 
-            return $request->get('study_academic_year_id');
         $data = [
            'study_academic_years'=>StudyAcademicYear::all(),
            'study_academic_year'=>$request->has('study_academic_year_id')? StudyAcademicYear::with('academicYear')->find($request->get('study_academic_year_id')) : null,
