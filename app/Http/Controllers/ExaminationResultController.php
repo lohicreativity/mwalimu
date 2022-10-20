05a54f89bf0c6ae13483724e71e1685876ae7c96
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-20 13:07:31 +0300

    comments

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -286,6 +286,7 @@ class ExaminationResultController extends Controller
                   ->first();
     
                   if(!$grading_policy){
+                     $assignment->module->name;
                      return redirect()->back()->with('error','Some programmes NTA level are missing grading policies');
                   }
                   
