ad8e721900099d0d72523e4c413ddfb75cf97256
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-20 09:15:11 +0300

    comments

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -162,7 +162,6 @@ class RegistrationController extends Controller
 	                        }
 						}else{
                             if($tuition_fee_paid < (0.6*($tuition_fee_invoice->amount + $fee_diff))){
-                                return $tuition_fee_paid."<br><br>".$tuition_fee_invoice->amount."<br><br>".$fee_diff;
 	                           return redirect()->back()->with('error','You cannot continue with registration because you have not paid sufficient tuition fee');
 	                        }
 						}
@@ -196,6 +195,7 @@ class RegistrationController extends Controller
 	                   return redirect()->back()->with('error','You cannot continue with registration because you have not paid sufficient tuition fee');
 	                }
                 }else{
+                    return $tuition_fee_paid."<br><br>".$tuition_fee_invoice->amount."<br><br>".$fee_diff."first";
                 	if($tuition_fee_paid < (0.6*$tuition_fee_invoice->amount)){
 	                   return redirect()->back()->with('error','You cannot continue with registration because you have not paid sufficient tuition fee');
 	                }
