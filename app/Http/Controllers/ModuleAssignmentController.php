f9748b3023b76e2bbf0ff62ba89bf06532dfbbd4
Ak47 <49089053+SalimShady23@users.noreply.github.com>
2022-10-20 11:58:52 +0300

    comments

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -137,14 +137,6 @@ class ModuleAssignmentController extends Controller
              ->latest()
              ->paginate(20) : [],
         ];
-
-    //     return ModuleAssignment::whereHas('studyAcademicYear.moduleAssignments',function($query) use ($request){
-    //         $query->where('study_academic_years.id',$request->get('study_academic_year_id'))
-    //         ->orderBy('year_of_study', 'desc')
-    //         ->orderBy('semester_id', 'asc');
-    //    })->where('staff_id',$staff->id)
-    //    ->where('confirmed',1)->get();
-
         return view('dashboard.academic.staff-assigned-modules',$data)->withTitle('Staff Assigned Modules');
     }
 
