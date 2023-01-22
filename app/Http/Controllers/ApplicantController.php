62b2fe8fcb83d715bb969857dd84267bc536ddc1
Amani Ghachocha <amanighachocha@gmail.com>
2023-01-22 13:31:25 +0000

    comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -665,7 +665,6 @@ class ApplicantController extends Controller
                                     $a_level_subsidiary_pass_count += 1;
                                  } */
 								 // lupi changed to properly count subsidiary points | tested
-								 
 								 if(unserialize($program->entryRequirements[0]->advance_must_subjects) != ''){
                                     if(unserialize($program->entryRequirements[0]->other_advance_must_subjects) != ''){
                                        if(in_array($result->subject_name, unserialize($program->entryRequirements[0]->advance_must_subjects))){
@@ -695,12 +694,7 @@ class ApplicantController extends Controller
                          }
                          
                        }
-					   if($o_level_pass_count >= $program->entryRequirements[0]->pass_subjects && ($a_level_subsidiary_pass_count >= 1 && $a_level_principle_pass_count >= 1)){
-                         $programs[] = $program;
-                       }
-                    /*    if(unserialize($program->entryRequirements[0]->advance_must_subjects) != ''){		// lupi changed to check for advance_must_subjects
-                       // if(unserialize($program->entryRequirements[0]->must_subjects) != ''){ original
-					   // lupi changed the code below to ignore checks on form IV must subjects
+                    /*    if(unserialize($program->entryRequirements[0]->must_subjects) != ''){		// original
                        if(($o_level_pass_count+$o_level_must_pass_count) >= $program->entryRequirements[0]->pass_subjects && ($a_level_subsidiary_pass_count >= 1 && $a_level_principle_pass_count >= 1)){
                        // if(($o_level_pass_count+$o_level_must_pass_count) >= $program->entryRequirements[0]->pass_subjects && ($a_level_subsidiary_pass_count >= 1 && $a_level_principle_pass_count >= 1) && $o_level_must_pass_count >= count(unserialize($program->entryRequirements[0]->must_subjects))){		// original
                            $programs[] = $program;
@@ -714,6 +708,11 @@ class ApplicantController extends Controller
                              $programs[] = $program;
                            }
                         } */
+						// lupi changed the code below to ignore checks on form IV must subjects
+					   if($o_level_pass_count >= $program->entryRequirements[0]->pass_subjects && ($a_level_subsidiary_pass_count >= 1 && $a_level_principle_pass_count >= 1)){
+                         $programs[] = $program;
+                       }
+					   
                        $has_btc = false;
                       
 
