c0e2f98233382088055d09414699e60919085e2d
Amani Ghachocha <amanighachocha@gmail.com>
2023-01-22 12:42:10 +0000

    comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -673,11 +673,10 @@ class ApplicantController extends Controller
                        if(unserialize($program->entryRequirements[0]->advance_must_subjects) != ''){		// lupi changed to check for advance_must_subjects
                        // if(unserialize($program->entryRequirements[0]->must_subjects) != ''){ original
 					   // lupi changed the code below to ignore checks on form IV must subjects
-
+					   return $o_level_pass_count+$o_level_must_pass_count." ".$a_level_subsidiary_pass_count." ".$a_level_principle_pass_count;
                        if(($o_level_pass_count+$o_level_must_pass_count) >= $program->entryRequirements[0]->pass_subjects && ($a_level_subsidiary_pass_count >= 1 && $a_level_principle_pass_count >= 1)){
                        // if(($o_level_pass_count+$o_level_must_pass_count) >= $program->entryRequirements[0]->pass_subjects && ($a_level_subsidiary_pass_count >= 1 && $a_level_principle_pass_count >= 1) && $o_level_must_pass_count >= count(unserialize($program->entryRequirements[0]->must_subjects))){		// original
-                           					   return $o_level_pass_count+$o_level_must_pass_count;
-						   $programs[] = $program;
+                           $programs[] = $program;
                         }
                         }else{
 					
