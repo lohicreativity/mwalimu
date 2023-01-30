1f596a37c41e5ea2c8880b2277cfc3997d8ced26
salimshady23 <slimshadysaid@gmail.com>
2023-01-30 09:44:28 +0300

    comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -789,6 +789,9 @@ class ApplicantController extends Controller
                       
 
                        if(unserialize($program->entryRequirements[0]->equivalent_majors) != ''){
+
+                           return 123;
+
                            foreach(unserialize($program->entryRequirements[0]->equivalent_majors) as $sub){
                                 foreach($applicant->nacteResultDetails as $det){
                                    if(str_contains(strtolower($det->programme),strtolower($sub)) && str_contains(strtolower($det->programme),'basic')){
