bf68c8a1a911381bece550511938be7ff00d0acc
salimshady23 <slimshadysaid@gmail.com>
2023-01-30 16:04:50 +0300

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -758,19 +758,9 @@ class ApplicationController extends Controller
 
             $applicant = Applicant::find($request->get('applicant_id'));
 
-            // $applicant = User::find()->applicants()->with(['selections.campusProgram.program','selections'=>function($query){
-            //     $query->orderBy('order','asc');
-            // },'nectaResultDetails'=>function($query){
-            //      $query->where('verified',1);
-            // },'nacteResultDetails'=>function($query){
-            //      $query->where('verified',1);
-            // },'outResultDetails'=>function($query){
-            //      $query->where('verified',1);
-            // },'selections.campusProgram.campus','nectaResultDetails.results','nacteResultDetails.results','outResultDetails.results','programLevel','applicationWindow'])->where('campus_id',session('applicant_campus_id'))->first();
-
-            return $applicant;
-
             $window = $applicant->applicationWindow;
+
+            return $window;
 
             $campus_programs = $window? $window->campusPrograms()->whereHas('program',function($query) use($applicant){
                     $query->where('award_id',$applicant->program_level_id);
