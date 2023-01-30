adc6e350074eb7fc47c60f0852be80a30d42ffa7
salimshady23 <slimshadysaid@gmail.com>
2023-01-30 16:25:07 +0300

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -766,7 +766,7 @@ class ApplicationController extends Controller
                     $query->where('application_window_id',$window->id);
             }])->where('campus_id',session('applicant_campus_id'))->get() : [];
 
-            return $campus_programs;
+            return $request->get('campus_program_id');
 
             // foreach ($campus_programs as $program) {
                 
