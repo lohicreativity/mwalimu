97bc827bb98a7b951c9159b91daf5ddfb876c067
salimshady23 <slimshadysaid@gmail.com>
2023-01-30 11:55:05 +0300

    comments

--- app/Domain/Academic/Actions/ProgramAction.php
+++ app/Domain/Academic/Actions/ProgramAction.php
@@ -44,6 +44,9 @@ class ProgramAction implements ProgramInterface{
 	}
 
 	public function update(Request $request){
+
+                return $request->get('program_id');
+                
 		        $program = Program::find($request->get('program_id'));
                 $program->name = $request->get('name');
                 $program->code = $request->get('code');
--- resources/views/dashboard/academic/programs.blade.php
+++ resources/views/dashboard/academic/programs.blade.php
@@ -206,12 +206,14 @@
                   <tr>
                     <td>@if(count($program->campusPrograms) != 0) {{ $program->campusPrograms[0]->regulator_code }} @endif</td>
                     <td>{{ $program->code }}</td>
-					<td>@foreach($program->departments as $dept) <p>{{ $dept->name }}</p> @endforeach</td>
-					<td>@foreach($program->departments as $dept) 
-					         @foreach($dept->staffs as $stf) 
-							    @if($stf->user->hasRole('hod') && $stf->campus_id == $staff->campus_id) <p class="ss-font-xs ss-no-margin">{{ $stf->title}} {{ $stf->first_name }} {{ $stf->surname }}</p> @endif 
-							 @endforeach 
-					    @endforeach</td>
+					          <td>@foreach($program->departments as $dept) <p>{{ $dept->name }}</p> @endforeach</td>
+					          <td>
+                      @foreach($program->departments as $dept) 
+					              @foreach($dept->staffs as $stf) 
+							            @if($stf->user->hasRole('hod') && $stf->campus_id == $staff->campus_id) <p class="ss-font-xs ss-no-margin">{{ $stf->title}} {{ $stf->first_name }} {{ $stf->surname }}</p> @endif 
+							          @endforeach 
+					            @endforeach
+                    </td>
                     <td>
                       @can('edit-programme')
                       <a class="btn btn-info btn-sm" href="#" data-toggle="modal" data-target="#ss-edit-program-{{ $program->id }}">
