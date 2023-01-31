c10a32204d30f18ea0f6c0cefd891333922bf49e
salimshady23 <slimshadysaid@gmail.com>
2023-01-31 11:46:53 +0300

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -857,7 +857,7 @@ class ApplicationController extends Controller
                 if ($program->id == $selection->campus_program_id) {
 
                     if (unserialize($program->entryRequirements[0]->equivalent_must_subjects) != '') {
-                            $applicant->avn_no_results = 1;
+                            $applicant->avn_no_results = null;
                             $applicant->save();
                     }
                 }
