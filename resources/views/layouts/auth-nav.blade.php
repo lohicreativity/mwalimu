22f8d766e36fb5c159770b5ab64e3e2c96db5d13
salimshady23 <slimshadysaid@gmail.com>
2023-01-31 15:11:26 +0300

    comments

--- resources/views/layouts/auth-nav.blade.php
+++ resources/views/layouts/auth-nav.blade.php
@@ -45,7 +45,7 @@
               <li class="nav-item">
                 <a href="{{ url('application/upload-avn-documents') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Upload Documents @if($applicant->teacher_diploma_certificate == 1 || $applicant->documents_complete_status == 1 || $applicant->status == "SUBMITTED") <i class="fa fa-check"></i> @endif</p>
+                  <p>Upload Documents @if($applicant->teacher_diploma_certificate == 1 || $applicant->documents_complete_status == 1 || $applicant->submission_complete_status == 1) <i class="fa fa-check"></i> @endif</p>
                 </a>
               </li>
               @endif
