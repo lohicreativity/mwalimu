39d8bac31245bc31a349289516dfd535ac7073b8
salimshady23 <slimshadysaid@gmail.com>
2023-02-01 10:43:57 +0300

    comments

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -32,7 +32,7 @@ class DepartmentController extends Controller
            ->paginate(20)
     	];
 
-      return $data['departments']['campuses'];
+      return $data['departments']['campuses']->id;
 
     	// return view('dashboard.academic.departments',$data)->withTitle('Departments');
     }
