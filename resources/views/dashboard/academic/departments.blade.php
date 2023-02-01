eeddda755561509fd0b55c3bbad1c7205692ae7b
salimshady23 <slimshadysaid@gmail.com>
2023-02-01 19:31:50 +0300

    comments

--- resources/views/dashboard/academic/departments.blade.php
+++ resources/views/dashboard/academic/departments.blade.php
@@ -229,9 +229,9 @@
                                       {!! Form::label('','Parent') !!}
                                       <select name="parent_id" class="form-control" id="parents" required>
                                          <option value="">Select Parent</option>
-                                         @foreach($all_departments as $dept)
+                                         <!-- @foreach($all_departments as $dept)
                                          <option value="{{ $dept->id }}" @if($dept->id == $department->parent_id) selected="selected" @endif>{{ $dept->name }}</option>
-                                         @endforeach
+                                         @endforeach -->
                                       </select>
                                     </div>
                                     <div class="form-group col-4">
