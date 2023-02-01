8ed4af3c6ab41ea991a3c553a2a3f8d5d7412312
salimshady23 <slimshadysaid@gmail.com>
2023-02-01 15:52:30 +0300

    comments

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -25,7 +25,7 @@ class DepartmentController extends Controller
       if (Auth::user()->hasRole('administrator')) {
          $departments = Department::with('unitCategory','campuses')->paginate(20);
       } else if (Auth::user()->hasRole('admission-officer')) {
-         $departments = Department::with('campuses')->get();
+         $departments = Department::whereHas('campuses')->with('campuses')->get();
 
          return $departments;
       }
