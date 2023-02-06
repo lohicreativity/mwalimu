e3d8c5aca9c2805086ce6e0a409630a70e4c8444
salimshady23 <slimshadysaid@gmail.com>
2023-02-06 19:33:35 +0300

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2883,7 +2883,7 @@ class ApplicationController extends Controller
          $data = [
             'staff' => $staff,
             'campus_id' => $staff->campus_id,
-            'attachments'=>AdmissionAttachment::with('campus')->get(),
+            'attachments'=>AdmissionAttachment::with('campus')->paginate(20),
             'application_windows'=>ApplicationWindow::where('campus_id',$staff->campus_id)->get(),
             'application_window'=>ApplicationWindow::find($request->get('application_window_id')),
             'awards'=>Award::all(),
