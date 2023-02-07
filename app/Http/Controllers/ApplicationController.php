a5e2730f1f6d0082ff5c806a25374d72de1c8117
salimshady23 <slimshadysaid@gmail.com>
2023-02-07 09:12:51 +0300

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2990,19 +2990,19 @@ class ApplicationController extends Controller
 
         $campus_id = $staff->campus_id;
 
-        $applicants = Applicant::where('campus_id', $campus_id)
-        ->where('status', 'SELECTED')
-        ->whereHas('selections',function($query) use($request){
-            $query->where('status','SELECTED');
-        })
-        ->get();
+        // $applicants = Applicant::where('campus_id', $campus_id)
+        // ->where('status', 'SELECTED')
+        // ->whereHas('selections',function($query) use($request){
+        //     $query->where('status','SELECTED');
+        // })
+        // ->get();
         
 
-        // $applicants = Applicant::whereHas('selections',function($query) use($request){
-        //      $query->where('status','SELECTED');
-        // })->with(['nextOfKin','intake','selections'=>function($query){
-        //      $query->where('status','SELECTED');
-        // },'selections.campusProgram.program','applicationWindow','country','selections.campusProgram.campus'])->where('program_level_id',$request->get('program_level_id'))->where('status','SELECTED')->where('application_window_id',$request->get('application_window_id'))->get();  
+        $applicants = Applicant::whereHas('selections',function($query) use($request){
+             $query->where('status','SELECTED');
+        })->with(['nextOfKin','intake','selections'=>function($query){
+             $query->where('status','SELECTED');
+        },'selections.campusProgram.program','applicationWindow','country','selections.campusProgram.campus'])->where('program_level_id',$request->get('program_level_id'))->where('status','SELECTED')->where('campus_id', $campus_id)->where('application_window_id',$request->get('application_window_id'))->get();  
         
 
         return $applicants;
