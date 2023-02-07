3ec6193c77328b78c5ba3b22b29bda9657d46ac5
salimshady23 <slimshadysaid@gmail.com>
2023-02-07 11:51:26 +0300

    comments

--- resources/views/dashboard/application/edit-basic-information.blade.php
+++ resources/views/dashboard/application/edit-basic-information.blade.php
@@ -115,6 +115,7 @@
                          'class'=>'form-control',
                          'required'=>true
                       ];
+                      
                   }else{
                       $first_name = [
                          'placeholder'=>'First name',
@@ -141,11 +142,7 @@
                          'readonly'=>App\Domain\Application\Models\Applicant::hasRequestedControlNumber($applicant)? true : null,
                          'required'=>true
                       ];
-                      $gender = [
-                         'class'=>'form-control',
-                         'readonly'=>App\Domain\Application\Models\Applicant::hasRequestedControlNumber($applicant)? true : null,
-                         'required'=>true
-                      ];
+                      
                   }
 
                   $street = [
@@ -232,7 +229,7 @@
                    <div class="row">
                     <div class="form-group col-6">
                        {!! Form::label('','Gender') !!}
-                       <select name="gender" class="form-control" required>
+                       <select name="gender" class="form-control" @if($applicant->status == 'ADMITTED') readonly @else @if(App\Domain\Application\Models\Applicant::hasConfirmedResults($applicant)) readonly @endif  @endif  required>
                          <option value="">Select Gender</option>
                          <option value="M" @if($applicant->gender == 'M') selected="selected" @else @if($applicant->status == 'ADMITTED') disabled="disabled" @endif @endif>Male</option>
                          <option value="F" @if($applicant->gender == 'F') selected="selected" @else @if($applicant->status == 'ADMITTED') disabled="disabled" @endif @endif>Female</option>
