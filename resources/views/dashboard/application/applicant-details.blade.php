99dbc11be222834e06ed7c6410ed1f19723c9409
salimshady23 <slimshadysaid@gmail.com>
2023-02-07 17:28:16 +0300

    comments

--- resources/views/dashboard/application/applicant-details.blade.php
+++ resources/views/dashboard/application/applicant-details.blade.php
@@ -21,12 +21,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>{{ __('Edit Applicant Details') }}</h1>
+            <h1>{{ __('Applicant Details') }}</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">{{ __('Edit Applicant Search') }}</li>
+              <li class="breadcrumb-item active">{{ __('Applicant Search') }}</li>
             </ol>
           </div>
         </div>
