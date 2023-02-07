dc44d7aa24cca61008e7db728d58923ee6a7eaac
salimshady23 <slimshadysaid@gmail.com>
2023-02-07 09:28:04 +0300

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -3003,11 +3003,8 @@ class ApplicationController extends Controller
                 $query->where('status','SELECTED');
            },'selections.campusProgram.program','applicationWindow','country','selections.campusProgram.campus'])->where('program_level_id',$request->get('program_level_id'))->where('status','SELECTED')->where('campus_id', $campus_id)->where('application_window_id',$request->get('application_window_id'))->get();  
         }
-
-        return $applicants;
-        
                 
-   
+
    	   // Applicant::whereHas('intake.applicationWindows',function($query) use($request){
         //      $query->where('id',$request->application_window_id);
         // })->whereHas('selections',function($query) use($request){
