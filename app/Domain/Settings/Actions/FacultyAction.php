1c1e1dc66a961b0cc0aaad36b91f4a63f206729f
salimshady23 <slimshadysaid@gmail.com>
2023-02-09 16:24:49 +0300

    commments

--- app/Domain/Settings/Actions/FacultyAction.php
+++ app/Domain/Settings/Actions/FacultyAction.php
@@ -5,7 +5,9 @@ namespace App\Domain\Settings\Actions;
 use Illuminate\Http\Request;
 use App\Domain\Settings\Models\Faculty;
 use App\Domain\Settings\Models\CampusFaculty;
+use App\Models\User;
 use App\Domain\Settings\Repositories\Interfaces\FacultyInterface;
+use Auth;
 
 class FacultyAction implements FacultyInterface{
 	
@@ -14,7 +16,12 @@ class FacultyAction implements FacultyInterface{
                 $faculty = new Faculty;
                 $faculty->name          = $request->get('name');
                 $faculty->abbreviation  = $request->get('abbreviation');
-                $faculty->campus_id     = $request->get('campuses');
+                if (Auth::user()->hasRole('administrator')) {
+                        $faculty->campus_id     = $request->get('campuses');
+                } else if (Auth::user()->hasRole('admission-officer')) {
+                        $staff = User::find(Auth::user()->id)->staff;
+                        $faculty->campus_id = $staff->campus_id;      
+                }
                 $faculty->save();
 
                 $faculty->campuses()->sync($request->get('campuses'));
