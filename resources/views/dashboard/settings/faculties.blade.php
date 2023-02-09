c7fa2c2ec9be2504d2a05a71b2b1a37bfc08a1da
salimshady23 <slimshadysaid@gmail.com>
2023-02-09 13:59:37 +0300

    commments

--- resources/views/dashboard/settings/faculties.blade.php
+++ resources/views/dashboard/settings/faculties.blade.php
@@ -115,6 +115,7 @@
                                             </a>
                                         </td>
                                     <tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
