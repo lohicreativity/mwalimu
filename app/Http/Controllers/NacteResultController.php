c3bda8e524917d820c7d0daf748755a65535ca65
salimshady23 <slimshadysaid@gmail.com>
2023-02-13 13:36:53 +0300

    comments

--- app/Http/Controllers/NacteResultController.php
+++ app/Http/Controllers/NacteResultController.php
@@ -56,7 +56,6 @@ class NacteResultController extends Controller
 
     public function confirmNacteRegNumber(Request $request)
     {
-        // $applicant = Applicant::find($request->get('applicant_id'));
         // $applicant->nacte_reg_no = $request->get('nacte_reg_no');
         // if(NectaResultDetail::where('applicant_id',$applicant->id)->where('verified',1)->count() != 0){
         //    $applicant->results_complete_status = 1;
@@ -64,6 +63,7 @@ class NacteResultController extends Controller
         // $applicant->save();
 
         $nacte_detail = NacteResultDetail::find($request->get('nacte_result_detail_id'));
+        $applicant = Applicant::find($request->get('applicant_id'));
 
         if($applicant->nacte_reg_no != $nacte_detail->registration_number){
             if(strtoupper($applicant->first_name) != strtoupper($nacte_detail->first_name) || strtoupper($applicant->surname) != strtoupper($nacte_detail->last_name)){
