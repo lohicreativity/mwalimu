8d93333945242192486b4f941b29749c383ad28b
salimshady23 <slimshadysaid@gmail.com>
2023-02-13 14:03:41 +0300

    comments

--- app/Http/Controllers/NacteResultController.php
+++ app/Http/Controllers/NacteResultController.php
@@ -62,10 +62,11 @@ class NacteResultController extends Controller
         $applicant = Applicant::find($request->get('applicant_id'));
 
         if($applicant->nacte_reg_no != $nacte_detail->registration_number){
+
             if(strtoupper($applicant->first_name) != strtoupper($nacte_detail->first_name) || strtoupper($applicant->surname) != strtoupper($nacte_detail->last_name)){
-                return $request->get('necta_result_detail_id');
-                // return redirect()->to('application/nullify-nacte-reg-results?detail_id='.$request->get('necta_result_detail_id'));
+                return redirect()->to('application/nullify-nacte-reg-results?detail_id='.$request->get('nacte_result_detail_id'));
             }
+
         } else {
 
             $applicant->nacte_reg_no = $request->get('nacte_reg_no');
