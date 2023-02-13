b053a4db744291e50bce49adc051dbce5995bb20
salimshady23 <slimshadysaid@gmail.com>
2023-02-13 10:47:18 +0300

    comments

--- resources/views/dashboard/application/request-results.blade.php
+++ resources/views/dashboard/application/request-results.blade.php
@@ -240,7 +240,7 @@
                   
                   <div class="row">
                   <div class="form-group col-4">
-                    {!! Form::label('','AVN') !!}
+                    {!! Form::label('','AVN123') !!}
                     {!! Form::text('avn',null,$avn) !!}
                   </div>
                   <div class="form-group col-4">
