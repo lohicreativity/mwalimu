b62a0914708ae3ab8c8ccb553d6d3e74b72aad05
salimshady23 <slimshadysaid@gmail.com>
2023-02-13 13:57:44 +0300

    comments

--- app/Http/Controllers/NacteResultController.php
+++ app/Http/Controllers/NacteResultController.php
@@ -61,9 +61,6 @@ class NacteResultController extends Controller
         $nacte_detail = NacteResultDetail::find($request->get('nacte_result_detail_id'));
         $applicant = Applicant::find($request->get('applicant_id'));
 
-        return $request->get('nacte_reg_no');
-
-
         if($applicant->nacte_reg_no != $nacte_detail->registration_number){
             if(strtoupper($applicant->first_name) != strtoupper($nacte_detail->first_name) || strtoupper($applicant->surname) != strtoupper($nacte_detail->last_name)){
                 return redirect()->to('application/nullify-nacte-reg-results?detail_id='.$request->get('necta_result_detail_id'));
@@ -92,8 +89,9 @@ class NacteResultController extends Controller
     public function nullifyNacteReg(Request $request)
     {
         $detail = NacteResultDetail::find($request->get('detail_id'));
-        $detail->delete();
-        return redirect()->back()->with('error','NECTA results names do not match your application names');
+        return $detail;
+        // $detail->delete();
+        // return redirect()->back()->with('error','NECTA results names do not match your application names');
     }
 
     /**
