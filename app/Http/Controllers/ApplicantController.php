908b951ff1db59a5dff56bc1c80387f2f6e001e7
salimshady23 <slimshadysaid@gmail.com>
2023-02-15 11:02:06 +0300

    comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -263,7 +263,8 @@ class ApplicantController extends Controller
                      return redirect()->to('application/submission')->with('error','Application window already closed');
                  }
                  if($applicant->multiple_admissions !== null && $applicant->status == 'SELECTED'){
-                     return redirect()->to('application/basic-information')->with('error','Application window already closed');
+                     return view('dashboard.application.edit-basic-information');
+                     // return redirect()->to('application/basic-information')->with('error','Application window already closed');
                   //   return redirect()->to('application/admission-confirmation')->with('error','Application window already closed');
                  }
             }
