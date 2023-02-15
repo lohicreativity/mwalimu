c9f39d58084461d91b564823cc6e95c5af6b38d9
salimshady23 <slimshadysaid@gmail.com>
2023-02-15 11:38:44 +0300

    comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -262,7 +262,7 @@ class ApplicantController extends Controller
         }])
         ->where('campus_id',session('applicant_campus_id'))->first();
 
-        return $applicant->selections[0];
+        return $applicant->selections[0]->campusProgram->program->name;
 
         if($applicant->is_tamisemi !== 1 && $applicant->is_transfered != 1){
             if(!ApplicationWindow::where('campus_id',session('applicant_campus_id'))->where('begin_date','<=',now()->format('Y-m-d'))->where('end_date','>=',now()->format('Y-m-d'))->where('status','ACTIVE')->first()){
