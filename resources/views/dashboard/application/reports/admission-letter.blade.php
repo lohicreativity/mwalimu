56d0923963813906794d873733ce43a98497220b
salimshady23 <slimshadysaid@gmail.com>
2023-02-15 16:37:07 +0300

    comments

--- resources/views/dashboard/application/reports/admission-letter.blade.php
+++ resources/views/dashboard/application/reports/admission-letter.blade.php
@@ -216,7 +216,8 @@
      .footer-text {
       font-size: 14px;
       border-top: 2px solid #000;
-      margin-bottom: 30px;
+      position: relative;
+      bottom: 30px;
      }
      p, li{
         text-align: justify;
