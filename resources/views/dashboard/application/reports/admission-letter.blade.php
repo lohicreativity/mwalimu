67856659d45a0de7b275f1e7907ac4671c841420
salimshady23 <slimshadysaid@gmail.com>
2023-02-15 15:47:56 +0300

    comments

--- resources/views/dashboard/application/reports/admission-letter.blade.php
+++ resources/views/dashboard/application/reports/admission-letter.blade.php
@@ -5,7 +5,228 @@
   <title></title>
   <style type="text/css">
       
-      
+      body{
+         font-family: helvetica;
+      }
+     .container {
+        padding-right: 15px;
+        padding-left: 15px;
+        margin-right: auto;
+        margin-left: auto;
+      }
+      .container-fluid {
+        padding-right: 15px;
+        padding-left: 15px;
+        margin-right: auto;
+        margin-left: auto;
+      }
+      .row {
+        margin-right: -15px;
+        margin-left: -15px;
+      }
+      .col-md-1, .col-md-2, .col-md-3, .col-md-4, .col-md-5, .col-md-6, .col-md-7, .col-md-8, .col-md-9, .col-md-10, .col-md-11, .col-md-12 {
+        float: left;
+      }
+      .col-md-12 {
+        width: 100%;
+      }
+      .col-md-11 {
+        width: 91.66666667%;
+      }
+      .col-md-10 {
+        width: 83.33333333%;
+      }
+      .col-md-9 {
+        width: 75%;
+      }
+      .col-md-8 {
+        width: 66.66666667%;
+      }
+      .col-md-7 {
+        width: 58.33333333%;
+      }
+      .col-md-6 {
+        width: 50%;
+      }
+      .col-md-5 {
+        width: 41.66666667%;
+      }
+      .col-md-4 {
+        width: 33.33333333%;
+      }
+      .col-md-3 {
+        width: 25%;
+      }
+      .col-md-2 {
+        width: 16.66666667%;
+      }
+      .col-md-1 {
+        width: 8.33333333%;
+      }
+     table {
+        background-color: transparent;
+      }
+      caption {
+        padding-top: 8px;
+        padding-bottom: 8px;
+        color: #777;
+        text-align: left;
+      }
+      th {
+        text-align: left;
+      }
+      .table {
+        width: 100%;
+        max-width: 100%;
+        margin-bottom: 20px;
+        border-collapse: collapse;
+      }
+      .table > thead > tr > th,
+      .table > tbody > tr > th,
+      .table > tfoot > tr > th,
+      .table > thead > tr > td,
+      .table > tbody > tr > td,
+      .table > tfoot > tr > td {
+        padding: 8px;
+        line-height: 1.42857143;
+        vertical-align: top;
+        border-top: 1px solid #ddd;
+      }
+      .table > thead > tr > th {
+        vertical-align: bottom;
+        border-bottom: 2px solid #ddd;
+      }
+      .table > caption + thead > tr:first-child > th,
+      .table > colgroup + thead > tr:first-child > th,
+      .table > thead:first-child > tr:first-child > th,
+      .table > caption + thead > tr:first-child > td,
+      .table > colgroup + thead > tr:first-child > td,
+      .table > thead:first-child > tr:first-child > td {
+        border-top: 0;
+      }
+      .table > tbody + tbody {
+        border-top: 2px solid #ddd;
+      }
+      .table .table {
+        background-color: #fff;
+      }
+      .table-condensed > thead > tr > th,
+      .table-condensed > tbody > tr > th,
+      .table-condensed > tfoot > tr > th,
+      .table-condensed > thead > tr > td,
+      .table-condensed > tbody > tr > td,
+      .table-condensed > tfoot > tr > td {
+        padding: 5px;
+      }
+      .table-bordered {
+        border: 1px solid #ddd;
+      }
+      .table-bordered > thead > tr > th,
+      .table-bordered > tbody > tr > th,
+      .table-bordered > tfoot > tr > th,
+      .table-bordered > thead > tr > td,
+      .table-bordered > tbody > tr > td,
+      .table-bordered > tfoot > tr > td {
+        border: 1px solid #ddd;
+      }
+      .table-bordered > thead > tr > th,
+      .table-bordered > thead > tr > td {
+        border-bottom-width: 2px;
+      }
+
+
+     .page-break {
+          page-break-after: always;
+      }
+      .ss-bold{
+         font-weight: bold;
+      }
+      .ss-center{
+         text-align: center;
+      }
+      .ss-left{
+         text-align: left;
+      }
+      .ss-right{
+         text-align: right;
+      }
+      .ss-italic{
+        font-style: italic;
+      }
+     .ss-no-margin{
+        margin: 0px;
+     }
+     .ss-margin-top-lg{
+         margin-top: 80px;
+      } 
+     .ss-margin-bottom-lg{
+         margin-bottom: 50px;
+     }
+     .ss-margin-bottom{
+         margin-bottom: 20px;
+     }
+     .ss-font-sm{
+        font-size: 14px;
+     }
+     .ss-font-xs{
+        font-size: 12px;
+     }
+     .ss-letter-head{
+        /*margin-bottom: 20px;*/
+        /*text-align: right;*/
+        text-transform: uppercase;
+     }
+     .ss-letter-head h1, .ss-letter-head h3{
+        margin: 0px;
+     }
+     .ss-sign-column{
+        min-width: 120px !important;
+     }
+     .ss-black-line{
+       width: 100%;
+       height: 5px;
+       background-color: #000;
+     }
+     .ss-photo{
+       width: 120px;
+     }
+     .ss-logo{
+       width: 120px;
+       height: auto;
+     }
+     .ss-signature{
+        height: 120px;
+        width: auto;
+     }
+
+     .place-right {
+      text-align: right;
+      font-size: 20px;
+     }
+
+
+     .ss-line-bottom{
+       border-bottom: 2px solid #000;
+     }
+     .ss-uppercase{
+        text-transform: uppercase;
+     }
+     .ss-margin-top{
+        margin-top: 20px;
+     }
+     .ss-color-blue{
+        color: #371261;
+     }
+     p, li{
+        text-align: justify;
+        margin-bottom: 10px;
+     }
+     @page {
+        header: page-header;
+        footer: page-footer;
+        text-align: center;
+        border-top: 2px solid #000;
+      }
   </style>
 </head>
 
@@ -34,16 +255,7 @@
           </div><!-- end of col-md-3 -->
         </div><!-- end of row -->
 
-        <div class="d-flex justify-content-between">
-            <div>
-              <p>Our Ref: {{ strtoupper($reference_number) }}</p>
-              <p>.............................................</p>
-              <p>.............................................</p> 
-            </div>
-            <div>
-              <span>{{ now()->format('jS F Y') }}</span>
-            </div>
-        </div>
+        <p class="place-right">Hello World</p>
 
         
         <!-- <div class="row">
@@ -98,7 +310,6 @@
 
       </div><!-- end of container -->
 
-<hr>
 <htmlpagefooter name="page-footer" class="ss-center">
   Kigamboni, Ferry Street, MNMA P. O. Box 9193, Dar Es Salaam. Tel: +255 (22) 2820041/47, Fax: +255 (22) 2820816. Email: rector@mnma.ac.tz Website: www.mnma.ac.tz
 </htmlpagefooter>
