893f69743195256fc9b1e2f6842112aad13bd55a
salimshady23 <slimshadysaid@gmail.com>
2023-02-16 16:29:24 +0300

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -499,16 +499,14 @@ class ApplicationController extends Controller
          }
 
 
-         foreach ($list as $applicant) {
-            foreach ($applicant->nectaResultDetails as $necta_results) {
+        //  foreach ($list as $applicant) {
+        //     foreach ($applicant->nectaResultDetails as $necta_results) {
+        //         $o_level_schools = $necta_results->exam_id == 1 ? array($necta_results->center_name) : null;
+        //         $a_level_schools = $necta_results->exam_id == 2 ? array($necta_results->center_name) : null;    
+        //     }
+        //  }
 
-                $o_level_schools = $necta_results->exam_id == 1 ? array($necta_results->center_name) : null;
-                $a_level_schools = $necta_results->exam_id == 2 ? array($necta_results->center_name) : null;
-                
-            }
-         }
-
-         return $a_level_schools;
+         return $list;
 
 
 
