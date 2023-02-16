4a11a1eb45761b16b93827b45ccecea4a278afd8
salimshady23 <slimshadysaid@gmail.com>
2023-02-16 14:23:40 +0300

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -493,7 +493,7 @@ class ApplicationController extends Controller
             ->where('application_window_id', $request->get('application_window_id'))
             ->where('campus_id', $staff->campus_id)
             ->where('status', 'SELECTED')
-            ->with(['nextOfKin', 'region', 'district', 'disabilityStatus', 'selections.campusProgram.program', 'selections'])
+            ->with(['nextOfKin', 'region', 'district', 'disabilityStatus', 'nectaResultDetails', 'selections.campusProgram.program', 'selections'])
             ->get();
          }
 
@@ -540,7 +540,7 @@ class ApplicationController extends Controller
                   fputcsv($file_handle,['S/N', 'FIRST NAME','MIDDLE NAME','SURNAME','GENDER', 'NATIONALITY', 'DISABILITY', 'DATEOFBIRTH', 'F4INDEXNO', 'F6INDEXNO', 'AVN NO', 'CHOICE1', 'CHOICE2', 'CHOICE3', 'CHOICE4', 'INSTITUTION CODE', 'ENTRY CATEGORY', 'OPTS', 'O-LEVEL RESULTS', 'APTS/GPA', 'A-LEVEL RESULTS/DIPLOMA', 'OPEN GPA', 'OPEN RESULTS', 'SELECTED', 'DATE REGISTERED', 'PHONE NUMBER', 'EMAIL ADDRESS', 'KIN PHONE NUMBER', 'DISTRICT', 'REGION', 'CLEARANCE', 'CLEARANCE STATUS', 'TCU ADMISSION STATUS', 'TCU VERIFICATION STATUS', 'CONFIRM STATUS', 'BATCH NO', 'DIPLOMA INSTITUTE', 'PROGRAM COURSE', 'DIPLOMA GPA', 'DIPLOMA RESULTS', 'O-LEVEL SCHOOL', 'CSEE PTS', 'A-LEVEL SCHOOL', 'ACSEE PTS', 'PROGRESS']);
                   
                   foreach ($list as $key => $applicant) { 
-                        foreach($applicant as $option){
+                        foreach($applicant->nectaResultDetails as $option){
                             $firstChoice = 0; $secondChoice = 0; $thirdChoice = 0; $fourthChoice = 0;
                             if($option->order == 1){
                                 $firstChoice = 1;
