b408320980801e008cc7689238841c7da138e093
salimshady23 <slimshadysaid@gmail.com>
2023-02-16 14:34:58 +0300

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -541,18 +541,20 @@ class ApplicationController extends Controller
                   $file_handle = fopen('php://output', 'w');
                   fputcsv($file_handle,['S/N', 'FIRST NAME','MIDDLE NAME','SURNAME','GENDER', 'NATIONALITY', 'DISABILITY', 'DATEOFBIRTH', 'F4INDEXNO', 'F6INDEXNO', 'AVN NO', 'CHOICE1', 'CHOICE2', 'CHOICE3', 'CHOICE4', 'INSTITUTION CODE', 'ENTRY CATEGORY', 'OPTS', 'O-LEVEL RESULTS', 'APTS/GPA', 'A-LEVEL RESULTS/DIPLOMA', 'OPEN GPA', 'OPEN RESULTS', 'SELECTED', 'DATE REGISTERED', 'PHONE NUMBER', 'EMAIL ADDRESS', 'KIN PHONE NUMBER', 'DISTRICT', 'REGION', 'CLEARANCE', 'CLEARANCE STATUS', 'TCU ADMISSION STATUS', 'TCU VERIFICATION STATUS', 'CONFIRM STATUS', 'BATCH NO', 'DIPLOMA INSTITUTE', 'PROGRAM COURSE', 'DIPLOMA GPA', 'DIPLOMA RESULTS', 'O-LEVEL SCHOOL', 'CSEE PTS', 'A-LEVEL SCHOOL', 'ACSEE PTS', 'PROGRESS']);
                   foreach ($list as $key => $applicant) { 
-                        foreach($applicant->nectaResultDetails as $option){
-                            $firstChoice = 0; $secondChoice = 0; $thirdChoice = 0; $fourthChoice = 0;
-                            if($option->order == 1){
-                                $firstChoice = 1;
-                            }elseif($option->order == 2){
-                                $secondChoice = 2;
-                            }elseif($option->order == 3){
-                                $thirdChoice = 3;
-                            }elseif($option->order == 4){
-                                $fourthChoice = 4;
-                            }
-                        }
+
+                        // foreach($applicant->nectaResultDetails as $option){
+                        //     $firstChoice = 0; $secondChoice = 0; $thirdChoice = 0; $fourthChoice = 0;
+                        //     if($option->order == 1){
+                        //         $firstChoice = 1;
+                        //     }elseif($option->order == 2){
+                        //         $secondChoice = 2;
+                        //     }elseif($option->order == 3){
+                        //         $thirdChoice = 3;
+                        //     }elseif($option->order == 4){
+                        //         $fourthChoice = 4;
+                        //     }
+                        // }
+
                         if ($applicant->confirmation_status == 1) {
                             $confirm = 'Confirmed';
                         } else {
