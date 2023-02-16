5a29f1064f754e6cee32a8ca5e49fe7c023244c9
salimshady23 <slimshadysaid@gmail.com>
2023-02-16 11:24:22 +0300

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -475,7 +475,7 @@ class ApplicationController extends Controller
             $list = Applicant::whereHas('intake.applicationWindows',function($query) use($request){
                  $query->where('id',$request->get('application_window_id'));
             })->whereHas('selections',function($query) use($request){
-                 $query->where('status','SELECTED');
+                 $query->where('status','ELIGIBLE');
             })->with(['nextOfKin','intake','selections.campusProgram.program','nectaResultDetails.results','nacteResultDetails.results'])->where('program_level_id',$request->get('program_level_id'))->where('campus_id',$staff->campus_id)->get();
          }
 
