b6ceeb90591fb1c2ad094c02d920f24a2c037053
salimshady23 <slimshadysaid@gmail.com>
2023-02-16 13:16:55 +0300

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -488,7 +488,7 @@ class ApplicationController extends Controller
             }])
             ->get();
 
-            return $list->nectaResultDetails[0];
+            return $list;
 
          }
 
