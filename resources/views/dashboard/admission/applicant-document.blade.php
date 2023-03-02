43d9962c0df9c27b1ba219d90179392be2ea153a
salimshady23 <slimshadysaid@gmail.com>
2023-03-02 18:34:54 +0300

    comments

--- resources/views/dashboard/admission/applicant-document.blade.php
+++ resources/views/dashboard/admission/applicant-document.blade.php
@@ -7,3 +7,33 @@
     width="100%">
     </iframe>
 @endif
+
+@if($applicant->birth_certificate)
+    <iframe
+    src="https://drive.google.com/viewerng/viewer?embedded=true&url={{ asset('uploads/'.$applicant->birth_certificate) }}#toolbar=0&scrollbar=0"
+    frameBorder="0"
+    scrolling="auto"
+    height="400px"
+    width="100%">
+    </iframe>
+@endif
+
+@if($applicant->passport_picture)
+    <iframe
+    src="https://drive.google.com/viewerng/viewer?embedded=true&url={{ asset('uploads/'.$applicant->passport_picture) }}#toolbar=0&scrollbar=0"
+    frameBorder="0"
+    scrolling="auto"
+    height="400px"
+    width="100%">
+    </iframe>
+@endif
+
+@if($applicant->passport_picture)
+    <iframe
+    src="https://drive.google.com/viewerng/viewer?embedded=true&url={{ asset('uploads/'.$applicant->passport_picture) }}#toolbar=0&scrollbar=0"
+    frameBorder="0"
+    scrolling="auto"
+    height="400px"
+    width="100%">
+    </iframe>
+@endif
