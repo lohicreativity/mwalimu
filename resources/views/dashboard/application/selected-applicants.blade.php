63a3c44c10bf1c7edd1115bbd26d98a05027d754
salimshady23 <slimshadysaid@gmail.com>
2023-03-02 18:54:05 +0000

    comments

--- resources/views/dashboard/application/selected-applicants.blade.php
+++ resources/views/dashboard/application/selected-applicants.blade.php
@@ -324,7 +324,7 @@
 									(4th Choice)
 								@endif
 								@break
-							@else
+							@elseif($applicant->status != 'SELECTED' || $applicant->status != 'ADMITTED')
 								@if(!$applicant->selections[count($applicant->selections) - 1]->order)
 									{{ $applicant->selections[count($applicant->selections) - 1] }}
 								@else
