55d4911e16524cd58692b3efa6ebe5ff1418a8c6
salimshady23 <slimshadysaid@gmail.com>
2023-03-02 18:32:33 +0300

    comments

--- resources/views/dashboard/admission/applicant-document.blade.php
+++ resources/views/dashboard/admission/applicant-document.blade.php
@@ -1,6 +1,6 @@
 @if($applicant->passport_picture)
     <iframe
-    src="https://drive.google.com/viewerng/viewer?embedded=true&url={{ asset('uploads/'.$applicant->veta_certificate) }}#toolbar=0&scrollbar=0"
+    src="https://drive.google.com/viewerng/viewer?embedded=true&url={{ asset('uploads/'.$applicant->passport_picture) }}#toolbar=0&scrollbar=0"
     frameBorder="0"
     scrolling="auto"
     height="400px"
