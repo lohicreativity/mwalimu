73dfd18b0a11f40a6940c36bc695b469c8fed2a6
salimshady23 <slimshadysaid@gmail.com>
2023-03-04 12:02:47 +0300

    comments

--- resources/views/dashboard/application/applicants-list.blade.php
+++ resources/views/dashboard/application/applicants-list.blade.php
@@ -216,32 +216,7 @@
                                   <div id="collapseNextOfKin" class="collapse" aria-labelledby="ss-next-of-kin" data-parent="#accordionExample-2">
                                     <div class="card-body">
                                       
-                                    <table class="table table-bordered table-condensed">
-                  <tr>
-                    <td>First name: </td>
-                    <td>{{ $applicant->nextOfKin->first_name }}</td>
-                  </tr>
-                  <tr>
-                    <td>Middle name: </td>
-                    <td>{{ $applicant->nextOfKin->middle_name }}</td>
-                  </tr>
-                  <tr>
-                    <td>Surname: </td>
-                    <td>{{ $applicant->nextOfKin->surname }}</td>
-                  </tr>
-                  <tr>
-                    <td>Gender: </td>
-                    <td>{{ $applicant->nextOfKin->gender }}</td>
-                  </tr>
-                  <tr>
-                    <td>Phone: </td>
-                    <td>{{ $applicant->nextOfKin->phone }}</td>
-                  </tr>
-                  <tr>
-                    <td>Address: </td>
-                    <td>{{ $applicant->nextOfKin->address }}</td>
-                  </tr>
-                </table>
+                                      
 
                                     </div>
                                   </div>
