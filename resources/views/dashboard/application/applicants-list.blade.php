d941ba727b5927631b0d7dd38a54e7af1a1a57d6
salimshady23 <slimshadysaid@gmail.com>
2023-03-04 12:12:22 +0300

    comments

--- resources/views/dashboard/application/applicants-list.blade.php
+++ resources/views/dashboard/application/applicants-list.blade.php
@@ -239,7 +239,7 @@
                                           <td>Control No</td>
                                           <td>
                                             @if($applicant->payment)
-                                            {{ $applicant->payment[0] }}
+                                            {{ $applicant->payment->control_no }}
                                             @endif
                                           </td> 
                                         </tr>
