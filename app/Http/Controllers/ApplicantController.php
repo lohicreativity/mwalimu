850f5ad37767298e92b86f8bb19bb36482c841ec
salimshady23 <slimshadysaid@gmail.com>
2023-03-04 17:22:32 +0300

    comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -1284,7 +1284,7 @@ class ApplicantController extends Controller
         }
         $selection_status = true;
         if(!ApplicationWindow::where('campus_id',session('applicant_campus_id'))->where('intake_id', $applicant->intake_id)->where('begin_date','<=',now()->format('Y-m-d'))->where('end_date','>=',now()->format('Y-m-d'))->where('status','ACTIVE')->first()){
-           $selection_status = $applicants == null ? false : true;
+           $selection_status = $applicants != null ? false : true;
         }
         $data = [
             'applicant'=>$applicant,
