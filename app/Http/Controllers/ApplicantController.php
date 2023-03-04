790244aace8069f0fa03ac3e28eb561597f90224
salimshady23 <slimshadysaid@gmail.com>
2023-03-04 16:04:16 +0300

    comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -107,7 +107,7 @@ class ApplicantController extends Controller
 
         $closed_window = ApplicationWindow::where('campus_id',$request->get('campus_id'))
         ->where('end_date','>=', implode('-', explode('-', now()->format('Y-m-d'))))
-        ->where('intake_id', $applicant->intake_id)
+        ->where('intake_id', $appl->intake_id)
         ->where('status','INACTIVE')->latest()->first();
 
 
