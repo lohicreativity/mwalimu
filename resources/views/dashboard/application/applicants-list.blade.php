34869a631a39339cfaf713f8fa1bcb94a9d77160
salimshady23 <slimshadysaid@gmail.com>
2023-03-04 15:25:33 +0300

    comments

--- resources/views/dashboard/application/applicants-list.blade.php
+++ resources/views/dashboard/application/applicants-list.blade.php
@@ -69,13 +69,22 @@
             @if($application_window)
              <div class="card">
                <div class="card-header">
-                 <a href="{{ url('application/download-applicants-list?duration='.$request->get('duration').'&status='.$request->get('status').'&department_id='.$request->get('department_id').'&gender='.$request->get('gender').'&nta_level_id='.$request->get('nta_level_id').'&campus_program_id='.$request->get('campus_program_id').'&application_window_id='.$request->get('application_window_id')) }}" class="btn btn-primary">Download Applicants List</a>
-                 <select name="" class="form-control">
-                    <option value="Progress">Progress</option>
-                    <option value="Completed">Completed</option>
-                    <option value="Submitted">Submitted</option>
-                    <option value="Total">Total</option>
-                 </select>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <a href="{{ url('application/download-applicants-list?duration='.$request->get('duration').'&status='.$request->get('status').'&department_id='.$request->get('department_id').'&gender='.$request->get('gender').'&nta_level_id='.$request->get('nta_level_id').'&campus_program_id='.$request->get('campus_program_id').'&application_window_id='.$request->get('application_window_id')) }}" class="btn btn-primary">Download Applicants List</a>
+                    </div>
+                    <div class="col-md-6">
+                      <label for="">Status</label>
+                      <select name="status" class="form-control">
+                        <option value="Progress">Progress</option>
+                        <option value="Completed">Completed</option>
+                        <option value="Submitted">Submitted</option>
+                        <option value="Total">Total</option>
+                      </select>
+                    </div>
+                  </div>
+                  
+                  
                </div>
                <!-- /.card-header -->
                <div class="card-body">
