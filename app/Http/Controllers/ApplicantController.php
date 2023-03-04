fa73f2e0da7a8db491b8e65582ea31d630d0a41f
salimshady23 <slimshadysaid@gmail.com>
2023-03-04 17:12:44 +0300

    comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -1273,7 +1273,13 @@ class ApplicantController extends Controller
     public function submission(Request $request)
       {
         $applicant = User::find(Auth::user()->id)->applicants()->with('programLevel')->where('campus_id',session('applicant_campus_id'))->first();
-        if(ApplicationWindow::where('campus_id',session('applicant_campus_id'))->where('begin_date','<=',now()->format('Y-m-d'))->where('end_date','>=',now()->format('Y-m-d'))->where('status','ACTIVE')->first()){
+
+
+         $applicants = Applicant::where('program_level_id', $applicant->program_level_id)->where('application_window_id', $applicant->application_window_id)->get();
+
+         return $applicants;
+
+        if(ApplicationWindow::where('campus_id',session('applicant_campus_id'))->where('intake_id', $applicant->intake_id)->where('begin_date','<=',now()->format('Y-m-d'))->where('end_date','>=',now()->format('Y-m-d'))->where('status','ACTIVE')->first()){
             if($applicant->programs_complete_status != 1){
                 return redirect()->back()->with('error','You must first select programmes');
             }
