d95317b84c2de3137ba0d37a09ed78143f43904e
salimshady23 <slimshadysaid@gmail.com>
2023-03-04 14:57:42 +0300

    comments

--- resources/views/dashboard/application/applicants-list.blade.php
+++ resources/views/dashboard/application/applicants-list.blade.php
@@ -153,7 +153,7 @@
                         <div class="modal-dialog modal-lg">
                           <div class="modal-content modal-lg">
                             <div class="modal-header">
-                              <h4 class="modal-title"><i class="fa fa-exclamation-sign"></i>Progress for {{ $applicant->first_name }} {{ $applicant->surname }} ({{ $applicant->index_number }})</h4>
+                              <h5 class="modal-title"><i class="fa fa-exclamation-sign"></i>{{ $applicant->first_name }} {{ $applicant->surname }} | {{ $applicant->index_number }} | {{ $applicant->programLevel->name }} | {{ $applicant->entry_mode }}</h5>
                               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                               </button>
