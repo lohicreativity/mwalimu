ec70d54ec283c0fb3225c598c3ebc26f959c8831
salimshady23 <slimshadysaid@gmail.com>
2023-03-06 06:50:38 +0000

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -3120,7 +3120,7 @@ class ApplicationController extends Controller
          if(!$ac_year){
             return redirect()->back()->with('error','No active academic year');
          }
-         $application_window = ApplicationWindow::where('campus_id',$staff->campus_id)->where('status', 'ACTIVE')->whereYear('end_date',explode('/',$ac_year->academicYear->year)[0])->first();
+         $application_window = ApplicationWindow::where('campus_id',$staff->campus_id)->whereYear('end_date',explode('/',$ac_year->academicYear->year)[0])->first();
 
          if(!$application_window){
              return redirect()->back()->with('error','No corresponding application window');
