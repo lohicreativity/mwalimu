73856cb436e0df7c4efa3f02ecf7a3a9be64539d
salimshady23 <slimshadysaid@gmail.com>
2023-03-07 11:53:28 +0300

    comments

--- resources/views/dashboard/application/edit-basic-information.blade.php
+++ resources/views/dashboard/application/edit-basic-information.blade.php
@@ -40,7 +40,7 @@
           <div class="col-12">
 
             @if(count($applicant->selections) > 0)
-              @if($applicant->confirmation_status != 'CANCELLED')
+              @if($applicant->confirmation_status != 'CANCELLED' && $applicant->status != 'ADMITTED')
                 <div class="alert alert-success">
                   <h3 class="text-white" style="font-size: 18px!important;"><i class="fa fa-check-circle"></i> 
                   Congratulations! You have been successfully selected for {{ $applicant->selections[0]->campusProgram->program->name }} program. @if($applicant->multiple_admissions == null) Please wait for admission package. @else Please <a href="{{ url('application/admission-confirmation') }}">click here</a> to confirm with us.@endif</h3>
