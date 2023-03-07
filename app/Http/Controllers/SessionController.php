094ee1f335022089222f96d710176a5ef84e41b2
salimshady23 <slimshadysaid@gmail.com>
2023-03-07 13:07:52 +0300

    comments

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -56,11 +56,6 @@ class SessionController extends Controller
 
 				if ($user->save()) {
 
-					Auth::logout();
-					$request->session()->invalidate();
-					$request->session()->regenerateToken();
-					return redirect()->to('student/login');
-
 					Auth::guard('web')->logout();
 					$request->session()->invalidate();
 					$request->session()->regenerateToken();
