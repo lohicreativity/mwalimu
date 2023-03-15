a226fe6d5c6220d8d53b90fbfd84a3ded91c9166
salimshady23 <slimshadysaid@gmail.com>
2023-03-15 18:10:15 +0000

    comments

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -632,7 +632,7 @@ class ExaminationResultController extends Controller
                           }
 
                             foreach($results as $resKey=>$result){
-								return $result;
+
                             $std = Student::with(['campusProgram.program.ntaLevel'])->find($result->student_id);
                                   
                             
@@ -640,7 +640,7 @@ class ExaminationResultController extends Controller
                                     $query->where('student_id',$std->id);
                                       })->with(['module'])->where('study_academic_year_id',$assignment->study_academic_year_id)->where('year_of_study',$assignment->programModuleAssignment->year_of_study)->where('semester_id',$rem->semester_id)->where('category','OPTIONAL')->get();
 
-
+return $optional_programs;
                                  $stud_buffer[$key]['total_credit'] = $total_credit;
                                  $stud_buffer[$key]['opt_credit'] = 0;
                                  $stud_buffer[$key]['opt_prog_status'] = true;
