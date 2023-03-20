24701e6f1938cac8e408d48fe44dd90562985d23
salimshady23 <slimshadysaid@gmail.com>
2023-03-20 16:30:05 +0000

    comments

--- app/Http/Controllers/GraduantController.php
+++ app/Http/Controllers/GraduantController.php
@@ -302,16 +302,6 @@ class GraduantController extends Controller
         if(!$graduant){
              return redirect()->back()->with('error','You are not in the graduants list, please check with the Examination Office');
         }
-		$x = Invoice::whereHas('feeType',function($query){$query->where('name','LIKE','%Graduation Gown%');})->whereHas('GatewayPayment')->where('payable_id', $student->id)
-			 ->where('applicable_id', $graduant->study_academic_year_id)->count();
-		return $x;
-/* 		$x = DB::table('gateway_payments')->join('invoices','gateway_payments.control_no','=','invoices.control_no')->join('fee_types','invoices.fee_type_id','=','fee_types.id')
-			->join('study_academic_years','invoices.applicable_id','=','study_academic_years.id')->join('academic_years','study_academic_years.academic_year_id','=','academic_years.id')
-			->select(DB::raw('gateway_payments.*, fee_types.name as fee_name, academic_years.year as academic_year'))->where(function($query) use($student){
-			$query->where('invoices.payable_id',$student->id)->where('invoices.payable_type','student')->where('invoices.applicable_type','academic_year');
-			})->orWhere(function($query) use($student){
-				$query->where('invoices.payable_id',$student->applicant_id)->where('invoices.payable_type','applicant')->where('invoices.applicable_type','academic_year');
-			})->latest()->get(); */
 			
         $data = [
            'student'=>$student,
