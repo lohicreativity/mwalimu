02b23c7c0ae16f0bafd0587f38e6058ef07f40e3
salimshady23 <slimshadysaid@gmail.com>
2023-03-20 06:01:34 +0000

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -1220,7 +1220,7 @@ class ApplicationController extends Controller
        $applicant = Applicant::with(['programLevel'])->find($request->get('applicant_id'));
 	   		  	    $previous_studied_programme = Applicant::whereHas('selections', function($query) {$query->where('status', 'SELECTED');})->with('selections.campusProgram.program')
 												->where('index_number', $applicant->index_number)->where('program_level_id', $applicant->program_level_id - 1)->first(); 
-		return $previous_studied_programme->selections[0]->program[0]->name;
+		return $previous_studied_programme->selections[0]->program;
        if($applicant->basic_info_complete_status == 0){
           return redirect()->back()->with('error','Basic information section not completed');
        }
