fd3925660feb9996e752f8b8e4034782aaea44cd
salimshady23 <slimshadysaid@gmail.com>
2023-03-21 11:35:51 +0000

    comments

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -96,7 +96,7 @@ class ExaminationResultController extends Controller
     	DB::beginTransaction();
 
     	$campus_program = CampusProgram::with('program')->find(explode('_',$request->get('campus_program_id'))[0]);
-		return $campus_program;
+
       if(ResultPublication::where('study_academic_year_id',$request->get('study_academic_year_id'))->where('semester_id',$request->get('semester_id'))->where('nta_level_id',$campus_program->program->nta_level_id)->where('status','PUBLISHED')->count() != 0){
                   return redirect()->back()->with('error','Unable to process because results already published');
               }
