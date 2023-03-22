88a9fbf0ee5e2ceca294f6a9e2205683cd89a947
salimshady23 <slimshadysaid@gmail.com>
2023-03-22 04:48:27 +0000

    comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -350,7 +350,7 @@ class ApplicantController extends Controller
         ->with(['selections' => function($query) {$query->where('status', 'SELECTED')->orWhere('status', 'PENDING')->first();}])
 		->where('campus_id',session('applicant_campus_id'))->first();
 		
-return $check_selected_applicant->selections[0];
+return $check_selected_applicant->selections[0]->status;
 		ApplicantProgramSelection::where('application_window_id', $applicant->application_window_id)
          ->where(function($query) {
             $query->where('status', 'SELECTED')
