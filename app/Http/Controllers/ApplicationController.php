0051d324bcafcc76cb74cb110dfc1f3979442886
salimshady23 <slimshadysaid@gmail.com>
2023-03-24 13:49:24 +0000

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4680,7 +4680,7 @@ class ApplicationController extends Controller
             }
             
         }
-		if(count($programs) == 1){
+		if(count($programs) <= 1){
 			if($programs->id == $student-campus_program_id){
 				return redirect()->back()->with('error','The student does not qualify to any other programme');
 			}	
