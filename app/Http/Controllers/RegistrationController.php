b42cad6d70537119178c96072916e717de2bd0be
salimshady23 <slimshadysaid@gmail.com>
2023-03-28 13:53:10 +0100

    comments

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -379,7 +379,7 @@ class RegistrationController extends Controller
 			})->whereHas('student.studentshipStatus',function($query){
 				  $query->where('name','ACTIVE')->orWhere('name','RESUMED');
 			})->with(['student.applicant.nacteResultDetails','student.applicant.nectaResultDetails','student.campusProgram.program'])->where('study_academic_year_id',session('active_academic_year_id'))->where('semester_id',session('active_semester_id'))->get();
-		   }elseif(Auth::user()->hasRole('admission-officer') || Auth::user()->hasRole('examin-officer')){
+		   }elseif(Auth::user()->hasRole('admission-officer') || Auth::user()->hasRole('examination-officer')){
 			   $active_students = Registration::whereHas('student.campusProgram',function($query) use($staff){
 				  $query->where('campus_id',$staff->campus_id);
 			})->whereHas('student.studentshipStatus',function($query){
