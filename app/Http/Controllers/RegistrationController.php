dd4fda6bbbc9a375358f859b80575ba42b1170d8
salimshady23 <slimshadysaid@gmail.com>
2023-03-31 18:21:50 +0100

    comments

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -645,7 +645,7 @@ class RegistrationController extends Controller
           } */
   		$ac_year = StudyAcademicYear::where('id',$request->get('study_academic_year_id'))->first();
         $semester = Semester::where('status','ACTIVE')->first();
-		return 1;
+return $request;
         $student = Student::whereHas('registrations', function($query) use($request, $semester){$query->where('study_academic_year_id',$request->get('study_academic_year_id'))->where('semester_id',$semester->id)->where('id_print_status', 0);})
 		           ->whereHas('campusProgram.program',function($query) use($request){$query->where('award_id',$request->get('program_level_id'));})
 				   ->whereHas('applicant',function($query) use($request){$query->where('campus_id',$request->get('campus_id'));})
