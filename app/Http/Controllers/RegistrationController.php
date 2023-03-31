2decde78396edc77cda2a86510ef17548b3fe405
salimshady23 <slimshadysaid@gmail.com>
2023-03-31 18:51:23 +0100

    comments

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -659,7 +659,7 @@ class RegistrationController extends Controller
         }
 
         $data = [
-            'student'=>$student,
+            'students'=>$student,
             'semester'=>$semester,
 			'study_academic_years'=>StudyAcademicYear::with('academicYear')->get(),
             'study_academic_year'=>$request->has('study_academic_year_id')? StudyAcademicYear::with('academicYear')->find($request->get('study_academic_year_id')) : StudyAcademicYear::where('status', 'ACTIVE')->first(),
