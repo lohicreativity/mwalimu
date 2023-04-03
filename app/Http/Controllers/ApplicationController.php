b35d855f01f190c5160f2b41bd6b43de9b8cbae4
salimshady23 <slimshadysaid@gmail.com>
2023-04-03 07:26:32 +0100

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -3605,7 +3605,7 @@ class ApplicationController extends Controller
 	public function resetInsuranceStatus(Request $request)
 	{
 		HealthInsurance::where('applicant_id',$request->get('applicant_id'))->delete();
-		$applicant = Applicant::where('applicant_id',$request->get('applicant_id'))->first();
+		$applicant = Applicant::where('id',$request->get('applicant_id'))->first();
 		$applicant->insurance_status = 0;
 		$applicant->save();
 		
