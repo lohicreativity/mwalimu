eca13ccb8f904db429ec0b1a92fe145f49e9a9ae
salimshady23 <slimshadysaid@gmail.com>
2023-04-04 17:32:28 +0100

    comments

--- resources/views/dashboard/finance/payer-details.blade.php
+++ resources/views/dashboard/finance/payer-details.blade.php
@@ -155,3 +155,16 @@
 	 </div>
 	 </div>
 	 </section>
+  </div>
+  <!-- /.content-wrapper -->
+  @include('layouts.footer')
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+@endsection
