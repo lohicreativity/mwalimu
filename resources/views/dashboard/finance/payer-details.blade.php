36908f1074c707b3f2551834e05b0e9e2f2b0615
salimshady23 <slimshadysaid@gmail.com>
2023-04-04 13:47:49 +0100

    comments

--- resources/views/dashboard/finance/payer-details.blade.php
+++ resources/views/dashboard/finance/payer-details.blade.php
@@ -1,4 +1,74 @@
 
+@extends('layouts.app')
+
+@section('content')
+
+<div class="wrapper">
+
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="{{ asset('dist/img/logo.png') }}" alt="{{ Config::get('constants.SITE_NAME') }}" height="60" width="60">
+  </div>
+
+  @include('layouts.auth-header')
+
+  @include('layouts.sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>{{ __('Payer Search') }}</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">{{ __('Payer Search') }}</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Search for Payer</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                 @php
+                     $identifier = [
+                         'class'=>'form-control',
+                         'placeholder'=>'Surname/registration number/Form IV index number',
+                         'required'=>true
+                     ];
+                 @endphp 
+                 {!! Form::open(['url'=>'finance/payer-details','class'=>'ss-form-processing','method'=>'GET']) !!}
+                   
+                  <div class="row">
+                  <div class="form-group col-6">
+                    {!! Form::label('','Enter keyword') !!}
+                    {!! Form::text('keyword',null,$identifier) !!}
+                  </div>
+                  </div>
+                  <div class="ss-form-actions">
+                   <button type="submit" class="btn btn-primary">{{ __('Search') }}</button>
+                  </div>
+
+                 {!! Form::close() !!}
+              </div>
+            </div>
+            <!-- /.card -
 	<div style="margin-top:20px;" class="modal fade" id="ss-progress-{{ $reg->student->id }}">
 		<div class="modal-dialog modal-lg">
 		  <div class="modal-content modal-lg">
