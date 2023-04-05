4b340ffa27e653ce13c4abe6770a267988631f46
salimshady23 <slimshadysaid@gmail.com>
2023-04-05 07:06:09 +0100

    comments

--- resources/views/dashboard/finance/payer-details.blade.php
+++ resources/views/dashboard/finance/payer-details.blade.php
@@ -71,87 +71,82 @@
 			
             <!-- /.card -->
 			@if($payer && $category === 'student')
-	<div style="margin-top:20px;" class="modal fade">
-		  <div class="modal-content modal-lg">
-			<div class="modal-header">
-			  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-				<span aria-hidden="true">&times;</span>
-			  </button>
-			</div>
-			<div class="modal-body">
+			<div style="margin-top:20px;" class="modal fade">
+			  <div class="modal-content modal-lg">
 
-				<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-				<!-- <div class="container bootstrap snippets bootdey"> -->
-				<div class="col-md-12">
-					<div class="row">
-						<div class="col-md-3 col-sm-3">
-							<div class="text-center">
-								<img class="profile-user-img img-fluid" src="{{ asset('uploads/'.$payer->image) }}" onerror="this.src='{{ asset("img/user-avatar.png") }}'" alt="Student Picture">
-												 
-							</div> <!-- /.thumbnail -->
+				<div class="modal-body">
 
-						</div> <!-- /.col -->
+					<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+					<!-- <div class="container bootstrap snippets bootdey"> -->
+					<div class="col-md-12">
+						<div class="row">
+							<div class="col-md-3 col-sm-3">
+								<div class="text-center">
+									<img class="profile-user-img img-fluid" src="{{ asset('uploads/'.$payer->image) }}" onerror="this.src='{{ asset("img/user-avatar.png") }}'" alt="Student Picture">
+													 
+								</div> <!-- /.thumbnail -->
+
+							</div> <!-- /.col -->
 
 
-						<div class="col-md-9 col-sm-9">
-							<h2>{{ $payer->first_name }} {{ $payer->middle_name }} {{ $payer->surname }}</h2>
-							<h6>{{ $payer->registration_number }} &nbsp; | &nbsp; {{ $payer->campusProgram->program->code}} &nbsp; | &nbsp; Year {{ $payer->year_of_study }} &nbsp; | &nbsp; <span style="color:red">{{ $payer->studentshipStatus->name }} </span></h6>
-							<hr>
-							<ul style="list-style-type: none; inline">
-								<li><i class="icon-li fa fa-envelope"></i> &nbsp; &nbsp;{{ $payer->email }}</li>
-								<li><i class="icon-li fa fa-phone"></i> &nbsp; &nbsp;{{ $payer->phone }}</li>
-							</ul>
-							<hr>
+							<div class="col-md-9 col-sm-9">
+								<h2>{{ $payer->first_name }} {{ $payer->middle_name }} {{ $payer->surname }}</h2>
+								<h6>{{ $payer->registration_number }} &nbsp; | &nbsp; {{ $payer->campusProgram->program->code}} &nbsp; | &nbsp; Year {{ $payer->year_of_study }} &nbsp; | &nbsp; <span style="color:red">{{ $payer->studentshipStatus->name }} </span></h6>
+								<hr>
+								<ul style="list-style-type: none; inline">
+									<li><i class="icon-li fa fa-envelope"></i> &nbsp; &nbsp;{{ $payer->email }}</li>
+									<li><i class="icon-li fa fa-phone"></i> &nbsp; &nbsp;{{ $payer->phone }}</li>
+								</ul>
+								<hr>
 
-							<div class="accordion" id="student-accordion">
-								<div class="card">
-								  <div class="card-header" id="ss-address">
-									  <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseAddress" aria-expanded="true" aria-controls="collapseAddress">
-										&nbsp; More Details &nbsp; <i class="fa fa-chevron-right list-group-chevron"></i>
-									  </button>
-								  </div>
+								<div class="accordion" id="student-accordion">
+									<div class="card">
+									  <div class="card-header" id="ss-address">
+										  <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseAddress" aria-expanded="true" aria-controls="collapseAddress">
+											&nbsp; More Details &nbsp; <i class="fa fa-chevron-right list-group-chevron"></i>
+										  </button>
+									  </div>
 
-								  <div id="collapseAddress" class="collapse" aria-labelledby="ss-address" data-parent="#student-accordion">
-									<div class="card-body">
+									  <div id="collapseAddress" class="collapse" aria-labelledby="ss-address" data-parent="#student-accordion">
+										<div class="card-body">
 
-									  @if($payer->applicant)
-										  &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Gender:</span> &nbsp; @if($payer->applicant->gender == 'M') Male @elseif($payer->applicant->gender == 'F') Female @endif
-										  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Date of Birth:</span> &nbsp; {{ $payer->applicant->birth_date }}
-										  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Nationality:</span> &nbsp; {{ $payer->applicant->nationality }}											  
-										  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Disability:</span> &nbsp; {{ $payer->applicant->disabilityStatus->name }}
-										  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Entry Mode:</span> &nbsp; {{ $payer->applicant->entry_mode }}	 												  
-										  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Postal Address:</span> &nbsp; {{ $payer->applicant->address }}	 	
-										  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Physical Address:</span> &nbsp; {{ $payer->applicant->ward->name }},&nbsp; {{ $payer->applicant->region->name }},&nbsp; {{ $payer->applicant->country->name }}	 	 
-									  @endif
+										  @if($payer->applicant)
+											  &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Gender:</span> &nbsp; @if($payer->applicant->gender == 'M') Male @elseif($payer->applicant->gender == 'F') Female @endif
+											  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Date of Birth:</span> &nbsp; {{ $payer->applicant->birth_date }}
+											  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Nationality:</span> &nbsp; {{ $payer->applicant->nationality }}											  
+											  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Disability:</span> &nbsp; {{ $payer->applicant->disabilityStatus->name }}
+											  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Entry Mode:</span> &nbsp; {{ $payer->applicant->entry_mode }}	 												  
+											  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Postal Address:</span> &nbsp; {{ $payer->applicant->address }}	 	
+											  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Physical Address:</span> &nbsp; {{ $payer->applicant->ward->name }},&nbsp; {{ $payer->applicant->region->name }},&nbsp; {{ $payer->applicant->country->name }}	 	 
+										  @endif
+										</div>
+									  </div>
 									</div>
-								  </div>
-								</div>
-								
-								<div class="card">
-								  <div class="card-header" id="ss-next-of-kin">
-									  <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseNextOfKin" aria-expanded="true" aria-controls="collapseNextOfKin">
-										&nbsp; Payment Details &nbsp; <i class="fa fa-chevron-right list-group-chevron"></i>
-									  </button>
-								  </div>
+									
+									<div class="card">
+									  <div class="card-header" id="ss-next-of-kin">
+										  <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseNextOfKin" aria-expanded="true" aria-controls="collapseNextOfKin">
+											&nbsp; Payment Details &nbsp; <i class="fa fa-chevron-right list-group-chevron"></i>
+										  </button>
+									  </div>
 
-								  <div id="collapseNextOfKin" class="collapse" aria-labelledby="ss-next-of-kin" data-parent="#student-accordion">
-									<div class="card-body">
+									  <div id="collapseNextOfKin" class="collapse" aria-labelledby="ss-next-of-kin" data-parent="#student-accordion">
+										<div class="card-body">
 
+										</div>
+									  </div>
 									</div>
-								  </div>
-								</div>
-							</div>                                  
+								</div>                                  
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		  </div>
-		  <!-- /.modal-content -->
-		</div> 
-	  <!-- /.modal -->
-
-	 @endif
-	 </div>
+			  </div>
+			  <!-- /.modal-content -->
+			</div> 
+		  <!-- /.modal -->
+		@endif
+	   </div>
 	 </div>
 	 </section>
   </div>
