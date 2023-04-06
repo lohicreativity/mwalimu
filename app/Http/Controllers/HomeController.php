d4b2a8b4d5b9cc76f0d62e7d069af0c946f842f5
salimshady23 <slimshadysaid@gmail.com>
2023-04-06 07:32:21 +0100

    comments

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -49,7 +49,7 @@ class HomeController extends Controller
             })->where('study_academic_year_id',session('active_academic_year_id'))->where('semester_id',session('active_semester_id'))->latest()->first(),
            'last_session'=>UserSession::where('user_id',Auth::user()->id)->orderBy('last_activity','desc')->offset(1)->first(),
 		   'internal_transfer_count'=>InternalTransfer::whereNull('loan_changed')->where('status','SUBMITTED')->count(),
-		   'loan_beneficiary_count'=>Student::whereHas('registration', function($query) use($ac_year){$query->where('study_academic_year_id', $ac_year->id);})
+		   'loan_beneficiary_count'=>Student::whereHas('registrations', function($query) use($ac_year){$query->where('study_academic_year_id', $ac_year->id);})
 		   ->whereHas('internalTransfer',function($query){$query->whereNull('loan_changed')->where('status','SUBMITTED');})
 		   ->whereHas('loanAllocation',function($query) use($ac_year){$query->where('study_academic_year_id', $ac_year->id);})->count()
         ];
