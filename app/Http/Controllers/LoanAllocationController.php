ade15bd28cdb011491022d2ca19207c0dfd11b13
salimshady23 <slimshadysaid@gmail.com>
2023-04-07 07:57:58 +0100

    comments

--- app/Http/Controllers/LoanAllocationController.php
+++ app/Http/Controllers/LoanAllocationController.php
@@ -142,7 +142,7 @@ class LoanAllocationController extends Controller
     {
 		$ac_year = StudyAcademicYear::where('status','ACTIVE')->first();
         $semester = Semester::where('status','ACTIVE')->first();
-		
+		return $request;
 		if($request->get('postponement_status') == 1 || $request->get('deceased_status') == 1){
 			LoanAllocation::where('student_id', $request->get('student_id'))->where('study_academic_year_id', $ac_year->id)->where('year_of_study',$request->get('year_of_study'))->latest()->delete();			
 		}elseif($request->get('transfer_status') == 1){
