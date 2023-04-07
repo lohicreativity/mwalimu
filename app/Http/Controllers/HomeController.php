42271fddf847810fdfe5dfbb520421fdade90a64
salimshady23 <slimshadysaid@gmail.com>
2023-04-07 12:22:01 +0100

    comments

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -84,7 +84,7 @@ class HomeController extends Controller
 		}		
 
 return Postponement::whereHas('student.applicant',function($query) use($staff){$query->where('campus_id',$staff->campus_id);})
-					->whereHas('student.campusProgram.program.department', function($query) use($staff){$query->where('department_id', $staff->department_id);})
+					->with(['student.campusProgram.program.department'])
 					->whereNull('recommended_by_user_id')->get();
         $data = [
            'staff'=>$staff,
