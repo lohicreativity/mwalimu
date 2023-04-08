61f40673e9e422b51964f72121ed153dc915103c
salimshady23 <slimshadysaid@gmail.com>
2023-04-08 17:26:13 +0100

    comments

--- resources/views/dashboard/finance/payer-details.blade.php
+++ resources/views/dashboard/finance/payer-details.blade.php
@@ -70,7 +70,7 @@
             </div>
 			
             <!-- /.card -->
-			@if($payer && $category === 'student')
+			@if($payer && $category == 'student' && $category != 'applicant')
 			<div style="margin-top:20px;" data-toggle="modal">
 			  <div class="modal-content">
 				<div class="modal-header">
@@ -184,6 +184,120 @@
 			  <!-- /.modal-content -->
 			</div> 
 		  <!-- /.modal -->
+		@if($payer && $category == 'applicant' && $category != 'student')
+			<div style="margin-top:20px;" data-toggle="modal">
+			  <div class="modal-content">
+				<div class="modal-header">
+				  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				  </button>
+				</div>
+				<div class="modal-body">
+
+					<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+					<!-- <div class="container bootstrap snippets bootdey"> -->
+					<div class="col-md-12">
+						<div class="row">
+							<div class="col-md-3 col-sm-3">
+								<div class="text-center">
+									<img class="profile-user-img img-fluid" src="{{ asset('uploads/'.$payer->image) }}" onerror="this.src='{{ asset("img/user-avatar.png") }}'" alt="Student Picture">
+													 
+								</div> <!-- /.thumbnail -->
+
+							</div> <!-- /.col -->
+
+
+							<div class="col-md-9 col-sm-9">
+								<h2>{{ $payer->first_name }} {{ $payer->middle_name }} {{ $payer->surname }}</h2>
+								<h6>{{ $payer->index_number }} &nbsp; | &nbsp; {{ $payer->programLevel->name}} &nbsp; | &nbsp; {{ $payer->intake->name }} Intake &nbsp; | &nbsp; 
+									<span style="color:red">@if($payer->submission_complete_status == 1) @if($payer->status == null) Submitted @else {{ $payer->status }} @endif @else In progress @endif</span></h6>
+								<hr>
+								<ul style="list-style-type: none; inline">
+									<li><i class="icon-li fa fa-envelope"></i> &nbsp; &nbsp;{{ $payer->email }}</li>
+									<li><i class="icon-li fa fa-phone"></i> &nbsp; &nbsp;{{ $payer->phone }}</li>
+								</ul>
+								<hr>
+
+								<div class="accordion" id="student-accordion">
+									<div class="card">
+									  <div class="card-header" id="ss-address">
+										  <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseAddress" aria-expanded="true" aria-controls="collapseAddress">
+											&nbsp; More Details &nbsp; <i class="fa fa-chevron-right list-group-chevron"></i>
+										  </button>
+									  </div>
+
+									  <div id="collapseAddress" class="collapse" aria-labelledby="ss-address" data-parent="#student-accordion">
+										<div class="card-body">
+
+											  &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Gender:</span> &nbsp; @if($payer->gender == 'M') Male @elseif($payer->gender == 'F') Female @endif
+											  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Date of Birth:</span> &nbsp; {{ $payer->birth_date }}
+											  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Nationality:</span> &nbsp; {{ $payer->nationality }}											  
+											  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Disability:</span> &nbsp; {{ $payer->disabilityStatus->name }}
+											  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Entry Mode:</span> &nbsp; {{ $payer->entry_mode }}	 												  
+											  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Postal Address:</span> &nbsp; {{ $payer->address }}	 	
+											  <br> &nbsp; &nbsp; &nbsp; <span style="font-style:italic">Physical Address:</span> &nbsp; {{ $payer->ward->name }},&nbsp; {{ $payer->region->name }},&nbsp; {{ $payer->country->name }}	 	 
+										
+										</div>
+									  </div>
+									</div>
+									
+									<div class="card">
+									  <div class="card-header" id="ss-payments">
+										  <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapsePayments" aria-expanded="true" aria-controls="collapsePayments">
+											&nbsp; Payment Details &nbsp; <i class="fa fa-chevron-right list-group-chevron"></i>
+										  </button>
+									  </div>
+									  
+									<div id="collapsePayments" class="collapse" aria-labelledby="ss-payments" data-parent="#student-accordion">
+
+									  <div class="card-body">			  
+									    <table class="table table-bordered ss-paginated-table" style="font-size:10pt">
+											<thead>
+											<tr>
+											   <th>SN</th>					
+											   <th>Date</th>
+											   <th>Invoice#</th>
+											   <th>Fee Type</th>										   
+											   <th>Fee Amount</th>
+											   <th>Paid Amount</th>
+											   <th>Balance</th>
+											</tr>
+										  </thead>
+										  <tbody>
+											@foreach($applicant_payments as $key=>$payments)
+											<tr>
+											   <td>{{ ($key+1) }}</td>
+											   <td>{{ date('Y-m-d',strtotime($payments->gatewayPayment->created_at))}}</td>
+											   <td title="Control#: {{ $payments->control_no }}">{{ $payments->reference_no }}</td> 
+											   <td>{{ $payments->feeType->name }}</td> 											   
+											   <td>{{ number_format($payments->gatewayPayment->bill_amount,2) }} {{ $payments->gatewayPayment->ccy }}</td>
+											   <td>
+												  {{ number_format($payments->gatewayPayment->bill_amount,2) }} {{ $payments->gatewayPayment->ccy }}
+											   </td>
+											   <td>
+												  {{ number_format($payments->gatewayPayment->bill_amount-$payments->gatewayPayment->paid_amount,2) }} {{ $payments->gatewayPayment->ccy }}
+											   </td>
+											</tr>
+											@endforeach
+										  </tbody>
+										 </table>
+									  </div>
+
+									  <div id="collapseNextOfKin" class="collapse" aria-labelledby="ss-next-of-kin" data-parent="#student-accordion">
+										<div class="card-body">
+
+										</div>
+									  </div>
+									</div>
+								</div>                                  
+							</div>
+						</div>
+					</div>
+				</div>
+			  </div>
+			  <!-- /.modal-content -->
+			</div> 
+		  <!-- /.modal -->			
 		@endif
 	   </div>
 	 </div>
