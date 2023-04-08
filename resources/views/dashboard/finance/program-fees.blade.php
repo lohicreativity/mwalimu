7028209d85cf21b42e1a5bd5ff476e88c94cde75
salimshady23 <slimshadysaid@gmail.com>
2023-04-08 15:31:02 +0100

    comments

--- resources/views/dashboard/finance/program-fees.blade.php
+++ resources/views/dashboard/finance/program-fees.blade.php
@@ -175,16 +175,7 @@
               </div>
               <!-- /.card-header -->
               <div class="card-body">
-                {!! Form::open(['url'=>'finance/program-fees','method'=>'GET']) !!}
-                <div class="input-group ss-stretch">
-                 <input type="text" name="query" class="form-control" placeholder="Search for academic year">
-                 <span class="input-group-btn">
-                   <button class="btn btn-default" type="submit"><span class="fa fa-search"></span></button>
-                 </span>
-                </div>
-                {!! Form::close() !!}
-
-                <table id="example2" class="table table-bordered table-hover ss-margin-top">
+                <table id="example2" class="table table-bordered table-hover ss-margin-top ss-paginated-table">
                   <thead>
                   <tr>
                     <th>Programme</th>
@@ -192,7 +183,11 @@
                     <th>Amount in USD</th>
                     <th>Academic Year</th>
                     <th>Year of Study</th>
-                    <th>Actions</th>
+					@if(Auth::user()->hasRole('administrator') || Auth::user()->hasRole('arc'))					
+						<th>Campus</th>
+					@else	
+						<th>Actions</th>
+					@endif
                   </tr>
                   </thead>
                   <tbody>
@@ -203,149 +198,160 @@
                     <td>{{ number_format($fee->amount_in_usd,2) }}</td>
                     <td>{{ $fee->studyAcademicYear->academicYear->year }}</td>
                     <td>{{ $fee->year_of_study }}</td>
-                    <td>
-                      @can('edit-programme-fee')
-                      <a class="btn btn-info btn-sm" href="#" data-toggle="modal" data-target="#ss-edit-amount-{{ $fee->id }}">
-                              <i class="fas fa-pencil-alt">
-                              </i>
-                              Edit
-                       </a>
-                      @endcan
-                       <div class="modal fade" id="ss-edit-amount-{{ $fee->id }}">
-                        <div class="modal-dialog modal-lg">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                              <h4 class="modal-title">Edit Fee Amount</h4>
-                              <button amount="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                              </button>
-                            </div>
-                            <div class="modal-body">
-                                 @php
-                                      $fee_in_tzs = [
-                                         'placeholder'=>'Amount in TZS',
-                                         'class'=>'form-control',
-                                         'required'=>true
-                                      ];
+					@if(Auth::user()->hasRole('administrator') || Auth::user()->hasRole('arc'))					
+						<td>
+							@foeach($campuses as $campus)
+								@if($fee->campusProgram->campus_id == $campus->id)
+									{{ $campus->name }}
+									@break
+								@endif	
+							@endforeach	
+						</td>
+					@elseif(Auth::user()->hasRole('finance-officer'))	
+						<td>
+						  @can('edit-programme-fee')
+						  <a class="btn btn-info btn-sm" href="#" data-toggle="modal" data-target="#ss-edit-amount-{{ $fee->id }}">
+								  <i class="fas fa-pencil-alt">
+								  </i>
+								  Edit
+						   </a>
+						  @endcan
+						   <div class="modal fade" id="ss-edit-amount-{{ $fee->id }}">
+							<div class="modal-dialog modal-lg">
+							  <div class="modal-content">
+								<div class="modal-header">
+								  <h4 class="modal-title">Edit Fee Amount</h4>
+								  <button amount="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								  </button>
+								</div>
+								<div class="modal-body">
+									 @php
+										  $fee_in_tzs = [
+											 'placeholder'=>'Amount in TZS',
+											 'class'=>'form-control',
+											 'required'=>true
+										  ];
 
-                                      $fee_in_usd = [
-                                         'placeholder'=>'Amount in USD',
-                                         'class'=>'form-control',
-                                         'required'=>true
-                                      ];
-                                  @endphp
-                                  {!! Form::open(['url'=>'finance/program-fee/update','class'=>'ss-form-processing']) !!}
+										  $fee_in_usd = [
+											 'placeholder'=>'Amount in USD',
+											 'class'=>'form-control',
+											 'required'=>true
+										  ];
+									  @endphp
+									  {!! Form::open(['url'=>'finance/program-fee/update','class'=>'ss-form-processing']) !!}
 
-                                    <div class="row">
-                                      <div class="form-group col-3">
-                                        {!! Form::label('','Programme') !!}
-                                        <select name="campus_program_id" class="form-control">
-                                          <option value="">Select Program</option>
-                                          @foreach($campus_programs as $program)
-                                            <option value="{{ $program->id }}" @if($program->id == $fee->campus_program_id) selected="selected" @endif>{{ $program->program->name }}</option>
-                                          @endforeach
-                                        </select>
-                                      </div>
-                                      <div class="form-group col-3">
-                                        {!! Form::label('','Amount in TZS') !!}
-                                        {!! Form::text('amount_in_tzs',$fee->amount_in_tzs,$fee_in_tzs) !!}
+										<div class="row">
+										  <div class="form-group col-3">
+											{!! Form::label('','Programme') !!}
+											<select name="campus_program_id" class="form-control">
+											  <option value="">Select Program</option>
+											  @foreach($campus_programs as $program)
+												<option value="{{ $program->id }}" @if($program->id == $fee->campus_program_id) selected="selected" @endif>{{ $program->program->name }}</option>
+											  @endforeach
+											</select>
+										  </div>
+										  <div class="form-group col-3">
+											{!! Form::label('','Amount in TZS') !!}
+											{!! Form::text('amount_in_tzs',$fee->amount_in_tzs,$fee_in_tzs) !!}
 
-                                        {!! Form::input('hidden','program_fee_id',$fee->id) !!}
-                                        {!! Form::input('hidden','year',$fee->studyAcademicYear->academicYear->year) !!}
-                                      </div>
-                                      <div class="form-group col-3">
-                                        {!! Form::label('','Amount in USD') !!}
-                                        {!! Form::text('amount_in_usd',$fee->amount_in_usd,$fee_in_usd) !!}
-                                      </div>
-                                      <div class="form-group col-3">
-                                        {!! Form::label('','Fee item') !!}
-                                        <select name="fee_item_id" class="form-control">
-                                          <option value="">Select Fee Item</option>
-                                          @foreach($fee_items as $item)
-                                            <option value="{{ $item->id }}" @if($fee->fee_item_id == $item->id) selected="selected" @else disabled="disabled" @endif>{{ $item->name }}</option>
-                                          @endforeach
-                                        </select>
-                                      </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="form-group col-4">
-                                        {!! Form::label('','Study academic year') !!}
-                                        <select name="study_academic_year_id" class="form-control">
-                                          <option value="">Select Study Academic Year</option>
-                                          @foreach($study_academic_years as $year)
-                                            <option value="{{ $year->id }}" @if($year->id == $fee->study_academic_year_id) selected="selected" @endif>{{ $year->academicYear->year }}</option>
-                                          @endforeach
-                                        </select>
-                                      </div>
-                                      <div class="form-group col-4">
-                                        {!! Form::label('','Year of study') !!}
-                                        <select name="year_of_study" class="form-control">
-                                          <option value="1" @if($fee->year_of_study == 1) selected="selected" @endif>1</option>
-                                          <option value="2" @if($fee->year_of_study == 2) selected="selected" @endif>2</option>
-                                          <option value="3" @if($fee->year_of_study == 3) selected="selected" @endif>3</option>
-                                        </select>
-                                      </div>
-                                      </div>
-                                      <div class="ss-form-actions">
-                                       <button amount="submit" class="btn btn-primary">{{ __('Save Changes') }}</button>
-                                      </div>
-                                {!! Form::close() !!}
+											{!! Form::input('hidden','program_fee_id',$fee->id) !!}
+											{!! Form::input('hidden','year',$fee->studyAcademicYear->academicYear->year) !!}
+										  </div>
+										  <div class="form-group col-3">
+											{!! Form::label('','Amount in USD') !!}
+											{!! Form::text('amount_in_usd',$fee->amount_in_usd,$fee_in_usd) !!}
+										  </div>
+										  <div class="form-group col-3">
+											{!! Form::label('','Fee item') !!}
+											<select name="fee_item_id" class="form-control">
+											  <option value="">Select Fee Item</option>
+											  @foreach($fee_items as $item)
+												<option value="{{ $item->id }}" @if($fee->fee_item_id == $item->id) selected="selected" @else disabled="disabled" @endif>{{ $item->name }}</option>
+											  @endforeach
+											</select>
+										  </div>
+										</div>
+										<div class="row">
+											<div class="form-group col-4">
+											{!! Form::label('','Study academic year') !!}
+											<select name="study_academic_year_id" class="form-control">
+											  <option value="">Select Study Academic Year</option>
+											  @foreach($study_academic_years as $year)
+												<option value="{{ $year->id }}" @if($year->id == $fee->study_academic_year_id) selected="selected" @endif>{{ $year->academicYear->year }}</option>
+											  @endforeach
+											</select>
+										  </div>
+										  <div class="form-group col-4">
+											{!! Form::label('','Year of study') !!}
+											<select name="year_of_study" class="form-control">
+											  <option value="1" @if($fee->year_of_study == 1) selected="selected" @endif>1</option>
+											  <option value="2" @if($fee->year_of_study == 2) selected="selected" @endif>2</option>
+											  <option value="3" @if($fee->year_of_study == 3) selected="selected" @endif>3</option>
+											</select>
+										  </div>
+										  </div>
+										  <div class="ss-form-actions">
+										   <button amount="submit" class="btn btn-primary">{{ __('Save Changes') }}</button>
+										  </div>
+									{!! Form::close() !!}
 
-                            </div>
-                            <div class="modal-footer justify-content-between">
-                              <button amount="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            </div>
-                          </div>
-                          <!-- /.modal-content -->
-                        </div>
-                        <!-- /.modal-dialog -->
-                      </div>
-                      <!-- /.modal -->
-                      
-                      @can('delete-programme-fee')
-                      <a class="btn btn-danger btn-sm" href="#" data-toggle="modal" data-target="#ss-delete-amount-{{ $fee->id }}">
-                              <i class="fas fa-trash">
-                              </i>
-                              Delete
-                       </a>
-                      @endcan
-                       <div class="modal fade" id="ss-delete-amount-{{ $fee->id }}">
-                        <div class="modal-dialog modal-lg">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                              <h4 class="modal-title"><i class="fa fa-exclamation-sign"></i> Confirmation Alert</h4>
-                              <button amount="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                              </button>
-                            </div>
-                            <div class="modal-body">
-                              <div class="row">
-                                <div class="col-12">
-                                    <div id="ss-confirmation-container">
-                                       <p id="ss-confirmation-text">Are you sure you want to delete this fee amount from the list?</p>
-                                       <div class="ss-form-controls">
-                                         <button amount="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                                         <a href="{{ url('finance/program-fee/'.$fee->id.'/destroy?campus_program_id='.$fee->campusProgram->id.'&year='.$fee->studyAcademicYear->academicYear->year.'&year_of_study='.$fee->year_of_study.'&study_academic_year_id='.$fee->studyAcademicYear->id) }}" class="btn btn-danger">Delete</a>
-                                         </div><!-- end of ss-form-controls -->
-                                      </div><!-- end of ss-confirmation-container -->
-                                  </div><!-- end of col-md-12 -->
-                               </div><!-- end of row -->
-                            </div>
-                            <div class="modal-footer justify-content-between">
-                              <button amount="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            </div>
-                          </div>
-                          <!-- /.modal-content -->
-                        </div>
-                        <!-- /.modal-dialog -->
-                      </div>
-                      <!-- /.modal -->
-                      <!-- <a class="btn btn-info btn-sm" href="{{ url('finance/program-fee/'.$fee->id.'/structure') }}">
-                              <i class="fas fa-list-alt">
-                              </i>
-                              Fee Structure
-                       </a> -->
-                    </td>
+								</div>
+								<div class="modal-footer justify-content-between">
+								  <button amount="button" class="btn btn-default" data-dismiss="modal">Close</button>
+								</div>
+							  </div>
+							  <!-- /.modal-content -->
+							</div>
+							<!-- /.modal-dialog -->
+						  </div>
+						  <!-- /.modal -->
+						  
+						  @can('delete-programme-fee')
+						  <a class="btn btn-danger btn-sm" href="#" data-toggle="modal" data-target="#ss-delete-amount-{{ $fee->id }}">
+								  <i class="fas fa-trash">
+								  </i>
+								  Delete
+						   </a>
+						  @endcan
+						   <div class="modal fade" id="ss-delete-amount-{{ $fee->id }}">
+							<div class="modal-dialog modal-lg">
+							  <div class="modal-content">
+								<div class="modal-header">
+								  <h4 class="modal-title"><i class="fa fa-exclamation-sign"></i> Confirmation Alert</h4>
+								  <button amount="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								  </button>
+								</div>
+								<div class="modal-body">
+								  <div class="row">
+									<div class="col-12">
+										<div id="ss-confirmation-container">
+										   <p id="ss-confirmation-text">Are you sure you want to delete this fee amount from the list?</p>
+										   <div class="ss-form-controls">
+											 <button amount="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+											 <a href="{{ url('finance/program-fee/'.$fee->id.'/destroy?campus_program_id='.$fee->campusProgram->id.'&year='.$fee->studyAcademicYear->academicYear->year.'&year_of_study='.$fee->year_of_study.'&study_academic_year_id='.$fee->studyAcademicYear->id) }}" class="btn btn-danger">Delete</a>
+											 </div><!-- end of ss-form-controls -->
+										  </div><!-- end of ss-confirmation-container -->
+									  </div><!-- end of col-md-12 -->
+								   </div><!-- end of row -->
+								</div>
+								<div class="modal-footer justify-content-between">
+								  <button amount="button" class="btn btn-default" data-dismiss="modal">Close</button>
+								</div>
+							  </div>
+							  <!-- /.modal-content -->
+							</div>
+							<!-- /.modal-dialog -->
+						  </div>
+						  <!-- /.modal -->
+						  <!-- <a class="btn btn-info btn-sm" href="{{ url('finance/program-fee/'.$fee->id.'/structure') }}">
+								  <i class="fas fa-list-alt">
+								  </i>
+								  Fee Structure
+						   </a> -->
+						</td>
+					@endif					
                   </tr>
                   @endforeach
                   
