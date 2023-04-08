9b51df2bfec957311320ec9d0c97b8c3049d819f
salimshady23 <slimshadysaid@gmail.com>
2023-04-08 12:04:38 +0100

    comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -386,13 +386,14 @@ class ApplicantController extends Controller
 				foreach($invoices as $invoice){
 					if(str_contains($invoice->feeType->name,'Tuition Fee')){
 						$paid_amount = GatewayPayment::where('bill_id',$invoice->reference_no)->sum('paid_amount');
+						return $paid_amount;
 						$fee_payment_percent = $paid_amount/$invoice->amount;         
 
 						if($loan_allocation){
 						   $fee_payment_percent = ($paid_amount+$loan_allocation->tuition_fee)/$invoice->amount;
 						}
 					}
-
+ 
 					if(str_contains($invoice->feeType->name,'Miscellaneous')){
 						$paid_amount = GatewayPayment::where('bill_id',$invoice->reference_no)->sum('paid_amount');
 						$other_fee_payment_status = $paid_amount >= $invoice->amount? 1 : 0;
