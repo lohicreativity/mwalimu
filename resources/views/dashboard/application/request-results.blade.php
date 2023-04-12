4444b5fe8264c337fa9206347af9fad4c02829a2
salimshady23 <slimshadysaid@gmail.com>
2023-04-12 17:32:26 +0100

    comments

--- resources/views/dashboard/application/request-results.blade.php
+++ resources/views/dashboard/application/request-results.blade.php
@@ -241,7 +241,7 @@
                   <div class="row">
                   <div class="form-group col-4">
                     {!! Form::label('','AVN') !!}
-                    {!! Form::text('avn',null,$avn) !!}
+                    {!! Form::text('avn', @if(nacte_results) nacte_results[0]->avn @endif,$avn) !!}
                   </div>
                   <div class="form-group col-4">
 
