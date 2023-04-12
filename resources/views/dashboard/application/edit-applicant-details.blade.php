8bcc49cbb1ff1f3d82312ae307d25a3f998cd375
salimshady23 <slimshadysaid@gmail.com>
2023-04-12 13:15:32 +0100

    comments

--- resources/views/dashboard/application/edit-applicant-details.blade.php
+++ resources/views/dashboard/application/edit-applicant-details.blade.php
@@ -106,7 +106,7 @@
                    <div class="row">
                     <div class="form-group col-4">
 					   {!! Form::label('','Date of Birth') !!}
-					   {!! Form::text('dob',App\Utils\DateMaker::toStandardDate($applicant->birth_date),$dob) !!}					
+					   {!! Form::text('dob',App\Utils\DateMaker::toDBDate($applicant->birth_date),$dob) !!}					
                     </div>
                     <div class="form-group col-4">
                        {!! Form::label('','Nationality') !!}
