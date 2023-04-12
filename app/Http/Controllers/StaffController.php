e5a8c363d8f7a7f39eadc95260a8a15435a3aee2
salimshady23 <slimshadysaid@gmail.com>
2023-04-12 15:20:35 +0100

    comments

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -239,7 +239,6 @@ class StaffController extends Controller
 	
     public function viewPayerDetails(Request $request)
     {
-		return Auth::user()->hasRole('staff');
 		if(!empty($request->keyword)){
 			$student_payer = Student::where('registration_number', $request->keyword)
 			->orWhere('surname',$request->keyword)
--- resources/views/layouts/auth-nav.blade.php
+++ resources/views/layouts/auth-nav.blade.php
@@ -234,7 +234,7 @@
               </li>
             </ul>
           </li>
-		  
+		  {{ $student }}		  
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-coins"></i>
