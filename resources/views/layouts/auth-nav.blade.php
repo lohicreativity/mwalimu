98d5ce0cd2096d9aca2c58c7ab3ebb87b908f86b
salimshady23 <slimshadysaid@gmail.com>
2023-04-12 15:27:42 +0100

    comments

--- resources/views/layouts/auth-nav.blade.php
+++ resources/views/layouts/auth-nav.blade.php
@@ -234,7 +234,7 @@
               </li>
             </ul>
           </li>
-		  {{ $student }}		  
+		  {{ $student->applicant }}		  
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-coins"></i>
