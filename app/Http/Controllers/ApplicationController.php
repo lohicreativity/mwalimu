848d6c0793f5730d5dc21bbba35871482ae49581
salimshady23 <slimshadysaid@gmail.com>
2023-04-14 14:50:04 +0100

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -7424,7 +7424,7 @@ class ApplicationController extends Controller
 								->where('application_window_id',$application_window->id);})->with('selections.campusProgram.program')->where('status','ADMITTED')
 								->where('application_window_id',$application_window->id)->where('campus_id', $staff->campus_id)
 								->where('index_number',$request->keyword)->orWhere('surname',$request->keyword)->first();
-		$student = Student::whereDoesntHave('registrations', function($query) use($ac_year, $semester){$query->where('semester_id',$semester_id)->where('study_academic_year_id',$ac_year->id);})
+		$student = Student::whereDoesntHave('registrations', function($query) use($ac_year, $semester){$query->where('semester_id',$semester->id)->where('study_academic_year_id',$ac_year->id);})
 							->orWhereHas('registrations', function($query) use($ac_year, $semester){$query->where('status','UNREGISTERED')->where('semester_id',$semester->id)->where('study_academic_year_id',$ac_year->id);})
 							->whereHas('studentshipStatus', function($query){$query->where('name','ACTIVE')->OrWhere('name','RESUMED');})
 							->whereHas('academicStatus', function($query){$query->where('name','!=','FAIL&DISCO')->orWhere('name','!=','DECEASED');})
