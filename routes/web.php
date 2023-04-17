e679ddc8333ca1dbcfd6db43704edb9771a9fdf9
salimshady23 <slimshadysaid@gmail.com>
2023-04-17 17:48:34 +0100

    comments

--- routes/web.php
+++ routes/web.php
@@ -39,11 +39,11 @@ Route::get('batch-processing', function (Request $request) {
 	//						->where('program_level_id',$request->get('program_level_id'))->first(); // from API
      $batch = ApplicantProgramSelection::whereHas('applicant',function($query) use($request){
 	 $query->where('program_level_id',1);})
-	 ->where('application_window_id', 1)->where('status', 'SELECTED')->first();
+	 ->where('application_window_id', 2)->where('status', 'SELECTED')->first();
 	      
 	$current_batch = $batch->batch_no + 1;
 	
-	$applicant = Applicant::where('index_number','S0836/0008/2019')->where('application_window_id', 1)
+	$applicant = Applicant::where('index_number','S0836/0008/2019')->where('application_window_id', 2)
 							->where('program_level_id',1)->first(); //Imitation of the previous statement
 	if($applicant){
 //	   $applicant->multiple_admissions = $data['AdmissionStatusCode'] == 225 ? 1 : 0; // from API
@@ -54,9 +54,9 @@ Route::get('batch-processing', function (Request $request) {
 	}
 
     ApplicantProgramSelection::whereHas('applicant',function($query) use($request){$query->where('program_level_id',1);})
-							 ->where('application_window_id', 1)->where('batch_no', 0)->update(['batch_no' => $current_batch]);
+							 ->where('application_window_id', 2)->where('batch_no', 0)->update(['batch_no' => $current_batch]);
      
-	Applicant::where('application_window_id', 1)->where(function($query) {$query->where('status', null)->orWhere('status', 'SELECTED');})
+	Applicant::where('application_window_id', 2)->where(function($query) {$query->where('status', null)->orWhere('status', 'SELECTED');})
 			 ->where('program_level_id',1)->where('batch_no', 0)->update(['batch_no' => $current_batch]);
 
 
