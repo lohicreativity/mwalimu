87d8bb223319b3016702bbabe94e085cdc3283e5
salimshady23 <slimshadysaid@gmail.com>
2023-04-17 17:39:45 +0100

    comments

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,7 @@ Route::get('batch-processing', function (Request $request) {
 //	   $applicant->multiple_admissions = $data['AdmissionStatusCode'] == 225 ? 1 : 0; // from API
 	   $applicant->multiple_admissions = 1; //Imitation of the previous statement
 	   $applicant->save();
-
+		return 1;
 	   $selection = ApplicantProgramSelection::where('applicant_id',$applicant)->where('status','APPROVING')->update(['status'=>'SELECTED']);
 
 	}
