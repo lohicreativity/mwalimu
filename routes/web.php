74d6c67206d83508f252e7e9c0c279279d198a71
salimshady23 <slimshadysaid@gmail.com>
2023-04-17 17:36:03 +0100

    comments

--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,7 @@ Route::get('test',function(){
 	// return $response;
 
 	// return $result;
-	 $acpac = new ACPACService;
+	// $acpac = new ACPACService;
 
 	// //$acpac->query("INSERT INTO receipts (BANK,BANKNAME,RCPNUMBER,RCPDATE,RCPDESC,IDCUST,NAMECUST,INVOICE,AMTAPPLIED,IMPORTED,IMPDATE) VALUES
  //   //('J','CRDB','REC03','10','TF','MNMA003','TEST','INV003','100.0','C','10')");
@@ -87,7 +87,7 @@ Route::get('test',function(){
 	// $acpac->query("DELETE FROM customer");
 	// $acpac->query("DELETE FROM invoices");
 	// $acpac->query("DELETE FROM receipts");
-	$results = $acpac->query('SELECT * FROM customer');
+/* 	$results = $acpac->query('SELECT * FROM customer');
      while ($row = sqlsrv_fetch_array($results)) {
      	print_r($row);
     }
@@ -104,7 +104,7 @@ Route::get('test',function(){
     $results = $acpac->query('SELECT * FROM receipts');
      while ($row = sqlsrv_fetch_array($results)) {
      	print_r($row);
-    }
+    } */
 
 	//$acpac->query("INSERT INTO customer (IDCUST,IDGRP,NAMECUST,TEXTSTRE1,TEXTSTRE2,TEXTSTRE3,TEXTSTRE4,NAMECITY,CODESTTE,CODEPSTL,CODECTRY,NAMECTAC,TEXTPHON1,TEXTPHON2,CODETERR,IDACCTSET,CODECURN,EMAIL1,EMAIL2) VALUES ('BTBA000122','16722','LEOPOLD, LEONTINE ','ILEMELA','ARUMERU','BANG','9193 DSM','Tanzania','Tanzania','ILEMELA','Tanzania','NGWARA, NYAMBOHA KIKARO','255754991909','0754991909','BTC.BA','STD','TSH','yusufu.erick@mnma.ac.tz','leontine97@gmail.com')");
 	// $acpac->query("DELETE FROM customer");
