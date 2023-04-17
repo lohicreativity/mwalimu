943be12ae0d6c24ce0bf7ca44a405f4baf933df5
salimshady23 <slimshadysaid@gmail.com>
2023-04-17 14:36:33 +0100

    comments

--- resources/views/dashboard/finance/loan-beneficiaries.blade.php
+++ resources/views/dashboard/finance/loan-beneficiaries.blade.php
@@ -81,7 +81,8 @@
             <div class="card">
               <div class="card-header">
                 <h3 class="card-title">{{ (List of Loan Beneficiaries) }}</h3>
-                <a href="{{ url('finance/download-loan-beneficiaries?study_academic_year_id='.$request->get('study_academic_year_id').'&year_of_study='.$request->get('year_of_study')) }}" class="btn btn-primary">Download Loan Beneficiaries</a>				
+                <a href="{{ url('finance/download-loan-beneficiaries?study_academic_year_id='.$request->get('study_academic_year_id').'&year_of_study='.$request->get('year_of_study')) }}" class="btn btn-primary">Download Loan Beneficiaries</a>
+				
               </div>
               <!-- /.card-header -->
               <div class="card-body">                  
