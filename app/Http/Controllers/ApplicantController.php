69388174f47242ea4b4614085d6fb3522d4166d5
salimshady23 <slimshadysaid@gmail.com>
2023-04-20 15:42:21 +0100

    comments

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -332,14 +332,14 @@ class ApplicantController extends Controller
               $applicant->save();
             }
         }
- Return $applicant;
+
         $regulator_status = Applicant::where('program_level_id', $applicant->program_level_id)
 								->whereHas('selections', function ($query) {$query->where('status', 'SELECTED')
 								->orWhere('status', 'PENDING');})
 								->where('application_window_id', $applicant->application_window_id)
 								->where('intake_id', $applicant->intake_id)
-								->count();
-								
+								->get();
+		return 	$regulator_status;					
 		$selected_applicants = Applicant::where('program_level_id', $applicant->program_level_id)
 						->whereHas('selections',function($query) use($applicant){$query->where('application_window_id',$applicant->application_window_id);})
 						->where('application_window_id', $applicant->application_window_id)
