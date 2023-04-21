860c21b90db2bfff90146e3b9c7e44a27ed1bca3
salimshady23 <slimshadysaid@gmail.com>
2023-04-21 09:24:16 +0100

    comments

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -67,7 +67,7 @@ class ModuleController extends Controller
 		$existing_module_record = Module::with('departments')->where('id', $module->id)
 										->whereHas('departments', function($query) use($request){$query->where('campus_id',$request->get('campus_id'));})->first();
 		
-			return $existing_module_record->departments;
+			return $existing_module_record->departments->name;
 		
 		if($existing_module_record){
 			return redirect()->back()->with('error','The module has already been assigned to another department');
