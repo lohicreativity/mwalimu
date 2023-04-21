434be09b8f9d906d01d4974f482b3625b9b17e3b
salimshady23 <slimshadysaid@gmail.com>
2023-04-21 17:36:14 +0100

    comments

--- resources/views/dashboard/academic/reports/students-with-course-work.blade.php
+++ resources/views/dashboard/academic/reports/students-with-course-work.blade.php
@@ -263,24 +263,24 @@
                 </div><!-- end of table-responsive -->
                 
                 
-
-
-          </div><!-- end of col-md-12 -->
-        </div><!-- end of row -->
           <div class="col-md-6">
              <div class="ss-bold ss-left">
                  <h3>Name of Head of Department: .................................</h3>
-                 <h3>Signature of Head of Department: ..............................</h3>
+                 <h3>Signature: ..............................</h3>
                  <h3>Date: ..................................</h3>
              </div>
           </div><!--end of col-md-6 -->
           <div class="col-md-6">
              <div class="ss-bold ss-left">
-                 <h3>Name of Examination Officer: .................................</h3>
-                 <h3>Signature of Examination Officer: .............................</h3>
+                 <h3>Name of the Lecturer: .................................</h3>
+                 <h3>Signature: .............................</h3>
                  <h3>Date: ..................................</h3>
              </div>
           </div><!--end of col-md-6 -->
+
+          </div><!-- end of col-md-12 -->
+        </div><!-- end of row -->
+
 
       </div><!-- end of container -->
 
