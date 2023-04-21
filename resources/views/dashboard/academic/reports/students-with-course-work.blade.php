10ca0ffe4a7f71ab3ffb2004843fb071d67077c2
salimshady23 <slimshadysaid@gmail.com>
2023-04-21 17:41:52 +0100

    comments

--- resources/views/dashboard/academic/reports/students-with-course-work.blade.php
+++ resources/views/dashboard/academic/reports/students-with-course-work.blade.php
@@ -263,14 +263,14 @@
                 </div><!-- end of table-responsive -->
                 
                 
-          <div class="col-md-10">
+          <div class="col-md-9">
              <div class="ss-left">
                  <h3>Name of Head of Department: .................................</h3>
                  <h3>Signature: ..............................</h3>
                  <h3>Date: ..................................</h3>
              </div>
           </div><!--end of col-md-6 -->
-          <div class="col-md-2">
+          <div class="col-md-3">
              <div class="ss-left">
                  <h3>Name of the Lecturer: .................................</h3>
                  <h3>Signature: .............................</h3>
