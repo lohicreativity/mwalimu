a0b469d1385add01d9d6d76f4cfe619bdc9914d7
salimshady23 <slimshadysaid@gmail.com>
2023-04-25 09:40:03 +0300

    comments

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -303,7 +303,7 @@ class ApplicationController extends Controller
 		$applicants = null;
          if (Auth::user()->hasRole('administrator')|| Auth::user()->hasRole('arc')) {
 
-            $applicants = Applicant::doesntHave('student')->whereHas('selections',function($query) use($request){
+            $applicants = Applicant::whereHas('selections',function($query) use($request){
                 $query->where('status','SELECTED');
            })->with(['disabilityStatus','ward','region','country','nextOfKin','intake','selections.campusProgram.program','nectaResultDetails','nacteResultDetails'])->where('application_window_id',$request->get('application_window_id'))->where('program_level_id',$request->get('program_level_id'))->where(function($query){
                $query->where('confirmation_status','!=','CANCELLED')->orWhere('confirmation_status','!=','TRANSFERED')->orWhereNull('confirmation_status');
