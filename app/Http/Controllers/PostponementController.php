7d541250391fb029cdb9aa9bb07a70ab783b1d11
salimshady23 <slimshadysaid@gmail.com>
2023-04-26 16:04:58 +0300

    comments

--- app/Http/Controllers/PostponementController.php
+++ app/Http/Controllers/PostponementController.php
@@ -34,12 +34,14 @@ class PostponementController extends Controller
                 $query->where('first_name','LIKE','%'.$request->get('query').'%')->orWhere('middle_name','LIKE','%'.$request->get('query').'%')->orWhere('surname','LIKE','%'.$request->get('query').'%')->orWhere('registration_number','LIKE','%'.$request->get('query').'%');
           })->whereHas('student.campusProgram', function($query) use($staff){$query->where('campus_id',$staff->campus_id);})
           ->with(['student','StudyAcademicYear.academicYear','semester'])->where('study_academic_year_id',$request->get('study_academic_year_id'))->get() : Postponement::with(['student','StudyAcademicYear.academicYear','semester'])->where('study_academic_year_id',$request->get('study_academic_year_id'))->whereNull('postponed_by_user_id')->get();
+        return $postponements;
         }elseif(Auth::user()->hasRole('hod')){
             $postponements = $request->get('query')? Postponement::whereHas('student',function($query) use($request){
                 $query->where('first_name','LIKE','%'.$request->get('query').'%')->orWhere('middle_name','LIKE','%'.$request->get('query').'%')->orWhere('surname','LIKE','%'.$request->get('query').'%')->orWhere('registration_number','LIKE','%'.$request->get('query').'%');
           })->whereHas('student.campusProgram', function($query) use($staff){$query->where('campus_id',$staff->campus_id);})
           ->whereHas('student.campusProgram.program.departments', function($query) use($staff){$query->where('id',$staff->department_id);})
           ->with(['student','StudyAcademicYear.academicYear','semester'])->where('study_academic_year_id',$request->get('study_academic_year_id'))->get() : Postponement::with(['student','StudyAcademicYear.academicYear','semester'])->where('study_academic_year_id',$request->get('study_academic_year_id'))->whereNull('postponed_by_user_id')->get();
+          return $postponements;
         }else{
             $postponements = [];
         }
