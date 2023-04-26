b653e8f501e66373ee307f26620bdc352208715f
salimshady23 <slimshadysaid@gmail.com>
2023-04-26 16:58:41 +0300

    comments

--- resources/views/dashboard/academic/postponements.blade.php
+++ resources/views/dashboard/academic/postponements.blade.php
@@ -62,7 +62,7 @@
               </div>
             </div>
             <!-- /.card -->
-
+@if(count($postponements)>0)
  
             <div class="card">
               <div class="card-header">
@@ -71,11 +71,21 @@
               <!-- /.card-header -->
                  
               <div class="card-body">
+                {!! Form::open(['url'=>'academic/postponements','method'=>'GET']) !!}
+                {!! Form::input('hidden','study_academic_year_id',$request->get('study_academic_year_id')) !!}
+                <div class="input-group ss-stretch">
+                 <input type="text" name="query" class="form-control" placeholder="Search for student name or registration number">
+                 <span class="input-group-btn">
+                   <button class="btn btn-default" type="submit"><span class="fa fa-search"></span></button>
+                 </span>
+                </div>
+                {!! Form::close() !!}
+
                 {!! Form::open(['url'=>'academic/accept-postponements','class'=>'ss-form-processing']) !!}
 
                 {!! Form::input('hidden','study_academic_year_id',$request->get('study_academic_year_id')) !!}
                  
-                <table id="example2" class="table table-bordered ss-margin-top ss-paginated-table">
+                <table id="example2" class="table table-bordered table-hover ss-margin-top ss-paginated-table">
                   <thead>
                   <tr>
                     <th>Student</th>
@@ -206,7 +216,7 @@
                     @endif 
                   </tr>
                   @endforeach
-
+                  @if(Auth::user()->hasRole('hod') || Auth::user()->hasRole('arc') || Auth::user()->hasRole('administrator'))
                   @if(!Auth::user()->hasRole('hod'))
                     @if(!$request->get('query'))
                      <tr>
@@ -218,7 +228,7 @@
                      </tr>
                     @endif
                   @endif 
-                  
+                  @endif
                   </tbody>
                 </table>
                 {!! Form::close() !!}
@@ -227,7 +237,7 @@
               
             </div>
             <!-- /.card -->
-
+@endif
           </div>
           <!-- /.col -->
         </div>
