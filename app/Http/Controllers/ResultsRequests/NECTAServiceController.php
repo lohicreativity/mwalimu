a8bd8a4181b1a1bd4f1749afa0a92ad62e94cd42
salimshady23 <slimshadysaid@gmail.com>
2023-05-03 20:30:47 +0300

    comments

--- app/Http/Controllers/ResultsRequests/NECTAServiceController.php
+++ app/Http/Controllers/ResultsRequests/NECTAServiceController.php
@@ -113,7 +113,7 @@ class NECTAServiceController extends Controller
         }
         if($details = NectaResultDetail::with('results')->where('index_number',str_replace('-','/',$index_number))
                                        ->where('exam_id',$exam_id)->where('applicant_id',$request->get('applicant_id'))->first()){
-
+            return response()->json(['details'=>$details,'exists'=>1]);
         }else{                
             try{
                 // $token = $this->getToken(config('constants.NECTA_API_KEY'));
