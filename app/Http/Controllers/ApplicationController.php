159af2f9fa9fd0ba172f2ea831030ffe4828c4a7
lohicreativity <lohi.sanday@gmail.com>
2023-08-02 21:26:59 +0300

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -149,15 +149,14 @@ class ApplicationController extends Controller
                                                 'basic_info_complete_status','next_of_kin_complete_status','payment_complete_status','results_complete_status','program_level_id')
                                                 ->with(['selections:id,order,campus_program_id,applicant_id','selections.campusProgram:id,program_id',
                                                 'selections.campusProgram.program:id,code','programLevel:id,name'])->where('programs_complete_status',0)->where('submission_complete_status',0)
-                                                ->where('application_window_id',$request->get('application_window_id'))->where('campus_id',$staff->campus_id)->paginate(1500);
-
+                                                ->where('application_window_id',$request->get('application_window_id'))->where('campus_id',$staff->campus_id)
+                                                ->whereHas('selections.campusProgram.program.departments',function($query) use($staff){$query->where('department_id',$staff->department_id);})->paginate(1500);
             }else{
                 $applicants = Applicant::select('id','first_name','surname','index_number','gender','phone','batch_id','submission_complete_status','programs_complete_status',
                                                 'basic_info_complete_status','next_of_kin_complete_status','payment_complete_status','results_complete_status','program_level_id')
                                                 ->with(['selections:id,order,campus_program_id,applicant_id','selections.campusProgram:id,program_id','selections.campusProgram.program:id,code',
                                                 'programLevel:id,name'])->where('programs_complete_status',0)->where('submission_complete_status',0)
-                                                ->where('application_window_id',$request->get('application_window_id'))->where('campus_id',$staff->campus_id)
-                                                ->whereHas('selections.campusProgram.program.departments',function($query) use($staff){$query->where('department_id',$staff->department_id);})->paginate(1500);
+                                                ->where('application_window_id',$request->get('application_window_id'))->where('campus_id',$staff->campus_id)->paginate(1500);
 
             }
         
