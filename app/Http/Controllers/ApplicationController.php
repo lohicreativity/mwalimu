a42d35b2ae386f531cde0d90eff860b69e8451ff
lohicreativity <lohi.sanday@gmail.com>
2023-08-13 11:53:40 +0300

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -1062,10 +1062,10 @@ class ApplicationController extends Controller
                                     ->where('status','APPROVING');})
                                     ->where('program_level_id',$request->get('program_level_id'))->where('campus_id',$staff->campus_id) */
                                     ->whereIn('id',$request->get('applicant_ids'))
-                                    ->with(['nextOfKin:id,first_name,surname','region:id,name','district:id,name','intake','selections.campusProgram.program.ntaLevel'=>function($query){
-                                        $query->where('status','APPROVING');},
+                                    ->with(['nextOfKin:id,first_name,surname','region:id,name','district:id,name','intake','selections.campusProgram.program.ntaLevel',
                                             'nectaResultDetails:id,applicant_id,index_number,verified,exam_id','nacteResultDetails:id,applicant_id,verified,registration_number,diploma_graduation_year',
-                                            'outResultDetails:id,applicant_id,verified','disabilityStatus:id,name','selections:id,status,campus_program_id,applicant_id','selections.campusProgram:id,regulator_code'])->get();
+                                            'outResultDetails:id,applicant_id,verified','disabilityStatus:id,name','selections:id,status,campus_program_id,applicant_id'=>function($query){
+                                                $query->where('status','APPROVING');},'selections.campusProgram:id,regulator_code'])->get();
 return $applicants;
             foreach($applicants as $applicant){
 
