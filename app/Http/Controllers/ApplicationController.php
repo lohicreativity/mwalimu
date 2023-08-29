42019e019c24b1b33b90fadfc54987a4274a1604
lohicreativity <lohi.sanday@gmail.com>
2023-08-29 20:30:33 +0300

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4688,7 +4688,7 @@ class ApplicationController extends Controller
 
         $numberToWords = new NumberToWords();
         $numberTransformer = $numberToWords->getNumberTransformer('en');
-
+return $applicant->selections[0]->campusProgram->campus->name;
         $data = [
          'applicant'=>$applicant,
          'campus_name'=>$applicant->selections[0]->campusProgram->campus->name,
