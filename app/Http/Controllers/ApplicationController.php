65db88ac54d4bc0e234deb0871717ccee1695f69
lohicreativity <lohi.sanday@gmail.com>
2023-09-06 23:40:14 +0300

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -8645,7 +8645,7 @@ class ApplicationController extends Controller
                 'outResultDetails'=>function($query){$query->select('id','applicant_id')->where('verified',1);}])->get(); 
 
         $errors = ApplicantFeedBackCorrection::whereNotNull('verification_id')->get();
-return $errors;
+
         $tcu_username = $tcu_token = $nactvet_authorization_key = null;
         if($staff->campus_id == 1){
             $tcu_username = config('constants.TCU_USERNAME_KIVUKONI');
