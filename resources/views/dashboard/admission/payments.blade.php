2b21faeb5663031c35b3e8c01035b22a500865ce
lohicreativity <lohi.sanday@gmail.com>
2023-10-24 12:06:00 +0300

    Update payments.blade.php

--- resources/views/dashboard/admission/payments.blade.php
+++ resources/views/dashboard/admission/payments.blade.php
@@ -70,7 +70,7 @@
                        </td>
                        <td>
                          @if(!$loan_allocation)
-                         @if(isset($fee_paid_amount) && isset($program_fee_invoice))
+                         @if($fee_paid_amount> 0 && !empty($program_fee_invoice))
                             {{ number_format($program_fee_invoice->amount - $fee_paid_amount,0) }} TZS
                          @endif
                          @endif
