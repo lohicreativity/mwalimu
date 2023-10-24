3804206b98684e66ed9190b5a02ce0341e9264b6
lohicreativity <lohi.sanday@gmail.com>
2023-10-24 18:15:51 +0300

    Update StaffController.php

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -240,11 +240,11 @@ class StaffController extends Controller
 		if(!empty($request->keyword)){
             $applicant = Applicant::select('id')->where('index_number',$request->keyword)->latest()->first();
             $applicant_id = $applicant? $applicant->id : 0;
-
+            return $applicant_id;
 			$student_payer = Student::where('registration_number', $request->keyword)
 			->orWhere('surname',$request->keyword)->orWhere('applicant_id',$applicant_id)
 			->with(['applicant','campusProgram.program','studentShipStatus'])->first();
-			$applicant_payer = Applicant::doesntHave('student')->with(['programLevel','intake','disabilityStatus'])
+			$applicant_payer = Applicant::whereDoesntHave('student',function($query) use($applicant_id){$query->where('applicant_id',$applicant_id);})->with(['programLevel','intake','disabilityStatus'])
             ->where('index_number', $request->keyword)->orWhere('surname',$request->keyword)->latest()->first();
 
             if(!$student_payer && !$applicant_payer){
