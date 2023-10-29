45f443d523c0a9960239b49dca7eb659e3168ad6
eric_future <futureeric20@gmail.com>
2023-10-29 19:23:55 +0300

    Update id-card.blade.php

--- resources/views/dashboard/registration/reports/id-card.blade.php
+++ resources/views/dashboard/registration/reports/id-card.blade.php
@@ -264,7 +264,7 @@
             @endif
         </div>
         <div class="col-md-6" style="float: right;">
-            <p style="-webkit-text-stroke: 1px white; font-weight: bold; color: red; text-align: right; padding-right: 60px;font-size: 16px;">{{ $student->campusProgram->campus->name }}</p>
+            <p style="-webkit-text-stroke: 1px white; font-weight: bold; color: red; text-align: right; padding-right: 70px;font-size: 16px;">{{ $student->campusProgram->campus->name }}</p>
         </div>
     </div> 
    </div>
