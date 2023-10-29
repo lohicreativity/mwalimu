4393540214bbe9d50e7c9d75731c0e9a08af9483
eric_future <futureeric20@gmail.com>
2023-10-29 19:31:59 +0300

    Update id-card.blade.php

--- resources/views/dashboard/registration/reports/id-card.blade.php
+++ resources/views/dashboard/registration/reports/id-card.blade.php
@@ -235,7 +235,7 @@
      <div style="border:2px solid #1b2066; margin-top: -20px; margin-bottom: 10px;"></div>
 
      <div class="container" style="position: relative; z-index: 1000; margin-top: 30px;">
-     <div class="row" style="margin-top: -20px; margin-bottom: 15px;">
+     <div class="row" style="margin-top: -20px;">
         <div class="col-md-3" style="padding: 5px;">
           @if(file_exists(public_path().'/avatars/'.$student->image))
           <img src="{{ asset('avatars/'.$student->image)}}" class="ss-logo" style="text-align: center; width: 100px; padding-left: 10px;">
@@ -252,7 +252,7 @@
            <img src="{{ asset('signatures/'.$student->signature) }}" style="width: 100px; height: auto; margin-top: 5px;"></h5>
         </div>
      </div> 
-     <div class="row" style="background-color:#1b2066; margin-top: -1px; position:absolute; margin-right: -80px;">
+     <div class="row" style="background-color:#1b2066; margin-top: -10px; position:absolute; margin-right: -80px;">
         <div class="col-md-6"> @if($semester->name == 'Semester 1')
             <p style="text-align:left; color: white; font-weight: bold; padding-left: 5px; font-size: 16px;">Semester One</p>
             @else
