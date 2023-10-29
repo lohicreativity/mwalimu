58c182e742828852f8e05441dc53e9547b9cadd3
eric_future <futureeric20@gmail.com>
2023-10-29 18:11:06 +0300

    Update id-card.blade.php

--- resources/views/dashboard/registration/reports/id-card.blade.php
+++ resources/views/dashboard/registration/reports/id-card.blade.php
@@ -258,7 +258,7 @@
      </div> 
    </div>
     </div>
-    <div class="row" style="background-color:#1b2066; width:100%; height: 50px; margin-top: -15px; position:absolute;padding: 5px;">
+    <div class="row" style="background-color:#1b2066; width:100%; height: 5%; margin-top: -15px; position:absolute;padding: 5px;">
         <div class="col-md-6"> @if($semester->name == 'Semester 1')
             <h2 style="text-align:left; color: white; font-weight: bold; padding-left: 5px; font-size: 30px;">Semester One</h2>
             @else
