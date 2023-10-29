bd357622aa8d1133c8ffc40de6e004eb2d39b7ca
eric_future <futureeric20@gmail.com>
2023-10-29 17:24:46 +0300

    Update id-card.blade.php

--- resources/views/dashboard/registration/reports/id-card.blade.php
+++ resources/views/dashboard/registration/reports/id-card.blade.php
@@ -259,13 +259,13 @@
     </div>
      <div class="row" style="background-color:#1b2066; width:auto;">
         <div class="col-md-6"> @if($semester->name == 'Semester 1')
-            <h5 style="text-align:left; color: white; font-weight: bold; padding: 10px;">Semester One</h5>
+            <h5 style="text-align:left; color: white; font-weight: bold; padding-left: 20px;">Semester One</h5>
             @else
-                <h5 style="text-align:left; color: white; font-weight: bold; padding: 10px;">Semester Two</h5>
+                <h5 style="text-align:left; color: white; font-weight: bold; padding-left: 20px;">Semester Two</h5>
             @endif
         </div>
         <div class="col-md-6" style="text-align: right;">
-            <h5 style="float: right;  -webkit-text-stroke: 1px white; font-weight: bold; color: red; padding: 10px;">{{ $student->campusProgram->campus->name }}</h5>
+            <h5 style="float: right;  -webkit-text-stroke: 1px white; font-weight: bold; color: red; padding-right: 20px;">{{ $student->campusProgram->campus->name }}</h5>
         </div>
     </div>   
    </div>
