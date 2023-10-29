7c1ff03d9c3eb2453c427b585bfc38d547965828
eric_future <futureeric20@gmail.com>
2023-10-29 16:32:53 +0300

    Update id-card.blade.php

--- resources/views/dashboard/registration/reports/id-card.blade.php
+++ resources/views/dashboard/registration/reports/id-card.blade.php
@@ -240,7 +240,7 @@
         </div>
      </div> -->
      <div class="container" style="position: relative; z-index: 1000;">
-     <div class="row" style="margin-top: -10px;">
+     <div class="row" style="margin-top: -20px;">
         <div class="col-md-3" style="text-align: center; padding: 5px;">
           @if(file_exists(public_path().'/avatars/'.$student->image))
           <img src="{{ asset('avatars/'.$student->image)}}" class="ss-logo" style="text-align: center; width: 150px;">
