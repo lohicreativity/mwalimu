5b13f1098062b9e9ce8c72686c6e4eb20259597c
Hance <85370286+hance910@users.noreply.github.com>
2023-10-30 16:49:09 +0300

    Update id-card.blade.php

--- resources/views/dashboard/registration/reports/id-card.blade.php
+++ resources/views/dashboard/registration/reports/id-card.blade.php
@@ -226,7 +226,7 @@
         @if ($tuition_payment_check)
             <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 400px; background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Kivukoni.jpg') }});  background-size: 100%; background-repeat: no-repeat;">
         @else
-            <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 100%; background-image: url({{ asset('img/mnma-id-bg-semi 1-Kivukoni.jpg') }});  background-size: 100%; background-repeat: no-repeat;">
+            <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 100%; background-image: url({{ asset('img/mnma-id-bg-semi 1-Kivukoni.jpg') }});  background-size: 100%; background-repeat: no-repeat; object-contain: fit;">
         @endif
    @elseif($student->applicant->campus_id == 2)
         @if ($tuition_payment_check)
@@ -247,7 +247,7 @@
      </div> --}}
      {{-- <div style="border:2px solid #1b2066; margin-top: -20px; margin-bottom: 10px;"></div> --}}
 
-     <div class="container" style="position: relative;">
+     <div class="container" style="position: relative; z-index: 1000; height: 400px; width: 100%;">
      <div class="row">
         <div class="col-md-3" style="position:absolute; padding-top: 100px;">
           @if(file_exists(public_path().'/avatars/'.$student->image))
