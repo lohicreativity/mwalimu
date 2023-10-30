f663b37bb2676a937abc2f4fd3d3a8a40dfa22f4
Hance <85370286+hance910@users.noreply.github.com>
2023-10-30 16:33:25 +0300

    Update id-card.blade.php

--- resources/views/dashboard/registration/reports/id-card.blade.php
+++ resources/views/dashboard/registration/reports/id-card.blade.php
@@ -224,15 +224,15 @@
 
    @if ($student->applicant->campus_id == 1)
         @if ($tuition_payment_check)
-            <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 320px; background-size: cover; background-repeat: no-repeat; background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Kivukoni.jpg') }});">
+            <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 320px; background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Kivukoni.jpg') }});  background-size: cover; background-repeat: no-repeat;">
         @else
-            <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 320px; background-size: cover; background-repeat: no-repeat; background-image: url({{ asset('img/mnma-id-bg-semi 1-Kivukoni.jpg') }});">
+            <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 320px; background-image: url({{ asset('img/mnma-id-bg-semi 1-Kivukoni.jpg') }});  background-size: cover; background-repeat: no-repeat;">
         @endif
    @elseif($student->applicant->campus_id == 2)
         @if ($tuition_payment_check)
-         <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 320px; background-size: cover; background-repeat: no-repeat; background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Karume.jpg') }});">
+         <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 320px; background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Karume.jpg') }});  background-size: cover; background-repeat: no-repeat;">
         @else
-         <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 320px; background-size: cover; background-repeat: no-repeat; background-image: url({{ asset('img/mnma-id-bg-semi1.jpg') }});">
+         <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 320px; background-image: url({{ asset('img/mnma-id-bg-semi1.jpg') }});  background-size: cover; background-repeat: no-repeat;">
         @endif
    @endif
 
