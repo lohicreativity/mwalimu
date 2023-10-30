60f0d8b286a10852fbb568b270f17625ab0f7063
Hance <85370286+hance910@users.noreply.github.com>
2023-10-30 16:55:13 +0300

    Update id-card.blade.php

--- resources/views/dashboard/registration/reports/id-card.blade.php
+++ resources/views/dashboard/registration/reports/id-card.blade.php
@@ -221,18 +221,17 @@
   </style>
 </head>
 <body>
-<div style="width: 750px; height: 400px;">
    @if ($student->applicant->campus_id == 1)
         @if ($tuition_payment_check)
-            <div id="ss-id-card" class="ss-id-card" style=" background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Kivukoni.jpg') }});  background-size: 100%; background-repeat: no-repeat;">
+            <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 400px; background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Kivukoni.jpg') }});  background-size: 750px 400px; background-repeat: no-repeat;">
         @else
-            <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi 1-Kivukoni.jpg') }});  background-size: contain; background-repeat: no-repeat;">
+            <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 400px; background-image: url({{ asset('img/mnma-id-bg-semi 1-Kivukoni.jpg') }});  background-size:  750px 400px; background-repeat: no-repeat;">
         @endif
    @elseif($student->applicant->campus_id == 2)
         @if ($tuition_payment_check)
-         <div id="ss-id-card" class="ss-id-card" style=" background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Karume.jpg') }});  background-size: contain; background-repeat: no-repeat;">
+         <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 400px; background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Karume.jpg') }});  background-size:  750px 400px; background-repeat: no-repeat;">
         @else
-         <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi1.jpg') }});  background-size: contain; background-repeat: no-repeat;">
+         <div id="ss-id-card" class="ss-id-card" style="width: 750px; height: 400px; background-image: url({{ asset('img/mnma-id-bg-semi1.jpg') }});  background-size:  750px 400px; background-repeat: no-repeat;">
         @endif
    @endif
 
@@ -303,7 +302,6 @@
                 </div>
         </div>
     </div> -->
-</div>
    <pagebreak>
      <div id="ss-id-card-back" class="ss-id-card" style="width: 750px; height: 400px; background-color: #FFF; ">
        <div class="container">
