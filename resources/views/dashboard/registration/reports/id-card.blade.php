99b61417f2d8db02f7a99f60e270d7d91c4eb6bb
eric_future <futureeric20@gmail.com>
2023-10-31 08:55:54 +0300

    Update id-card.blade.php

--- resources/views/dashboard/registration/reports/id-card.blade.php
+++ resources/views/dashboard/registration/reports/id-card.blade.php
@@ -223,15 +223,15 @@
 <body>
    @if ($student->applicant->campus_id == 1)
         @if ($tuition_payment_check)
-            <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Kivukoni.jpg') }});  background-size: contain; background-repeat: no-repeat; width: 750px; height: 400px; ">
+            <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Kivukoni.jpg') }});  background-size:  71rem 50rem; background-repeat: no-repeat; width: 750px; height: 400px; ">
         @else
             <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi 1-Kivukoni.jpg') }});  background-size:  71rem 50rem; background-repeat: no-repeat; width: 750px; height: 400px;">
         @endif
    @elseif($student->applicant->campus_id == 2)
         @if ($tuition_payment_check)
-         <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Karume.jpg') }});  background-size:  contain; background-repeat: no-repeat; width: 750px; height: 400px; ">
+         <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Karume.jpg') }});  background-size:  71rem 50rem; background-repeat: no-repeat; width: 750px; height: 400px; ">
         @else
-         <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi1.jpg') }});  background-size:  contain; background-repeat: no-repeat; width: 750px; height: 400px; ">
+         <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi1.jpg') }});  background-size:  71rem 50rem; background-repeat: no-repeat; width: 750px; height: 400px; ">
         @endif
    @endif
 
@@ -245,7 +245,6 @@
         </div>
      </div> --}}
      {{-- <div style="border:2px solid #1b2066; margin-top: -20px; margin-bottom: 10px;"></div> --}}
-     <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi 1-Kivukoni.jpg') }});  background-size:  71rem 50rem; background-repeat: no-repeat; width: 750px; height: 400px;">
      <div class="container" style="position: relative; z-index: 1000;">
      <div class="row">
         <div class="col-md-3" style="position:absolute; padding-top: 100px;">
