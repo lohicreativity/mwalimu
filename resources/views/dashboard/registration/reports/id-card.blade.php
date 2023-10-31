54fee73f1711cd69b75a51a84a6f6d73eb265e56
eric_future <futureeric20@gmail.com>
2023-10-31 08:39:53 +0300

    Update id-card.blade.php

--- resources/views/dashboard/registration/reports/id-card.blade.php
+++ resources/views/dashboard/registration/reports/id-card.blade.php
@@ -260,8 +260,8 @@
            <h4 style="margin: 0px 0px 0px 25px; padding-left: 15px;">NAME: <span style="font-style: italic; font-weight: normal;">{{ $student->first_name }} {{ $student->middle_name }} {{ $student->surname }}</span></h4>
            <h4 style="margin: 0px 0px 0px 25px; padding-left: 15px;">PROGRAM: <span style="font-style: italic; font-weight: normal;">{{ $student->campusProgram->program->code }}</span></h4>
            <h4 style="margin: 0px 0px 0px 25px; padding-left: 15px;">VALID TO: <span style="font-style: italic; font-weight: normal">{{ str_replace('-', '/', App\Utils\DateMaker::toStandardDate($study_academic_year->end_date)) }}</span></h4>
-           <h4 style="margin: 0px 0px 0px 25px; padding-left: 15px;">SIGNATURE:
-           <img src="{{ asset('signatures/'.$student->signature) }}" style="width: 100px; height: auto; font-weight: normal;"></h4>
+           <h4 style="margin: 0px 0px 0px 25px; padding-left: 15px;">
+           <img src="{{ asset('signatures/'.$student->signature) }}" style="width: 100px; height: auto; padding-top: 15px; font-weight: normal;"></h4>
         </div>
      </div>
      {{-- <div class="row" style="background-color:#1b2066; position:absolute; margin-right: -80px;">
