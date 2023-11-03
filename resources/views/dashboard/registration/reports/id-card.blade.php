9dc9f257990259c506a0b51eb23163222bbd5eec
eric_future <futureeric20@gmail.com>
2023-11-03 16:33:29 +0300

    testImg

--- resources/views/dashboard/registration/reports/id-card.blade.php
+++ resources/views/dashboard/registration/reports/id-card.blade.php
@@ -225,7 +225,7 @@
         @if ($tuition_payment_check)
             <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Kivukoni.jpg') }});  background-size:  71rem 47rem; background-repeat: no-repeat; width: 750px; height: 270px; ">
         @else
-            <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/IMG-20231029-WA0018.jpg') }});  background-size:  71rem 50rem; background-position: center; background-repeat: no-repeat; width: 100%;height: 100%;">
+            <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Karume.png') }});  background-size:  71rem 50rem; background-position: center; background-repeat: no-repeat; width: 100%;height: 100%;">
         @endif
    @elseif($student->applicant->campus_id == 2)
         @if ($tuition_payment_check)
