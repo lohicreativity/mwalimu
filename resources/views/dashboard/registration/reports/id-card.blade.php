6784184090367a61dc1c9af9d587d437d531684a
eric_future <futureeric20@gmail.com>
2023-11-03 13:31:47 +0300

    Update id-card.blade.php

--- resources/views/dashboard/registration/reports/id-card.blade.php
+++ resources/views/dashboard/registration/reports/id-card.blade.php
@@ -226,7 +226,7 @@
         @if ($tuition_payment_check)
             <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Kivukoni.jpg') }});  background-size:  71rem 47rem; background-repeat: no-repeat; width: 750px; height: 270px; ">
         @else
-            <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/IMG-20231029-WA0018.jpg') }});  background-size: 100% auto; background-repeat: no-repeat; width: 100%;height: 100%;">
+            <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/IMG-20231029-WA0018.jpg') }});  background-size: 100% auto; background-repeat: no-repeat; width: 100%;height: 100%; margin-bottom: 25px;">
         @endif
    @elseif($student->applicant->campus_id == 2)
         @if ($tuition_payment_check)
@@ -303,7 +303,7 @@
         </div>
     </div> -->
    <pagebreak>
-     <div id="ss-id-card-back" class="ss-id-card" style=" width: 100%;height: 100%; background-color: #FFF; ">
+     <div id="ss-id-card-back" class="ss-id-card" style="width: 750px; height: 400px; background-color: #FFF; ">
        <div class="container">
        <div class="row">
           <div class="col-md-12">
