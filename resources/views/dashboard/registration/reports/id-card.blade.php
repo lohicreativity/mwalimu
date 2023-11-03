5b408f125492cbd04a02895dd60073ded461e902
eric_future <futureeric20@gmail.com>
2023-11-03 16:45:53 +0300

    id-card-fix

--- resources/views/dashboard/registration/reports/id-card.blade.php
+++ resources/views/dashboard/registration/reports/id-card.blade.php
@@ -223,15 +223,15 @@
 <body>
    @if ($student->applicant->campus_id == 1)
         @if ($tuition_payment_check)
-            <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Kivukoni.png') }});  background-size:  71rem 47rem; background-repeat: no-repeat; width: 750px; height: 270px; ">
+            <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Kivukoni.png') }});  background-size:  71rem 47rem; background-position: center; background-repeat: no-repeat;  width: 100%; height: 100%;">
         @else
-            <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/IMG-20231029-WA0018.jpg') }});  background-size:  71rem 50rem; background-position: center; background-repeat: no-repeat; width: 100%;height: 100%;">
+            <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/IMG-20231029-WA0018.jpg') }});  background-size:  71rem 50rem; background-position: center; background-repeat: no-repeat; width: 100%; height: 100%;">
         @endif
    @elseif($student->applicant->campus_id == 2)
         @if ($tuition_payment_check)
-         <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Karume.png') }});  background-size:  71rem 47rem; background-repeat: no-repeat; width: 750px; height: 270px; ">
+         <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/mnma-id-bg-semi 1&2 Karume.png') }});  background-size:  71rem 47rem; background-position: center; background-repeat: no-repeat;  width: 100%; height: 100%;">
         @else
-         <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/WhatsApp Image 2023-10-16 at 14.42.44_9a95e42e.png') }});  background-size:  71rem 47rem; background-repeat: no-repeat; width: 750px; height: 270px; ">
+         <div id="ss-id-card" class="ss-id-card" style="background-image: url({{ asset('img/WhatsApp Image 2023-10-16 at 14.42.44_9a95e42e.png') }}); background-position: center; background-size:  71rem 47rem; background-repeat: no-repeat;  width: 100%; height: 100%;">
         @endif
    @endif
 
