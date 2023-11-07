cb9086bdc7db9ec5ded0b5a2b1e0c8b4ca259d96
lohicreativity <lohi.sanday@gmail.com>
2023-11-07 19:35:22 +0300

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2949,7 +2949,6 @@ class ApplicationController extends Controller
                     }
     
                     if(unserialize($campus_program->entryRequirements[0]->equivalent_majors) != ''){
-                        return $campus_program->entryRequirements[0]->pass_subjects.' - '.$diploma_gpa.' - '.$campus_program->entryRequirements[0]->equivalent_gpa;
                         if(($o_level_pass_count+$o_level_other_pass_count) >= $campus_program->entryRequirements[0]->pass_subjects && $has_major && $diploma_gpa >= $campus_program->entryRequirements[0]->equivalent_gpa){
 
                             $qualified = true;
