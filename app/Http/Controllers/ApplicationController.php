428f2b76093c194e78f298dcfff887e78736e96f
lohicreativity <lohi.sanday@gmail.com>
2023-11-07 20:41:43 +0300

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -7962,7 +7962,7 @@ class ApplicationController extends Controller
         // dd( $campus_progs);
 
         $campus_programs = $available_progs;
-        $award = $applicant->programLevel;
+return $campus_programs;
         $programs = [];
 
         $o_level_grades = ['A'=>5,'B+'=>4,'B'=>3,'C'=>2,'D'=>1,'E'=>0.5,'F'=>0];
