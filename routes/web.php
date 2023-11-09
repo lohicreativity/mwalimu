1729b5776bee5395488497c6449902163371af62
lohicreativity <lohi.sanday@gmail.com>
2023-11-09 22:20:51 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -106,7 +106,7 @@ Route::get('test',function(){
 	// return $response;
 
 	// return $result;
-	 $acpac = new ACPACService;
+	// $acpac = new ACPACService;
     // $acpac->query("UPDATE receipts SET RCPTYPE = 2, REVACT = 104");
 
 	// //$acpac->query("INSERT INTO receipts (BANK,BANKNAME,RCPNUMBER,RCPDATE,RCPDESC,IDCUST,NAMECUST,INVOICE,AMTAPPLIED,IMPORTED,IMPDATE) VALUES
@@ -136,10 +136,10 @@ Route::get('test',function(){
 
     // echo '<br><br><br>';
 
-    $results = $acpac->query('SELECT * FROM receipts');
-     while ($row = sqlsrv_fetch_array($results)) {
-     	print_r($row);
-    }
+    // $results = $acpac->query('SELECT * FROM receipts');
+    //  while ($row = sqlsrv_fetch_array($results)) {
+    //  	print_r($row);
+    // }
  
 	//$acpac->query("INSERT INTO customer (IDCUST,IDGRP,NAMECUST,TEXTSTRE1,TEXTSTRE2,TEXTSTRE3,TEXTSTRE4,NAMECITY,CODESTTE,CODEPSTL,CODECTRY,NAMECTAC,TEXTPHON1,TEXTPHON2,CODETERR,IDACCTSET,CODECURN,EMAIL1,EMAIL2) VALUES ('BTBA000122','16722','LEOPOLD, LEONTINE ','ILEMELA','ARUMERU','BANG','9193 DSM','Tanzania','Tanzania','ILEMELA','Tanzania','NGWARA, NYAMBOHA KIKARO','255754991909','0754991909','BTC.BA','STD','TSH','yusufu.erick@mnma.ac.tz','leontine97@gmail.com')");
 	// $acpac->query("DELETE FROM customer");
