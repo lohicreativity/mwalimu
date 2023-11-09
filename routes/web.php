1f38bbea59c81459aeca36533dfb8a4a68f14a6f
lohicreativity <lohi.sanday@gmail.com>
2023-11-09 22:04:34 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -122,10 +122,10 @@ Route::get('test',function(){
 	// $acpac->query("DELETE FROM customer");
 	// $acpac->query("DELETE FROM invoices");
 	// $acpac->query("DELETE FROM receipts");
- 	// $results = $acpac->query('SELECT * FROM customer');
-    //  while ($row = sqlsrv_fetch_array($results)) {
-    //  	print_r($row);
-    // }
+ 	$results = $acpac->query('SELECT * FROM customer');
+     while ($row = sqlsrv_fetch_array($results)) {
+     	print_r($row);
+    }
 
     // echo '<br><br><br>';
 
@@ -136,10 +136,10 @@ Route::get('test',function(){
 
     // echo '<br><br><br>';
 
-    $results = $acpac->query('SELECT * FROM receipts');
-     while ($row = sqlsrv_fetch_array($results)) {
-     	print_r($row);
-    }
+    // $results = $acpac->query('SELECT * FROM receipts');
+    //  while ($row = sqlsrv_fetch_array($results)) {
+    //  	print_r($row);
+    // }
  
 	//$acpac->query("INSERT INTO customer (IDCUST,IDGRP,NAMECUST,TEXTSTRE1,TEXTSTRE2,TEXTSTRE3,TEXTSTRE4,NAMECITY,CODESTTE,CODEPSTL,CODECTRY,NAMECTAC,TEXTPHON1,TEXTPHON2,CODETERR,IDACCTSET,CODECURN,EMAIL1,EMAIL2) VALUES ('BTBA000122','16722','LEOPOLD, LEONTINE ','ILEMELA','ARUMERU','BANG','9193 DSM','Tanzania','Tanzania','ILEMELA','Tanzania','NGWARA, NYAMBOHA KIKARO','255754991909','0754991909','BTC.BA','STD','TSH','yusufu.erick@mnma.ac.tz','leontine97@gmail.com')");
 	// $acpac->query("DELETE FROM customer");
