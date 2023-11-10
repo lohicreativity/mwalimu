621b3d83c125a2027bb4378a2d65e8907a51c787
lohicreativity <lohi.sanday@gmail.com>
2023-11-10 17:38:58 +0300

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -8531,7 +8531,7 @@ class ApplicationController extends Controller
   
                 if(unserialize($program->entryRequirements[0]->equivalent_majors) != '' && unserialize($program->entryRequirements[0]->equivalent_must_subjects) == ''){
                     if(($o_level_pass_count+$o_level_other_pass_count) >= $program->entryRequirements[0]->pass_subjects && $has_major && $diploma_gpa >= $program->entryRequirements[0]->equivalent_gpa){
-return 1;
+
                         $programs[] = $program;
 
                     }
@@ -8540,7 +8540,7 @@ return 1;
                             $equivalent_must_subjects_count >= count(unserialize($program->entryRequirements[0]->equivalent_must_subjects)) &&
                             $diploma_gpa >= $program->entryRequirements[0]->equivalent_gpa)  || ($o_level_pass_count >= $program->entryRequirements[0]->pass_subjects &&
                             $has_nacte_results && $diploma_gpa >= $program->entryRequirements[0]->equivalent_gpa)){
-                        $programs[] = $program;
+                       // $programs[] = $program;
 
                     }
                 }
