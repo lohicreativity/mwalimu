fd314fad70bd89250211db8cdce650e3d52e7ab8
lohicreativity <lohi.sanday@gmail.com>
2023-11-27 21:55:34 +0300

    Update RegistrationController.php

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -683,7 +683,7 @@ class RegistrationController extends Controller
 			$student = Student::whereHas('registrations', function($query) use($request, $semester){$query->where('study_academic_year_id',$request->get('study_academic_year_id'))->where('semester_id',$semester->id)->where('id_print_status', 0)->where('status', 'REGISTERED');})
 					   ->whereHas('campusProgram.program',function($query) use($request){$query->where('award_id',$request->get('program_level_id'));})
 					   ->whereHas('applicant',function($query) use($request){$query->where('campus_id',$request->get('campus_id'));})
-					   ->with('applicant','campusProgram.program','campusProgram.campus')->latest()->get();
+					   ->with('applicant','campusProgram.program','campusProgram.campus')->latest()->get()->limit(1);
 		}
 
 /*         if(count($student) == 0){
@@ -705,7 +705,6 @@ class RegistrationController extends Controller
 			'staff'=>User::find(Auth::user()->id)->staff,
             'request'=>$request
         ];
-        return $data[0]->students;
         return view('dashboard.registration.id-card',$data)->withTitle('ID Card');
     }
 
