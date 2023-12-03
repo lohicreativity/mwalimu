5a97f484b59f8d0e9ba8575144a7d3f92f64855e
lohicreativity <lohi.sanday@gmail.com>
2023-12-03 13:19:33 +0300

    Update RegistrationController.php

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -724,7 +724,7 @@ class RegistrationController extends Controller
             'awards'=>Award::all(),
             'campuses'=>Campus::all(),
 			'staff'=>User::find(Auth::user()->id)->staff,
-            'students'=>Student::select('id','signature','image')->where('id',$request->id)->first(),
+            'students'=>Student::select('id','signature','image')->where('id',$request->id)->get(),
             'compose'=>1,
             'request'=>$request
         ];
