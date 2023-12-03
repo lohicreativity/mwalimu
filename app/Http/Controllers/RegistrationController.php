cda72ac847de0512e10d6ac58996a9771039883e
lohicreativity <lohi.sanday@gmail.com>
2023-12-03 11:53:40 +0300

    Update RegistrationController.php

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -683,7 +683,7 @@ class RegistrationController extends Controller
 			$student = Student::whereHas('registrations', function($query) use($request, $semester){$query->where('study_academic_year_id',$request->get('study_academic_year_id'))->where('semester_id',$semester->id)->where('id_print_status', 0)->where('status', 'REGISTERED');})
 					   ->whereHas('campusProgram.program',function($query) use($request){$query->where('award_id',$request->get('program_level_id'));})
 					   ->whereHas('applicant',function($query) use($request){$query->where('campus_id',$request->get('campus_id'));})
-					   ->with('applicant','campusProgram.program','campusProgram.campus')->latest()->paginate(200);
+					   ->with('applicant')->latest()->paginate(200);
 		}
 
 /*         if(count($student) == 0){
