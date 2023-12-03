c1f2af73bb9201bdff3f1db49a6abec7ec010ec8
lohicreativity <lohi.sanday@gmail.com>
2023-12-03 09:34:21 +0300

    Update AdmissionController.php

--- app/Http/Controllers/AdmissionController.php
+++ app/Http/Controllers/AdmissionController.php
@@ -244,6 +244,13 @@ class AdmissionController extends Controller
                     ->orWhere('name','LIKE','%Supervision Fee%')->orWhere('name','LIKE','%Student\'s Welfare Emergence%')->orWhere('name','LIKE','%Caution Money%')
                     ->orWhere('name','LIKE','%Student\'s Union%')->orWhere('name','LIKE','%Medical Examination%');});
             })->where('study_academic_year_id', $study_academic_year->id)->where('campus_id', session('applicant_campus_id'))->sum('amount_in_tzs');
+
+            $other_fees_usd = FeeAmount::whereHas('feeItem', function($query){
+                $query->where('name','LIKE','%Master%')->where(function($query){$query->where('name','LIKE','%Registration Fee%')->orWhere('name','LIKE','%New ID Card Fee%')
+                    ->orWhere('name','LIKE','%Supervision Fee%')->orWhere('name','LIKE','%Student\'s Welfare Emergence%')->orWhere('name','LIKE','%Caution Money%')
+                    ->orWhere('name','LIKE','%Student\'s Union%')->orWhere('name','LIKE','%Medical Examination%');});
+            })->where('study_academic_year_id', $study_academic_year->id)->where('campus_id', session('applicant_campus_id'))->sum('amount_in_usd');
+
  
     	}else{
     		$quality_assurance_fee = FeeAmount::whereHas('feeItem',function($query){
