15c371ec4742f05d1f7052df52c3503726bd1446
lohicreativity <lohi.sanday@gmail.com>
2023-12-04 15:23:01 +0300

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -8986,8 +8986,10 @@ class ApplicationController extends Controller
 		}
 
         $intake = $applicant->intake_id == 1? 'September' : 'March';
-        return $applicant->campus_id.' - '.$intake.' - '.$award->name;
+
 		$dates = SpecialDate::where('name','New Registration Period')->where('study_academic_year_id',$ac_year->id)->where('campus_id',$applicant->campus_id)->where('intake',$intake)->first();
+
+        return $dates;
         $reg_date = null;
         foreach($dates as $date){
             if(in_array($award->name, unserialize($date->applicable_levels))){
