6e029d20714f62b73f6bd6bf282ca557caf1e3f1
lohicreativity <lohi.sanday@gmail.com>
2023-12-07 08:53:23 +0300

    Update student-search.blade.php

--- resources/views/dashboard/academic/student-search.blade.php
+++ resources/views/dashboard/academic/student-search.blade.php
@@ -237,8 +237,8 @@
                     </div>
                   </div>
                 </div>
+                <a href="{{ url('student/deceased?student_id='.$student->id) }}" class="btn btn-primary">Deceased</a> <a href="{{ url('student/reset-password?student_id='.$student->id) }}" class="btn btn-primary">Reset Password</a> <a href="{{ url('student/reset-control-number?student_id='.$student->id) }}" class="btn btn-primary">Reset Control Number</a>
               </div>
-              <a href="{{ url('student/deceased?student_id='.$student->id) }}" class="btn btn-primary">Deceased</a> <a href="{{ url('student/reset-password?student_id='.$student->id) }}" class="btn btn-primary">Reset Password</a> <a href="{{ url('student/reset-control-number?student_id='.$student->id) }}" class="btn btn-primary">Reset Control Number</a>
               </div>
               <!-- /.modal-content -->
             </div> 
