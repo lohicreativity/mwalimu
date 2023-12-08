c869fadf5796fe622f6b3974e6d815a1bfafbc04
lohicreativity <lohi.sanday@gmail.com>
2023-12-08 16:06:47 +0300

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -5101,10 +5101,13 @@ class ApplicationController extends Controller
      */
     public function showFailedInsuranceRegistrations(Request $request)
     {
+        $staff = User::find(Auth::user()->id)->staff;
         $data = [
            'study_academic_years'=>StudyAcademicYear::with('academicYear')->get(),
            'study_academic_year'=>StudyAcademicYear::find($request->get('study_academic_year_id')),
-           'records'=>InsuranceRegistration::with(['student:id,applicant_id,first_name,middle_name,surname,gender,phone,campus_program_id','applicant:id,index_number'])->where('study_academic_year_id',$request->get('study_academic_year_id'))->where('is_success',0)->get(),
+           'records'=>InsuranceRegistration::with(['student:id,applicant_id,first_name,middle_name,surname,gender,phone,campus_program_id',
+                                                   'applicant'=>function($query) use($staff){$query->select('id','index_number')->where('campus_id',$staff->campus_id);}])
+                                           ->where('study_academic_year_id',$request->get('study_academic_year_id'))->where('is_success',0)->get(),
            'request'=>$request
         ];
         return view('dashboard.admission.failed-insurance-registrations',$data)->withTitle('Failed Insurance Registrations');
