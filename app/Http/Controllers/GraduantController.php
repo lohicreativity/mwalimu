0187e23681de5aa9333d56397e7a557ade30c41d
lohicreativity <lohi.sanday@gmail.com>
2023-12-15 15:58:04 +0300

    Update GraduantController.php

--- app/Http/Controllers/GraduantController.php
+++ app/Http/Controllers/GraduantController.php
@@ -765,7 +765,7 @@ class GraduantController extends Controller
         }
 
         $list = Student::select('id','first_name','middle_name','surname','gender','nationality','birth_date','disability_status_id','academic_status_id','campus_program_id','year_of_study','study_mode','applicant_id','registration_year','registration_number')
-                             ->whereIn('id',[$submitted_list_ids])
+                             ->whereIn('id',$submitted_list_ids)
                              ->with(['applicant.nectaResultDetails'=>function($query){$query->select('id','applicant_id','index_number','exam_id')->where('verified',1);},
                                      'academicStatus:id,name','applicant:id,entry_mode,index_number','campusProgram.campus:id,code','campusProgram.program.award:id,name','annualRemarks:id,student_id,year_of_study'])
                              ->get();
