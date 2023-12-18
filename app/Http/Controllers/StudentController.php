a5654f1921bcebcb1e36704729986e918b395f7d
lohicreativity <lohi.sanday@gmail.com>
2023-12-18 16:14:42 +0300

    Update StudentController.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -136,7 +136,6 @@ class StudentController extends Controller
                     if($tuition_fee_loan>0){
                       $fee_payment_percent = ($paid_amount+$tuition_fee_loan)/$invoice->amount;
                     }
-                    return $paid_amount.' + '.$tuition_fee_loan;
                     break;
                   }
                 }
