e4d44ca888905dd8dc6108c4b7d7c6b3765b326c
lohicreativity <lohi.sanday@gmail.com>
2023-12-20 10:58:32 +0300

    dd

--- resources/views/dashboard/finance/payer-details.blade.php
+++ resources/views/dashboard/finance/payer-details.blade.php
@@ -174,7 +174,7 @@
 												@if ($payments->gatewayPayment)
 													@if (str_contains($payments->feeType->name,'Tuition'))
 														@foreach($total_paid_fee as $fee)
-															@if($payments->reference_no == $fee->reference_no)
+															@if($payments->reference_no == $fee[0]->reference_no)
 																{{ number_format($fee->amount,2) }}
 															@endif
 														@endforeach
@@ -190,7 +190,7 @@
 												@if ($payments->gatewayPayment)
 													@if (str_contains($payments->feeType->name,'Tuition'))
 														@foreach($total_paid_fee as $fee)
-															@if($payments->reference_no == $fee->reference_no)
+															@if($payments->reference_no == $fee[0]->reference_no)
 																{{ number_format($payments->gatewayPayment->bill_amount-$fee->amount,2) }} 
 															@endif
 														@endforeach
