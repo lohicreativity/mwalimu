2e6d2b4e5d3b7b6ec13c47dd6e590b87ad96604d
lohicreativity <lohi.sanday@gmail.com>
2023-12-20 10:44:15 +0300

    Update StaffController.php

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -282,7 +282,7 @@ class StaffController extends Controller
                 foreach($paid_as_student as $payment){
                     if(str_contains($payment->feeType->name, 'Tuition')){
                         $fee_paid_amount = array('reference_no'=>$payment->reference_no, 'amount'=>GatewayPayment::where('bill_id', $payment->reference_no)->sum('paid_amount'));
-                        $total_fee_paid_amount = array_merge($fee_paid_amount,$total_fee_paid_amount);
+                        $total_fee_paid_amount[] = array_merge($fee_paid_amount,$total_fee_paid_amount);
                         return $total_fee_paid_amount;
                     }
                 }
