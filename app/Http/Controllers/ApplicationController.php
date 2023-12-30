55961f48c6af361b976467200975b7e211d20372
lohicreativity <lohi.sanday@gmail.com>
2023-12-30 12:24:51 +0300

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -7437,13 +7437,12 @@ class ApplicationController extends Controller
                                                           ->get()
         ];
 
-        return InternalTransfer::select('id','current_campus_program_id')
+        return InternalTransfer::distinct()->select('id','current_campus_program_id')
         ->whereHas('student.applicant',function($query) use($staff){$query
                                       ->where('campus_id',$staff->campus_id);})
         ->with('currentProgram.program')
         ->where('status','SUBMITTED')
-        ->distinct()
-        ->get('current_campus_program_id');
+        ->get();
         return view('dashboard.registration.submit-internal-transfer',$data)->withTitle('Internal Transfer');
     }
 
