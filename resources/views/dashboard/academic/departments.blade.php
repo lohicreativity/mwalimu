d7fa3368afad25aa5990b534d76bfabdd5cbc835
lohicreativity <lohi.sanday@gmail.com>
2024-01-07 13:38:08 +0300

    Update departments.blade.php

--- resources/views/dashboard/academic/departments.blade.php
+++ resources/views/dashboard/academic/departments.blade.php
@@ -335,7 +335,7 @@
 
 
                                           @foreach($departments as $department)
-                                          <option value="{{ $department->id }}" @if($department->parent_id == $department->id) selected = 'selected' @endif>{{ $department->name }} - {{$parent_id}}
+                                          <option value="{{ $department->id }}" @if($department->parent_id == $department->id) selected = 'selected' @endif>{{ $parent_id }} - @foreach($department->campuses as $campus) {{ $campus->name }} @endforeach
                                           </option>
                                           @endforeach 
                                         </select>
