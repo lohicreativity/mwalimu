2419cfe665a201fe8d2775fb3a2eb52da839ec38
esternwinluck <esternwinluck@gmail.com>
2024-01-18 10:04:47 +0300

    liviwire

---
+++ app/Http/Livewire/Finance/Payments.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Livewire\Finance;
+
+use App\Domain\Academic\Models\StudyAcademicYear;
+use App\Domain\Finance\Exports\PaymentsReportExport;
+use App\Domain\Finance\Models\FeeType;
+use App\Domain\Finance\Models\GatewayPayment;
+use Livewire\Component;
+use Livewire\WithPagination;
+use Maatwebsite\Excel\Facades\Excel;
+
+class Payments extends Component
+{
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public $searchInput;
+
+    public $feeTypeId;
+
+    public $studyAcademicYear;
+
+    public $from;
+
+    public $to;
+
+    public function getFeeTypesProperty()
+    {
+        return FeeType::query()->orderBy('name')->get();
+    }
+
+    public function getStudyAcademicYearsProperty()
+    {
+        return StudyAcademicYear::query()
+            ->with('academicYear')
+            ->get()
+            ->sortBy('academicYear.year');
+    }
+
+    public function fromDate(): string
+    {
+        $fromDate = filled($this->from) ? $this->from : now()->format('Y-m-d');
+
+        return $fromDate.' 00:00:00';
+    }
+
+    public function toDate(): string
+    {
+        $toDate = filled($this->to) ? $this->to : now()->format('Y-m-d');
+
+        return $toDate.' 23:59:59';
+    }
+
+    public function exportPayments(): \Symfony\Component\HttpFoundation\BinaryFileResponse
+    {
+        return Excel::download(
+            new PaymentsReportExport($this->gatewayPayment()),
+            'payment-report.csv'
+        );
+    }
+
+    public function gatewayPayment()
+    {
+        return GatewayPayment::query()
+            ->when(filled($this->searchInput), function ($query) {
+                $query->where(function ($q){
+                    $q->orWhere('gateway_payments.control_no', 'like', "%$this->searchInput%")->orWhere('bill_id', 'like', "%$this->searchInput%");
+                });
+            })
+            ->whereHas('invoice', function ($q){
+                $q->where('payable_id', '>', 0)->where('payable_type', 'student')
+                ->when(filled($this->feeTypeId), fn($q) => $q->where('fee_type_id', $this->feeTypeId))
+                    ->when(filled($this->studyAcademicYear), fn($q) => $q->where(
+                        fn($q) => $q->where('applicable_id', $this->studyAcademicYear)->where('applicable_type', 'academic_year')
+                    ));
+            })
+            ->when(filled($this->from), fn($q) => $q->whereBetween('gateway_payments.created_at', [$this->fromDate(), $this->toDate()]))
+
+            ->with(['invoice.payable', 'invoice.feeType',]);
+    }
+
+    public function render()
+    {
+        return view('livewire.finance.payments', [
+            'payments' => $this->gatewayPayment()->paginate(50)
+        ]);
+    }
+}
---
+++ resources/views/livewire/finance/payments.blade.php
@@ -0,0 +1,81 @@
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">{{ __('List of Payments') }}</h3>
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body">
+
+        <div class="row">
+            <div class="col-2">
+                <input wire:model="searchInput" type="text"  class="form-control" placeholder="Search by Inv# or control#"/>
+            </div>
+
+            <div class="form-group col-2">
+                <select wire:model="feeTypeId" class="form-control">
+                    <option value="">Select Fee Type</option>
+                    @foreach($this->feeTypes as $feeType)
+                        <option value="{{ $feeType->id }}">{{ $feeType->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div class="form-group col-2">
+                <select wire:model="studyAcademicYear" class="form-control">
+                    <option value="">Select Study Academic Year</option>
+                    @foreach($this->studyAcademicYears as $studyAcademicYear)
+                        <option value="{{ $studyAcademicYear->id }}" >{{ $studyAcademicYear->academicYear->year }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div class="col-2">
+                <input wire:model="from" type="date"  class="form-control" />
+                <small class="text-muted">Start date</small>
+            </div>
+
+            <div class="col-2">
+                <input wire:model="to" type="date"  class="form-control" />
+                <small class="text-muted">End date</small>
+            </div>
+
+            <div class="col-2">
+                <button wire:click="exportPayments()" type="button" class="btn btn-primary">{{ __('Export') }}</button>
+            </div>
+        </div>
+
+
+        <table id="example2" class="table table-bordered table-hover ss-margin-top">
+            <thead>
+            <tr>
+                <th>Invoice Number</th>
+                <th>Payer</th>
+                <th>Payer ID</th>
+                <th>For</th>
+                <th>Amount</th>
+                <th>Reference Number</th>
+                <th>Control Number</th>
+                <th>Receipt Number</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($payments as $payment)
+                <tr>
+                    <td>{{ $payment->bill_id }}</td>
+                    <td>{{ $payment->invoice?->payable?->first_name }} {{ $payment->invoice?->payable?->middle_name }} {{ $payment->invoice?->payable?->surname }}</td>
+                    <td>{{ $payment->invoice->payable?->registration_number }}</td>
+                    <td>{{ $payment->invoice->feeType->name }}</td>
+                    <td>{{ number_format($payment->paid_amount,2) }} {{ $payment->ccy }}</td>
+                    <td>{{ $payment->pay_refId }}</td>
+                    <td>{{ $payment->control_no }}</td>
+                    <td>{{ $payment->psp_receipt_no }}</td>
+                </tr>
+            @endforeach
+
+            </tbody>
+        </table>
+        <div class="ss-pagination-links">
+            {!! $payments->links() !!}
+        </div>
+    </div>
+    <!-- /.card-body -->
+</div>
