1dd514408ca39dd486a6bafd0e4a2dcc7f5a747b
lohicreativity <lohi.sanday@gmail.com>
2024-01-18 11:33:22 +0300

    Update PaymentsReportExport.php

--- app/Domain/Finance/Exports/PaymentsReportExport.php
+++ app/Domain/Finance/Exports/PaymentsReportExport.php
@@ -25,14 +25,18 @@ class PaymentsReportExport implements FromQuery, WithHeadings
             ->join('campus_program', function(JoinClause $q){
                 $q->on('students.campus_program_id', '=', 'campus_program.id')->where('campus_id', 1);
             })
+            ->join('fee_types', function(JoinClause $q){
+                $q->on('invoices.fee_type_id', '=', 'fee_types.id');
+            })
             ->select('students.registration_number as Reg#', 'students.first_name as "First Name"','students.middle_name as "Middle Name"',
                      'students.surname as Surname','students.gender as Sex','students.phone as Phone','students.year_of_study as "Year of Study"',
-                     'invoices.reference_no as Invoice#', 'invoices.control_no as Control#', 'paid_amount as "Amount Paid"');
+                     'invoices.reference_no as Invoice#', 'invoices.control_no as Control#', 'paid_amount as "Amount Paid"','code as "Programme Code"',
+                     'fee_types.name as "Fee Type"');
     }
 
     public function headings(): array
     {
-        return ['Reg#', 'First Name', 'Middle Name', 'Surname', 'Sex', 'Phone', 'Year of Study', 'Invoice#', 'Control#', 'Amount Paid'];
+        return ['Reg#', 'First Name', 'Middle Name', 'Surname', 'Sex', 'Phone', 'Year of Study', 'Programme Code', 'Invoice#', 'Control#', 'Fee Type', 'Amount Paid'];
     }
 
 }
