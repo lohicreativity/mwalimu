6b52c723d4e29ec0aead3ad7041485f90eeef171
lohicreativity <lohi.sanday@gmail.com>
2024-01-26 12:17:23 +0300

    Update ModuleAssignmentController.php

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -47,8 +47,8 @@ class ModuleAssignmentController extends Controller
 	public function index(Request $request)
 	{
     $staff = User::find(Auth::user()->id)->staff;
-    $campus_department = CampusDepartment::select('department_id')->get();
-						return count($campus_department);			
+    $campus_department = CampusDepartment::all();
+						return $campus_department->department_id;			
 		$data = [
            'study_academic_years'=>StudyAcademicYear::with('academicYear')->get(),
            'study_academic_year'=>$request->has('study_academic_year_id')? StudyAcademicYear::with('academicYear')->find($request->get('study_academic_year_id')) : null,
