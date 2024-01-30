9d3e97cb9b549fcc9bdcdc5d68c647d3d9de977e
lohicreativity <lohi.sanday@gmail.com>
2024-01-30 08:23:17 +0300

    dd

--- app/Domain/Finance/Exports/PaymentsReportExport.php
+++ app/Domain/Finance/Exports/PaymentsReportExport.php
@@ -20,22 +20,22 @@ class PaymentsReportExport implements FromQuery, WithHeadings
 
     public function query()
     {
-        $staff = User::find(Auth::user()->id)->staff;
-        if (Auth::user()->hasRole('administrator') || Auth::user()->hasRole('arc')) {
-            $campus_id = [1,2,3];
-        }else{
-            $campus_id = [$staff->campus_id];
-        }
+        // $staff = User::find(Auth::user()->id)->staff;
+        // if (Auth::user()->hasRole('administrator') || Auth::user()->hasRole('arc')) {
+        //     $campus_id = [1,2,3];
+        // }else{
+        //     $campus_id = [$staff->campus_id];
+        // }
         return $this->payment->join('invoices', 'gateway_payments.bill_id', '=', 'invoices.reference_no')
-            ->join('students', function(JoinClause $q){
-                $q->on('invoices.payable_id', '=', 'students.id')->where('payable_type', 'student');
-            })            
-            ->join('campus_program', function(JoinClause $q) use($campus_id){
-                $q->on('students.campus_program_id', '=', 'campus_program.id')->whereIn('campus_id', $campus_id);
-            })
-            ->join('fee_types', function(JoinClause $q){
-                $q->on('invoices.fee_type_id', '=', 'fee_types.id');
-            })
+            // ->join('students', function(JoinClause $q){
+            //     $q->on('invoices.payable_id', '=', 'students.id')->where('payable_type', 'student');
+            // })            
+            // ->join('campus_program', function(JoinClause $q) use($campus_id){
+            //     $q->on('students.campus_program_id', '=', 'campus_program.id')->whereIn('campus_id', $campus_id);
+            // })
+            // ->join('fee_types', function(JoinClause $q){
+            //     $q->on('invoices.fee_type_id', '=', 'fee_types.id');
+            // })
             ->select('students.registration_number as "Registration Number"', 'students.first_name as "First Name"','students.middle_name as "Middle Name"',
                      'students.surname as Surname','students.gender as Sex','students.phone as Phone','students.year_of_study as "Year of Study"', 'campus_program.code as "Programme Code"',
                      'invoices.reference_no as "Invoice Number"', 'invoices.control_no as "Control Number"', 'fee_types.name as "Fee Type"', 'paid_amount as "Amount Paid"'
