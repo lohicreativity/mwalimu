0b1f45d6e20d4a1ff47cdf71af085781a7f3e2b2
lohicreativity <lohi.sanday@gmail.com>
2024-01-30 08:12:02 +0300

    Update Payments.php

--- app/Http/Livewire/Finance/Payments.php
+++ app/Http/Livewire/Finance/Payments.php
@@ -57,6 +57,8 @@ class Payments extends Component
 
     public function exportPayments(): \Symfony\Component\HttpFoundation\BinaryFileResponse
     {
+        return
+            new PaymentsReportExport($this->gatewayPayment());
         return Excel::download(
             new PaymentsReportExport($this->gatewayPayment()),
             'payment-report.csv'
