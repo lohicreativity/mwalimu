75637db1b786a80beff8536b54d54bdc8a0f3e7c
lohicreativity <lohi.sanday@gmail.com>
2024-02-08 08:40:16 +0300

    Update application-batches.blade.php

--- resources/views/dashboard/application/application-batches.blade.php
+++ resources/views/dashboard/application/application-batches.blade.php
@@ -138,6 +138,7 @@
                      @endif
  
                       @can('delete-application-window')
+                      {{ $ba->batch_no }} {{ $certificate_max }}
                       @if(($ba->program_level_id == 1) && $ba->batch_no == $certificate_max)
                       <a class="btn btn-info btn-sm" href="#" data-toggle="modal" data-target="#ss-edit-batch-{{ $ba->id }}">
                               <i class="fas fa-pencil-alt">
