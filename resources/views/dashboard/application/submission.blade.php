47f4a9a3e50f7d68e8acea28b9dc3fbc1750ec58
lohicreativity <lohi.sanday@gmail.com>
2024-02-08 15:13:06 +0300

    Update submission.blade.php

--- resources/views/dashboard/application/submission.blade.php
+++ resources/views/dashboard/application/submission.blade.php
@@ -97,6 +97,7 @@
                 @else
                   @if($applicant->submission_complete_status == 1)
                       @if(empty($applicant->status))
+                      {{ $applicant }}
                           <div class="card card-default">
                               <div class="card-header">
                                 <h3 class="card-title">{{ __('Application Submission') }}</h3>
