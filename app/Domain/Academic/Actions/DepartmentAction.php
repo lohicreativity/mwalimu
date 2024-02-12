e05bf7ac445d1fd923562d94367da9342c31e92d
lohicreativity <lohi.sanday@gmail.com>
2024-02-12 09:21:30 +0300

    Update DepartmentAction.php

--- app/Domain/Academic/Actions/DepartmentAction.php
+++ app/Domain/Academic/Actions/DepartmentAction.php
@@ -7,6 +7,8 @@ use App\Domain\Academic\Models\Department;
 use App\Domain\Academic\Repositories\Interfaces\DepartmentInterface;
 use Auth, DB;
 use App\Domain\Settings\Models\CampusDepartment;
+use App\Domain\Academic\Models\StudyAcademicYear;
+use App\Domain\Academic\Models\ModuleAssignmentRequest;
 
 class DepartmentAction implements DepartmentInterface{
 	
@@ -28,9 +30,8 @@ class DepartmentAction implements DepartmentInterface{
 	}
 
 	public function update(Request $request){
-        $department = Department::where('name',$request->get('name'))->first();
-
-        if(!$department){
+        $similar_department = Department::where('name',$request->get('name'))->first();
+        if(!$similar_department){
             $department = Department::find($request->get('department_id'));
             $department->name = $request->get('name');
             $department->description = $request->get('description');
@@ -45,7 +46,15 @@ class DepartmentAction implements DepartmentInterface{
                                              ->where('unit_category_id',$request->get('current_unit_category_id'))
                                              ->where('parent_id', $request->get('current_parent_id'))
                                              ->update(['department_id'=>$department->id,'campus_id'=>$request->get('campus_id'),'parent_id'=>$request->get('parent_id'),'unit_category_id'=>$request->get('unit_category_id')]);
+        
+        if($similar_department && $similar_department->id != $request->get('department_id')){
+            DB::table('program_department')->where('department_id',$request->get('department_id'))->where('campus_id',$request->get('current_campus_id'))->update(['department_id'=>$similar_department->id]);
+            DB::table('module_department')->where('department_id',$request->get('department_id'))->where('campus_id',$request->get('current_campus_id'))->update(['department_id'=>$similar_department->id]);
+            
+            $ac_year = StudyAcademicYear::with('academicYear')->where('status','ACTIVE')->first();
 
+            ModuleAssignmentRequest::where('department_id',$request->get('department_id'))->where('study_academic_year_id',$ac_year->id)->update(['department_id'=>$similar_department->id]);
+        }
         // DB::table('campus_department')->where('department_id',$request->get('current_parent_id'))
         //                               ->where('campus_id',$request->get('current_campus_id'))
         //                               ->update(['campus_id'=>$request->get('current_campus_id'),'parent_id'=>$request->get('parent_id')]);
