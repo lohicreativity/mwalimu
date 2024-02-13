b73689e1c5b047fb1721864f57f8bf83e93f8ee5
lohicreativity <lohi.sanday@gmail.com>
2024-02-13 12:03:57 +0300

    Update modules.blade.php

--- resources/views/dashboard/academic/modules.blade.php
+++ resources/views/dashboard/academic/modules.blade.php
@@ -61,7 +61,7 @@
             <!-- general form elements -->
             <div class="card card-default">
               <div class="card-header">
-                <h3 class="card-title">{{ __('Add/Edit Module') }}</h3>
+                <h3 class="card-title">{{ __('Add Module') }}</h3>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
