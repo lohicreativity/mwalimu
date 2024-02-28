9ef05db54a295118ac514b8b4c662b61afb96f30
lohicreativity <lohi.sanday@gmail.com>
2024-02-28 16:52:13 +0300

    Update ApplicationBatchController.php

--- app/Http/Controllers/ApplicationBatchController.php
+++ app/Http/Controllers/ApplicationBatchController.php
@@ -65,7 +65,7 @@ class ApplicationBatchController extends Controller
                 $batch_ids[] = $ba->id;
             }
         }
-
+return $certificate_max;
         $batch_ids = ApplicantProgramSelection::select('batch_id')->whereIn('status',['SELECTED','PENDING'])->whereIn('batch_id',$batch_ids)->get();
         
     	$data = [
