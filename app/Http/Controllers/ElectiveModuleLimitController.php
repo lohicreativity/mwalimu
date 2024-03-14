70bda22c6c36cdf25b0549162b350061fc0da1e3
lohicreativity <lohi.sanday@gmail.com>
2024-03-14 15:08:08 +0300

    Update ElectiveModuleLimitController.php

--- app/Http/Controllers/ElectiveModuleLimitController.php
+++ app/Http/Controllers/ElectiveModuleLimitController.php
@@ -19,7 +19,7 @@ class ElectiveModuleLimitController extends Controller
      * Display a list of limits
      */
     public function index(Request $request)
-    { return StudyAcademicYear::with('academicYear')->find($request->get('study_academic_year_id'));
+    { 
     	$data = [
            'study_academic_years'=>StudyAcademicYear::with('academicYear')->get(),
            'study_academic_year'=>$request->has('study_academic_year_id')? StudyAcademicYear::with('academicYear')->find($request->get('study_academic_year_id')) : null,
