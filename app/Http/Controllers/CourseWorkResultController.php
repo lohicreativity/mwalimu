54c4f9e6f40fa3760bacb02235a10b65fcb70ef2
lohicreativity <lohi.sanday@gmail.com>
2024-03-18 07:17:31 +0300

    Update CourseWorkResultController.php

--- app/Http/Controllers/CourseWorkResultController.php
+++ app/Http/Controllers/CourseWorkResultController.php
@@ -101,7 +101,7 @@ class CourseWorkResultController extends Controller
      * Update course work results
      */
     public function update(Request $request)
-    {   return 'Under construction';
+    {   
         $validations = [];
         $messages = [];
         $assessment_plans = AssessmentPlan::where('module_assignment_id',$request->get('module_assignment_id'))->get();
