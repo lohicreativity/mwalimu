6879ba42770762a0ccd6a3185d8acdc9318e851b
lohicreativity <lohi.sanday@gmail.com>
2024-03-18 09:23:45 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -328,12 +328,12 @@ class ExaminationResultController extends Controller
                                        ->where('module_assignment_id',$assignment->id)
                                        ->get();
 
-         $enrolled_students = Student::select('id')->whereHas('studentshipStatus',function($query){$query->where('name','ACTIVE')->orWhere('name','RESUMED');})
+         $enrolled_students = Student::whereHas('studentshipStatus',function($query){$query->where('name','ACTIVE')->orWhere('name','RESUMED');})
                                      ->whereHas('registrations',function($query) use($assignment){$query->where('year_of_study',$assignment->programModuleAssignment->year_of_study)
                                                                                                                ->where('semester_id',$assignment->programModuleAssignment->semester_id);})
-                                     ->where('campus_program_id',$assignment->programModuleAssignment->campus_program_id)->get();
+                                     ->where('campus_program_id',$assignment->programModuleAssignment->campus_program_id)->get('id');
 
-                                     return $enrolled_students;
+                                     return count($enrolled_students);
 
          foreach($results as $key=>$result){
             $student = Student::select('id','campus_program_id')->with(['campusProgram.program.ntaLevel'])->find($result->student_id);
