b5a7d4655cd272c9dadb72b8bd51b9911d7ca9bb
lohicreativity <lohi.sanday@gmail.com>
2024-03-20 23:50:02 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -270,6 +270,7 @@ class ExaminationResultController extends Controller
 
                $course_work_based = $final_pass_score = $course_work_pass_score = $module_pass_mark = 0;
                foreach($module_assignment_buffer as $key=>$module_buffer){
+                  return $key.' - '.$result->module_assignment_id;
                   if($module_assignment_buffer[$key]){
                      $course_work_based = $module_buffer['course_work_based'];
                      $final_pass_score = $module_buffer['final_pass_score'];
