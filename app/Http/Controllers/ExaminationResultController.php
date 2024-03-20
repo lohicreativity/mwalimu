a712c1443071a2b76b52cb81514d7a7727910917
lohicreativity <lohi.sanday@gmail.com>
2024-03-20 21:07:37 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -166,7 +166,7 @@ class ExaminationResultController extends Controller
                $module_assignment_buffer[$module_assignment->id]['final_pass_score'] = $module_assignment->programModuleAssignment->final_pass_score;
                $module_assignment_buffer[$module_assignment->id]['course_work_pass_score'] = $module_assignment->programModuleAssignment->course_work_pass_score;
                $module_assignment_buffer[$module_assignment->id]['module_pass_mark'] = $module_assignment->programModuleAssignment->module_pass_mark;
-
+return $module_assignment_buffer;
             }elseif($module_assignment->category == 'OPTIONAL'){
                $no_of_optional_modules += 1;
                $optional_modules[] = $module_assignment;
