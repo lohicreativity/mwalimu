6dcf356d04a77258e8e8a828ba510540c17a7ab2
lohicreativity <lohi.sanday@gmail.com>
2024-03-20 22:07:41 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -259,13 +259,11 @@ class ExaminationResultController extends Controller
 
             foreach($results as $result){
                $course_work_based = $final_pass_score = $course_work_pass_score = $module_pass_mark = 0;
-return $module_assignment_buffer[$result->module_assignment_id];
-               if($module_assignment_buffer[$result->module_assignment_id]){
-                  $course_work_based = $module_assignment_buffer[$result->module_assignment_id]['course_work_based'];
-                  $final_pass_score = $module_assignment_buffer[$result->module_assignment_id]['final_pass_score'];
-                  $course_work_pass_score = $module_assignment_buffer[$result->module_assignment_id]['course_work_pass_score'];
-                  $module_pass_mark = $module_assignment_buffer[$result->module_assignment_id]['module_pass_mark'];
-               }
+
+               $course_work_based = $module_assignment_buffer[$result->module_assignment_id]['course_work_based'];
+               $final_pass_score = $module_assignment_buffer[$result->module_assignment_id]['final_pass_score'];
+               $course_work_pass_score = $module_assignment_buffer[$result->module_assignment_id]['course_work_pass_score'];
+               $module_pass_mark = $module_assignment_buffer[$result->module_assignment_id]['module_pass_mark'];
 
                if($result->retakeHistory && isset($result->retakeHistory->retakeHistory->retakableResults[0])){
                   $processed_result = ExaminationResult::find($result->retakeHistory->retakeHistory->retakableResults[0]->id);
