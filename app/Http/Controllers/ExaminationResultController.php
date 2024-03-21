c303ff021206b2379e9bf922fefb5a34c6288cb3
lohicreativity <lohi.sanday@gmail.com>
2024-03-21 09:46:37 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -3709,7 +3709,7 @@ class ExaminationResultController extends Controller
               ->where('status', 'PUBLISHED')
               ->where('type', 'SUPP')
               ->get();
-
+return $missing_modules[1];
          $data = [
          	'semesters'=>$semesters,
          	'annual_remark'=>$annual_remark,
