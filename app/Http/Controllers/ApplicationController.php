8b1e733d2ab266b9931b7747fe831ef1f3627b05
lohicreativity <lohi.sanday@gmail.com>
2024-03-21 13:34:18 +0300

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -6696,7 +6696,7 @@ class ApplicationController extends Controller
     public function showConfirmAdmission(Request $request)
     {
         $student = $applicant = null;
-        if($request->get('student_confirmation') == 1){
+        if($request->get('student_confirmation',1)){
             $student = User::find(Auth::user()->id)->student()->with('applicant')->first();
             $applicant = $student->applicant;
         }else{
