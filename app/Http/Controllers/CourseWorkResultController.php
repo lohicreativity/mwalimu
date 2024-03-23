8d4035d54287916009f75695a1fb1885b4cfb810
lohicreativity <lohi.sanday@gmail.com>
2024-03-23 09:51:35 +0300

    Update CourseWorkResultController.php

--- app/Http/Controllers/CourseWorkResultController.php
+++ app/Http/Controllers/CourseWorkResultController.php
@@ -205,9 +205,9 @@ class CourseWorkResultController extends Controller
          }
 
          if($exam_result){
-            if(is_null($course_work) && is_null($exam_result->course_work_score)){
+            if(is_null($course_work) && is_null($exam_result->course_work_score)){                  return 1;
                if(is_null($exam_result->final_score)){
-                  return 1;
+
                   $retake_history? $retake_history->delete() : null;
                   $carry_history? $carry_history->delete() : null;
                   $exam_result->delete();
