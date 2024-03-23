75de56aad240e5bb40592aa6a8721448f50bc48c
lohicreativity <lohi.sanday@gmail.com>
2024-03-23 13:00:28 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -1952,7 +1952,7 @@ class ExaminationResultController extends Controller
      * Process student results
      */
     public function processStudentResults(Request $request, $student_id, $ac_yr_id,$yr_of_study, $process_type = null)
-    { 
+    { return 1;
       try{
          DB::beginTransaction();
          $student = Student::findOrFail($student_id);
