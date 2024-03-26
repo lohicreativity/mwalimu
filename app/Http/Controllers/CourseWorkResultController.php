d4538abd1eb3530fc5d9ad6c9cab6b021b905f6b
lohicreativity <lohi.sanday@gmail.com>
2024-03-26 14:39:40 +0300

    Update CourseWorkResultController.php

--- app/Http/Controllers/CourseWorkResultController.php
+++ app/Http/Controllers/CourseWorkResultController.php
@@ -164,9 +164,10 @@ class CourseWorkResultController extends Controller
                      $result->assessment_plan_id = $plan->id;
                      $result->module_assignment_id = $module_assignment->id;
                      $result->uploaded_by_user_id = Auth::user()->id;
-                     $result->save();
+
                   }
-               }         
+               }
+               $result->save();         
         	   }
             if($request->get('plan_'.$plan->id.'_score') == null){
                $no_of_components_without_course_work++;
