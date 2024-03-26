207200caa382188614d72ca3c680f97b98904cd5
lohicreativity <lohi.sanday@gmail.com>
2024-03-26 13:01:19 +0300

    Update CourseWorkResultController.php

--- app/Http/Controllers/CourseWorkResultController.php
+++ app/Http/Controllers/CourseWorkResultController.php
@@ -199,7 +199,7 @@ class CourseWorkResultController extends Controller
             $exam_result = new ExaminationResult;
 
          }
-
+return count($exam_result);
          if($exam_result != null){
             return 100;
             if($no_of_components == $no_of_components_without_course_work){                 
