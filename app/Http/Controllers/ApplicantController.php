97bc32108b9d60d750cbc60d799397c8f89bf612
lohicreativity <lohi.sanday@gmail.com>
2024-03-27 19:16:04 +0300

    Update ApplicantController.php

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -936,7 +936,7 @@ class ApplicantController extends Controller
       }
                
       if($medical_insurance_fee &&  $students_union_fee && $caution_money_fee && $medical_examination_fee && $registration_fee && 
-         $identity_card_fee && $late_registration_fee && $welfare_emergence_fund &&  $quality_assurance_fee && $program_fee && $reference_number){
+         $identity_card_fee && $late_registration_fee && $welfare_emergence_fund &&  $quality_assurance_fee && !$program_fee && $reference_number){
          $ready_for_admission = true;
 
       }
