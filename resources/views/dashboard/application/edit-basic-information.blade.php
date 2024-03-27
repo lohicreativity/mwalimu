3b65fc4a16b6fe8193664f7f3c733af72fb03edf
lohicreativity <lohi.sanday@gmail.com>
2024-03-27 19:48:03 +0300

    Update edit-basic-information.blade.php

--- resources/views/dashboard/application/edit-basic-information.blade.php
+++ resources/views/dashboard/application/edit-basic-information.blade.php
@@ -68,9 +68,11 @@
                       <div class="alert alert-success">
                         <h3 class="text-white" style="font-size: 17px!important;"><i class="fa fa-check-circle"></i> 
                         Congratulations! You have been successfully selected for {{ $check_selected_applicant->selections[0]->campusProgram->program->name }} programme. 
-                        @if($applicant->multiple_admissions === null || $applicant->multiple_admissions === 0) Please @if($ready_for_admission) click <a href="{{ url('application/self-send-admission-letter?applicant_id='.$applicant->id) }}">here</a> to proceed with admission process or <a 
-                        href="{{ url('application/admission-confirmation') }}">here</a> to cancel the admission.@else
-                            wait for admission package or click <a href="{{ url('application/admission-confirmation') }}">here</a> to cancel the admission. @endif @elseif($applicant->confirmation_status === null) Please <a href="{{ url('application/admission-confirmation') }}">click here</a> to confirm with us.@endif</h3>
+                        @if($applicant->multiple_admissions === null || $applicant->multiple_admissions === 0 || $applicant->confirmation_status === 'CONFIRMED') 
+                        Please 
+                          @if($ready_for_admission) click <a href="{{ url('application/self-send-admission-letter?applicant_id='.$applicant->id) }}">here</a> to proceed with admission process or <a href="{{ url('application/admission-confirmation') }}">here</a> to cancel the admission.
+                          @else wait for admission package or click <a href="{{ url('application/admission-confirmation') }}">here</a> to cancel the admission. @endif 
+                        @elseif($applicant->confirmation_status === null) Please <a href="{{ url('application/admission-confirmation') }}">click here</a> to confirm with us.@endif</h3>
                       </div>
                   @elseif($applicant->status == 'ADMITTED' && !$student)
                       <div class="alert alert-success">
