31eea7f18d5cb3dd800368677be5ab8669e1bec7
lohicreativity <lohi.sanday@gmail.com>
2024-03-29 15:36:42 +0300

    Update final-program-results-first-semester.blade.php

--- resources/views/dashboard/academic/reports/final-program-results-first-semester.blade.php
+++ resources/views/dashboard/academic/reports/final-program-results-first-semester.blade.php
@@ -464,7 +464,7 @@
           </div><!-- end of col-md-12 -->
         </div><!-- end of row -->
         <div class="row">
-          <div class="col-md-4" style='padding-right:10px'><br>
+          <div class="col-md-4" style='padding-right:10px'>
             <span class="ss-bold" style="font-size:8pt"> KEYS </span> <br>
                 <div class="table-responsive">
                   <table class="table table-condensed table-bordered">
@@ -543,7 +543,7 @@
                     </table>
            </div><!-- end of table-responsive -->
           </div>
-          <div class="col-md-4" style='padding-right:10px;  float:left'> <br>
+          <div class="col-md-4" style='padding-right:10px;  float:left'>
             <span class="ss-bold" style="font-size:8pt"> MODULE CODE/NAME </span> <br>
                 <div class="table-responsive">
                    <table class="table table-condensed table-bordered">
@@ -561,7 +561,7 @@
                 </div><!-- end of table-responsive -->
           </div>
 
-          <div class="col-md-3"><br>
+          <div class="col-md-3">
             <span class="ss-bold" style="font-size:8pt"> DISTRIBUTION OF RESULTS BY SEX </span> <br>
               <div class="table-responsive">
                 <table class="table table-condensed table-bordered">
