8614bddc16cb7eab4d3a9e50d714c883953e8c4e
lohicreativity <lohi.sanday@gmail.com>
2024-04-03 10:59:45 +0300

    Update payer-details.blade.php

--- resources/views/dashboard/finance/payer-details.blade.php
+++ resources/views/dashboard/finance/payer-details.blade.php
@@ -178,8 +178,8 @@
 													{{ number_format($payments->amount,2) }} 
 												
 												@endif
-												{{ count($paid_receipts)}}
-											   <td title=@foreach($paid_receipts as $receipt) @if($receipt->bill_id == $payments->reference_no) "TZS {{ number_format($receipt->paid_amount,2) }} paid on {{ date('Y-m-d',strtotime($receipt->created_at)) }}" @endif @endforeach>
+								
+											   <td @foreach($paid_receipts as $receipt) @if($receipt->bill_id == $payments->reference_no)  title="TZS {{ number_format($receipt->paid_amount,2) }} paid on {{ date('Y-m-d',strtotime($receipt->created_at)) }}"> @endif @endforeach
 												@if ($payments->gatewayPayment)
 													@if (str_contains($payments->feeType->name,'Tuition'))
 														@foreach($total_paid_fee as $fee)
