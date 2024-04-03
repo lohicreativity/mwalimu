56a1060be10d3e112d522225ce143e8e6e961f7b
lohicreativity <lohi.sanday@gmail.com>
2024-04-03 10:40:48 +0300

    Update StaffController.php

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -336,7 +336,7 @@ class StaffController extends Controller
             }
 
             $paid_receipts = GatewayPayment::select('bill_id','payment_channel','cell_number','psp_receipt_no','psp_name','created_at')->whereIn('bill_id',$reference_no)->get();
-return $paid_receipts;
+return $paid_as_student;
             $data = [
 				'payer'=>$student_payer? $student_payer : $applicant_payer,
 				'category'=>$student_payer? 'student' : 'applicant',
