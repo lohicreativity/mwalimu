9d2a166d5fd65850b6bf2d5e28a43c54d61d7270
lohicreativity <lohi.sanday@gmail.com>
2024-04-18 20:59:00 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -3264,11 +3264,16 @@ class ExaminationResultController extends Controller
                                   ->whereHas('student.applicant',function($query)use($staff){$query->where('campus_id',$staff->campus_id);})
                                   ->with(['moduleAssignment.programModuleAssignment.module.ntaLevel:id,name','student:id,gender'])->get();
 
-      $module_assignments = ModuleAssignment::whereHas('programModuleAssignment',function($query) use($request){$query->where('study_academic_year_id',$request->get('study_academic_year_id'));})
+      $module_assignment = ModuleAssignment::whereHas('programModuleAssignment',function($query) use($request){$query->where('study_academic_year_id',$request->get('study_academic_year_id'));})
                                             ->whereHas('programModuleAssignment.campusProgram',function($query) use($staff){$query->where('campus_id',$staff->campus_id);})
                                             ->where('study_academic_year_id',$request->get('study_academic_year_id'))->get();
 
-
+      $module_assignments = [];
+      foreach($module_assignment as $assignment){
+         if(ExaminationResult::where('module_assignment_id')->first()){
+            $module_assignments[] = $assignment;
+         }
+      }
 
 return count($module_assignments);
       foreach($results as $result){
