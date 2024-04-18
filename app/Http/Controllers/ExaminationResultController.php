3a855c56046e645a6696778955d63d7a8ecb8498
lohicreativity <lohi.sanday@gmail.com>
2024-04-18 14:24:10 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -3293,39 +3293,39 @@ class ExaminationResultController extends Controller
                         //       }
                         // }
 
-                        if($result->final_exam_remark == 'POSTPONED'){
-                              $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['post_students'] += 1;
+                        // if($result->final_exam_remark == 'POSTPONED'){
+                        //       $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['post_students'] += 1;
 
-                              $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['post_students_rate'] = $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['post_students']*100/$report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['total_students'];
+                        //       $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['post_students_rate'] = $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['post_students']*100/$report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['total_students'];
 
-                              if($result->student->gender == 'M'){
-                                 $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['ML']['post_students'] += 1;
-                              }
+                        //       if($result->student->gender == 'M'){
+                        //          $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['ML']['post_students'] += 1;
+                        //       }
 
-                              if($result->student->gender == 'F'){
-                                 $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['FL']['post_students'] += 1;
-                              }
-                        }
+                        //       if($result->student->gender == 'F'){
+                        //          $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['FL']['post_students'] += 1;
+                        //       }
+                        // }
 
-                        if($result->final_exam_remark == 'INCOMPLETE'){
-                              $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['inc_students'] += 1;
+                        // if($result->final_exam_remark == 'INCOMPLETE'){
+                        //       $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['inc_students'] += 1;
 
-                              $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['inc_students_rate'] = $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['inc_students']*100/$report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['total_students'];
+                        //       $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['inc_students_rate'] = $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['inc_students']*100/$report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['total_students'];
 
-                              if($result->student->gender == 'M'){
-                                 $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['ML']['inc_students'] += 1;
-                              }
+                        //       if($result->student->gender == 'M'){
+                        //          $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['ML']['inc_students'] += 1;
+                        //       }
 
-                              if($result->student->gender == 'F'){
-                                 $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['FL']['inc_students'] += 1;
-                              }
-                        }
+                        //       if($result->student->gender == 'F'){
+                        //          $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['FL']['inc_students'] += 1;
+                        //       }
+                        // }
 
-                        if($result->final_exam_remark == 'INCOMPLETE' || $result->final_exam_remark == 'POSTPONED'){
-                              $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['miss_take_students'] += 1;
+                        // if($result->final_exam_remark == 'INCOMPLETE' || $result->final_exam_remark == 'POSTPONED'){
+                        //       $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['miss_take_students'] += 1;
 
-                              $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['miss_take_students_rate'] = $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['miss_take_students']*100/$report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['total_students'];
-                        }
+                        //       $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['miss_take_students_rate'] = $report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['miss_take_students']*100/$report[$result->moduleAssignment->programModuleAssignment->module->ntaLevel->name][$department->name][$program->name]['total_students'];
+                        // }
                   
                      }  
                   }
