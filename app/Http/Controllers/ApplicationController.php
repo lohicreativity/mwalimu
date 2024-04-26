e25664cbd9ac98a5b7b6e064c574a66d6a67ee2e
lohicreativity <lohi.sanday@gmail.com>
2024-04-26 13:54:18 +0300

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -9654,7 +9654,7 @@ class ApplicationController extends Controller
      * Submit internal transfer
      */
     public function submitInternalTransfer(Request $request)
-    {
+    {return 1;
 		DB::beginTransaction();
         $student = Student::with(['applicant.selections.campusProgram','applicant.nectaResultDetails.results','applicant.nacteResultDetails.results','applicant.programLevel','applicant.campus','applicant.nextOfKin','applicant.intake'])->find($request->get('student_id'));
 
