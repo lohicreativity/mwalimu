799087c557552151434b4aad1d4d088110a65074
lohicreativity <lohi.sanday@gmail.com>
2024-04-27 10:47:26 +0300

    Update ModuleAssignmentController.php

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -551,7 +551,12 @@ class ModuleAssignmentController extends Controller
      * Show total students CSV
      */
     public function totalStudentsFormattedCSV(Request $request, $id)
-    {
+    { return Student::select('id','registration_number','studentship_status_id')
+        ->whereHas('studentshipStatus',function($query){$query->where('name','ACTIVE')->OrWhere('name','RESUMED');})
+        ->whereHas('examinationResults', function($query) {$query->where('module_assignment_id',693)->whereNotNull('final_uploaded_at')
+                                                                                         ->where('final_exam_remark','FAIL');})
+        ->whereHas('annualRemarks', function($query){$query->where('remark','SUPP');})
+        ->get();
         try{
             $module_assignment = ModuleAssignment::with(['programModuleAssignment.campusProgram.program.department','programModuleAssignment.campusProgram.campus',
 														 'studyAcademicYear.academicYear','programModuleAssignment.module','programModuleAssignment.students','module'])->findOrFail($id);
