a648b98b960ea6661fad542f39f9be8463573670
lohicreativity <lohi.sanday@gmail.com>
2024-04-27 14:06:25 +0300

    Update ModuleAssignmentController.php

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -1309,7 +1309,14 @@ class ModuleAssignmentController extends Controller
                 if($request->get('assessment_plan_id') == 'SUPPLEMENTARY'){
                     $invalid_students = [];
                     foreach($uploaded_students as $up_stud){
-                        return $up_stud;
+                        return Student::whereHas('academicStatus',function($query){$query->where('name','SUPP')->orWhere('name','POSTPONED');}) // Covers SUPP and SPECIAL EXAM cases
+                        //->whereHas('studentshipStatus',function($query){$query->where('name','ACTIVE')->orWhere('name','RESUMED');})
+                        ->whereHas('registrations',function($query) use($module_assignment){$query->where('year_of_study',$module_assignment->programModuleAssignment->year_of_study)
+                                                                                                  ->where('semester_id',$module_assignment->programModuleAssignment->semester_id)
+                                                                                                  ->where('study_academic_year_id',$module_assignment->programModuleAssignment->study_academic_year_id);})
+                        ->where('campus_program_id',$module_assignment->programModuleAssignment->campus_program_id)
+                        ->where('registration_number',$up_stud->registration_number)
+                        ->count();
                         if(Student::whereHas('academicStatus',function($query){$query->where('name','SUPP')->orWhere('name','POSTPONED');}) // Covers SUPP and SPECIAL EXAM cases
                                   //->whereHas('studentshipStatus',function($query){$query->where('name','ACTIVE')->orWhere('name','RESUMED');})
                                   ->whereHas('registrations',function($query) use($module_assignment){$query->where('year_of_study',$module_assignment->programModuleAssignment->year_of_study)
