1e9bcff16a55dc5ee29ca8c9976b23953ebdfc59
lohicreativity <lohi.sanday@gmail.com>
2024-04-29 17:43:57 +0300

    Update ModuleAssignmentController.php

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -1269,7 +1269,7 @@ class ModuleAssignmentController extends Controller
                                       ->where('campus_program_id',$module_assignment->programModuleAssignment->campus_program_id)
                                       ->where('registration_number',$up_stud->registration_number)
                                       ->count() == 0){
-                                if($module_assignment->module->ntaLevel->id == 4 && $up_stud->academic_status_id == 3){
+                                if($module_assignment->module->ntaLevel->id == 4 && $up_stud->academic_status_id == 3){ return 1;
                                     if(Student::whereHas('academicStatus',function($query){$query->where('name','CARRY');}) // Covers CARRY cases
                                             //->whereHas('studentshipStatus',function($query){$query->where('name','ACTIVE')->orWhere('name','RESUMED');})
                                             ->whereHas('registrations',function($query) use($module_assignment){$query->where('year_of_study',$module_assignment->programModuleAssignment->year_of_study + 1)
@@ -1286,10 +1286,10 @@ class ModuleAssignmentController extends Controller
                                                     ->where('study_academic_year_id',$module_assignment->study_academic_year_id)
                                                     ->where('semester_id',$module_assignment->programModuleAssignment->semester_id)
                                                     ->where('status','APPROVED')
-                                                    ->count() == 0){
+                                                    ->count() == 0){ return 2;
                                  $invalid_students[] = $up_stud;
                  
-                                }else{
+                                }else{ return 3;
                                     $invalid_students[] = $up_stud;
                                 }    
                             }
