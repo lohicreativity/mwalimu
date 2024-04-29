2d2908d2eba79dff2e7ddc13b12901736058d419
lohicreativity <lohi.sanday@gmail.com>
2024-04-29 13:22:57 +0300

    Update ModuleAssignmentController.php

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -1316,10 +1316,18 @@ class ModuleAssignmentController extends Controller
                                                                                                             ->where('study_academic_year_id',$module_assignment->programModuleAssignment->study_academic_year_id);})
                                   ->where('campus_program_id',$module_assignment->programModuleAssignment->campus_program_id)
                                   ->where('registration_number',$up_stud->registration_number)
-                                  ->count() == 0){ return 1;
+                                  ->count() == 0){
                             $invalid_students[] = $up_stud;
                         }elseif($module_assignment->module->ntaLevel->id == 4){
-                            return 2;
+                            return Student::whereHas('academicStatus',function($query){$query->where('name','CARRY');}) // Covers CARRY cases
+                            //->whereHas('studentshipStatus',function($query){$query->where('name','ACTIVE')->orWhere('name','RESUMED');})
+                            ->whereHas('registrations',function($query) use($module_assignment){$query->where('year_of_study',$module_assignment->programModuleAssignment->year_of_study + 1)
+                                                                                                      ->where('semester_id',$module_assignment->programModuleAssignment->semester_id)
+                                                                                                      ->where('study_academic_year_id',$module_assignment->programModuleAssignment->study_academic_year_id);})
+                            ->where('campus_program_id',$module_assignment->programModuleAssignment->campus_program_id)
+                            ->where('registration_number',$up_stud->registration_number)
+                            ->get();
+                            
                             if(Student::whereHas('academicStatus',function($query){$query->where('name','CARRY');}) // Covers CARRY cases
                                       //->whereHas('studentshipStatus',function($query){$query->where('name','ACTIVE')->orWhere('name','RESUMED');})
                                       ->whereHas('registrations',function($query) use($module_assignment){$query->where('year_of_study',$module_assignment->programModuleAssignment->year_of_study + 1)
