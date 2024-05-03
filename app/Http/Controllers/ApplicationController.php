4b18d8c750f122a584c9992b487090015efbec61
lohicreativity <lohi.sanday@gmail.com>
2024-05-03 14:18:25 +0300

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4565,7 +4565,7 @@ class ApplicationController extends Controller
         if($reg_date ==  null){
             return redirect()->back()->with('error','Registration period has not been set');
         }
-        return 10;
+        return 11;
         $now = strtotime(date('Y-m-d'));
         $reg_date_time = strtotime($reg_date);
         $datediff = $reg_date_time - $now;
