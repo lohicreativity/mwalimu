7e6beb5027ed52127a08e9a948503b09cc99d1a8
lohicreativity <lohi.sanday@gmail.com>
2024-05-06 13:54:19 +0300

    Update ModuleAssignmentController.php

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -1911,6 +1911,15 @@ class ModuleAssignmentController extends Controller
                                 $result = new ExaminationResult;
                             }
 
+                            if($result->student_id == 4843){
+                                return SpecialExam::where('student_id',$student->id)
+                                ->where('module_assignment_id',$module_assignment->id)
+                                ->where('type','FINAL')
+                                ->where('semester_id',$module_assignment->programModuleAssignment->semester_id)
+                                ->where('study_academic_year_id',$request->get('study_academic_year_id'))
+                                ->where('status','APPROVED')
+                                ->first();
+                            }
                             $result->module_assignment_id = $request->get('module_assignment_id');
                             $result->student_id = $student->id;
                             if($final_special_exam || $sup_special_exam){
