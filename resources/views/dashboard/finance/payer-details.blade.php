e468b2eef430de6ee15d8158f71910737e70721e
lohicreativity <lohi.sanday@gmail.com>
2024-05-06 16:05:07 +0300

    dd

--- resources/views/dashboard/finance/payer-details.blade.php
+++ resources/views/dashboard/finance/payer-details.blade.php
@@ -190,15 +190,11 @@
 											   <td @if($number_of_receipts > 1) title="@foreach($paid_receipts as $receipt) @if($receipt->bill_id == $payments->reference_no)TZS {{number_format($receipt->paid_amount,2) }} paid on {{ date('Y-m-d',strtotime($receipt->created_at))}}&#10; @endif @endforeach" @endif> 
 												@if ($payments->gatewayPayment)
 													@if (str_contains($payments->feeType->name,'Tuition'))
-														@php
-															$total_paid = 0;
-															foreach($total_paid_fee as $fee){
-																if($payments->reference_no == $fee['reference_no']){
-																	$total_paid =+ $fee['amount'];
-																}
-															}
-														@endphp
-														{{ number_format($total_paid,2) }}
+														@foreach($total_paid_fee as $fee)
+															@if($payments->reference_no == $fee['reference_no'])
+																{{ number_format($fee['amount'],2) }}
+															@endif
+														@endforeach
 													@else
 														{{ number_format($payments->gatewayPayment->paid_amount,2) }} 
 													@endif
