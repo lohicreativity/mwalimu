f95798ec0ec30ba8a35470a754b199508553c1f2
lohicreativity <lohi.sanday@gmail.com>
2024-05-06 14:03:20 +0300

    Update ModuleAssignmentController.php

--- app/Http/Controllers/ModuleAssignmentController.php
+++ app/Http/Controllers/ModuleAssignmentController.php
@@ -1592,7 +1592,7 @@ class ModuleAssignmentController extends Controller
                     }
                 }elseif($request->get('assessment_plan_id') == 'SUPPLEMENTARY'){
                     $semester_remark = SemesterRemark::where('semester_id',$module_assignment->programModuleAssignment->semester_id)
-                                                     ->where('study_academic_year_id',$request->get('study_academic_year_id'))
+                                                     ->where('study_academic_year_id',$module_assignment->programModuleAssignment->study_academic_year_id)
                                                      ->where('student_id',$student->id)
                                                      ->first();
 
@@ -1614,12 +1614,12 @@ class ModuleAssignmentController extends Controller
                                                         ->where('module_assignment_id',$module_assignment->id)
                                                         ->where('type','FINAL')
                                                         ->where('semester_id',$module_assignment->programModuleAssignment->semester_id)
-                                                        ->where('study_academic_year_id',$request->get('study_academic_year_id'))
+                                                        ->where('study_academic_year_id',$module_assignment->programModuleAssignment->study_academic_year_id)
                                                         ->where('status','APPROVED')
                                                         ->first();
                     $grading_policy = GradingPolicy::where('nta_level_id',$module_assignment->module->ntaLevel->id)
                                                     ->where('grade','C')
-                                                    ->where('study_academic_year_id', $request->get('study_academic_year_id'))
+                                                    ->where('study_academic_year_id', $module_assignment->programModuleAssignment->study_academic_year_id)
                                                     ->first();
 
                     $upload_allowed = true;
@@ -1878,14 +1878,14 @@ class ModuleAssignmentController extends Controller
                                                            ->where('module_assignment_id',$module_assignment->id)
                                                            ->where('type','SUPP')
                                                            ->where('semester_id',$module_assignment->programModuleAssignment->semester_id)
-                                                           ->where('study_academic_year_id',$request->get('study_academic_year_id'))
+                                                           ->where('study_academic_year_id',$module_assignment->programModuleAssignment->study_academic_year_id)
                                                            ->where('status','APPROVED')
                                                            ->first();
                             $final_special_exam = SpecialExam::where('student_id',$student->id)
                                                              ->where('module_assignment_id',$module_assignment->id)
                                                              ->where('type','FINAL')
                                                              ->where('semester_id',$module_assignment->programModuleAssignment->semester_id)
-                                                             ->where('study_academic_year_id',$request->get('study_academic_year_id'))
+                                                             ->where('study_academic_year_id',$module_assignment->programModuleAssignment->study_academic_year_id)
                                                              ->where('status','APPROVED')
                                                              ->first();
                             // REMOVED because we do not deal with semester or annual postponement during supplementary exams
@@ -1896,7 +1896,7 @@ class ModuleAssignmentController extends Controller
                             //                             ->first();
                             $grading_policy = GradingPolicy::where('nta_level_id',$module_assignment->module->ntaLevel->id)
                                                            ->where('grade','C')
-                                                           ->where('study_academic_year_id', $request->get('study_academic_year_id'))
+                                                           ->where('study_academic_year_id', $module_assignment->programModuleAssignment->study_academic_year_id)
                                                            ->first();
 
                             $upload_allowed = true;
@@ -1911,16 +1911,16 @@ class ModuleAssignmentController extends Controller
                                 $result = new ExaminationResult;
                             }
 
-                            if($result->student_id == 4843){
-                                return $module_assignment->id.'-'.$request->get('study_academic_year_id').'-'.$module_assignment->programModuleAssignment->semester_id;
-                                return SpecialExam::where('student_id',$student->id)
-                                ->where('module_assignment_id',$module_assignment->id)
-                                ->where('type','FINAL')
-                                ->where('semester_id',$module_assignment->programModuleAssignment->semester_id)
-                                ->where('study_academic_year_id',$request->get('study_academic_year_id'))
-                                ->where('status','APPROVED')
-                                ->first();
-                            }
+                            // if($result->student_id == 4843){
+                            //     return $module_assignment->id.'-'.$request->get('study_academic_year_id').'-'.$module_assignment->programModuleAssignment->semester_id;
+                            //     return SpecialExam::where('student_id',$student->id)
+                            //     ->where('module_assignment_id',$module_assignment->id)
+                            //     ->where('type','FINAL')
+                            //     ->where('semester_id',$module_assignment->programModuleAssignment->semester_id)
+                            //     ->where('study_academic_year_id',$request->get('study_academic_year_id'))
+                            //     ->where('status','APPROVED')
+                            //     ->first();
+                            // }
                             $result->module_assignment_id = $request->get('module_assignment_id');
                             $result->student_id = $student->id;
                             if($final_special_exam || $sup_special_exam){
