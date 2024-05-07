4cd27d1c58420d896ea7735a288318d3010b7cd9
lohicreativity <lohi.sanday@gmail.com>
2024-05-07 10:07:25 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -3287,7 +3287,7 @@ class ExaminationResultController extends Controller
 
             foreach($department->programs as $program){
 
-               if($program->nta_level_id == $level->id && $program->pivot->campus_id == $staff->campus_id){                return $program;
+               if($program->nta_level_id == $level->id && $program->pivot->campus_id == $staff->campus_id){
                   //$report[$level->name][$department->name]['programs'][] = $program->name;
                   $report[$level->name][$department->name][$program->name]['total_students'] = 0;
                   $report[$level->name][$department->name][$program->name]['pass_students'] = 0;
