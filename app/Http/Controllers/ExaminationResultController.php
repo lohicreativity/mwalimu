13882acbb216b9b2e67134440f1fa82112d6fe94
lohicreativity <lohi.sanday@gmail.com>
2024-05-09 10:34:21 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -305,7 +305,7 @@ class ExaminationResultController extends Controller
                   }else{
                      $processed_result = $result;
                   }
-   return 11;
+   
                   if($result->course_work_remark == 'INCOMPLETE' || $result->final_remark == 'INCOMPLETE' || $result->final_remark == 'POSTPONED'){
                      if($result->course_work_remark == 'INCOMPLETE'){
                         $processed_result->grade = 'IC';
