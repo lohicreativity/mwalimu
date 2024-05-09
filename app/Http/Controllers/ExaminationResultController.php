7a46619b9dbd617a1e8fe7ff6b15dbd883c10447
lohicreativity <lohi.sanday@gmail.com>
2024-05-09 10:18:13 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -297,8 +297,9 @@ class ExaminationResultController extends Controller
                      $course_work_pass_score = $module_assignment_buffer[$result->module_assignment_id]['course_work_pass_score'];
                      $module_pass_mark = $module_assignment_buffer[$result->module_assignment_id]['module_pass_mark'];
                   }
-return $course_work_based;
+
                   if($result->retakeHistory && isset($result->retakeHistory->retakableResults[0])){
+                     return 12;
                      $processed_result = ExaminationResult::find($result->retakeHistory->retakableResults[0]->id);
       
                   }else{
