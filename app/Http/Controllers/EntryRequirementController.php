bddb1ff91f9f39a17d1209b0884bfc3ec4a238b5
lohicreativity <lohi.sanday@gmail.com>
2024-05-29 15:41:10 +0300

    Update EntryRequirementController.php

--- app/Http/Controllers/EntryRequirementController.php
+++ app/Http/Controllers/EntryRequirementController.php
@@ -148,11 +148,11 @@ class EntryRequirementController extends Controller
         $prev_window = ApplicationWindow::where('campus_id',$staff->campus_id)->where('intake_id',$application_window->intake_id)->latest()->offset(1)->first();
 
         $campusProgramIds = [];
-        foreach($application_window->campus_programs as $program){
-         $campusProgramIds[] = $program->pivot->campus_program_id;
-        }
+      //   foreach($application_window->campus_programs as $program){
+      //    $campusProgramIds[] = $program->pivot->campus_program_id;
+      //   }
         
-        return $campusProgramIds;
+        return $$application_window->campus_programs;
         if(!$prev_window){
             return redirect()->back()->with('error','No previous application window');
         }
