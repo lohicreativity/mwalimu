b12f7fc91acaf8b6e59134738e3de7febfebad18
lohicreativity <lohi.sanday@gmail.com>
2024-05-31 13:05:34 +0300

    Update edit-examination-results.blade.php

--- resources/views/dashboard/academic/edit-examination-results.blade.php
+++ resources/views/dashboard/academic/edit-examination-results.blade.php
@@ -95,7 +95,7 @@
 
                       <a href="{{ url('academic/results/'.$student->id.'/'.$result->moduleAssignment->id.'/'.$result->id.'/edit-course-work-results?ac_yr_id='.$result->moduleAssignment->study_academic_year_id.'&year_of_study='.$year_of_study) }}" class="ss-margin-top">Edit Coursework</a>
                     </div>
-                    @if($result->final_remark == 'FAIL' || $result->final_remark == 'PASS' || $result->final_remark == 'INCOMPLETE')
+                    @if($result->final_remark == 'FAIL' || $result->final_remark == 'PASS' )
                       <div class="form-group col-4">
                         {!! Form::label('','Final Score') !!}
                         {!! Form::text('final_score',$result->final_score,$final_score) !!}
