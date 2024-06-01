17554e0d3d44d69ce1674067a3386d56c56342cb
lohicreativity <lohi.sanday@gmail.com>
2024-06-01 08:50:09 +0300

    Update examination-results-report.blade.php

--- resources/views/dashboard/student/examination-results-report.blade.php
+++ resources/views/dashboard/student/examination-results-report.blade.php
@@ -41,7 +41,7 @@
 
             <div class="card">
               <div class="card-header">
-                 <h3>Examination Results</h3>
+                 <h3>Examination Results 2</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
