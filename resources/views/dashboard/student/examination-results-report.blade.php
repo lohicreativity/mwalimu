903d9bc95a8dc2f67adb20df6304fa29ff941667
lohicreativity <lohi.sanday@gmail.com>
2024-06-02 16:14:51 +0300

    Update examination-results-report.blade.php

--- resources/views/dashboard/student/examination-results-report.blade.php
+++ resources/views/dashboard/student/examination-results-report.blade.php
@@ -221,6 +221,7 @@
                             @if(!empty($result->supp_remark) && !$supp_publish_status) FAIL 
                             @elseif($special_exam_status && !empty($result->final_score) && !$supp_publish_status) POSTPONED
                             @elseif($result->supp_remark != null && $supp_publish_status) {{ $result->supp_remark }} 
+                            @else {{ $result->final_exam_remark }} 
                             @endif</td>
                         </tr>
                           @php
