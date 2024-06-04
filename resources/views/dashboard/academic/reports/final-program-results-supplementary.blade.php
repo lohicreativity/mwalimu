1c46366bcfe5e35235b4e2dc298290ccc8a07054
lohicreativity <lohi.sanday@gmail.com>
2024-06-04 19:32:48 +0300

    Update final-program-results-supplementary.blade.php

--- resources/views/dashboard/academic/reports/final-program-results-supplementary.blade.php
+++ resources/views/dashboard/academic/reports/final-program-results-supplementary.blade.php
@@ -361,14 +361,23 @@
                           @foreach($student->semesterRemarks as $rem)
                             @if($rem->semester->name == $mdKey)
                             <td class="ss-font-xs">
-                              @if($rem->supp_remark == 'INCOMPLETE') 
-                                {{ substr($rem->supp_remark,0,4) }} 
-                              @elseif($rem->supp_remark == 'POSTPONED EXAM')
-                                POSE
-                              @else 
-                                {{ $rem->supp_remark }} 
+                              @if($rem->semester->name != 'RETAKE' && $rem->semester->name != 'CARRY' && $rem->semester->name != 'INCOMPLETE')
+                                @if($rem->supp_remark == 'INCOMPLETE') 
+                                  {{ substr($rem->supp_remark,0,4) }} 
+                                @elseif($rem->supp_remark == 'POSTPONED EXAM')
+                                  POSE
+                                @else 
+                                  {{ $rem->supp_remark }} 
+                                @endif
+                              @else
+                                @if($rem->remark == 'INCOMPLETE') 
+                                  {{ substr($rem->remark,0,4) }} 
+                                @elseif($rem->remark == 'POSTPONED EXAM')
+                                  POSE
+                                @else 
+                                  {{ $rem->remark }} 
+                                @endif
                               @endif
-                              
                             </td>
                             @endif
                           @endforeach
