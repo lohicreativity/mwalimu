4df05f3b9b3779c129d961f5e42bbe9c86b113b0
lohicreativity <lohi.sanday@gmail.com>
2024-06-04 08:09:59 +0300

    Update final-student-annual-results.blade.php

--- resources/views/dashboard/academic/reports/final-student-annual-results.blade.php
+++ resources/views/dashboard/academic/reports/final-student-annual-results.blade.php
@@ -410,6 +410,7 @@
 
                       <tr>
                         <td>@if($remark->remark != 'PASS' && $supp_publish_status) <strong>{{ $remark->supp_remark }}</strong> @else <strong>{{ $remark->remark }}</strong> @endif
+                          @if($remark->remark != 'REPEAT' || $remark->remark != 'FAIL&DISCO' || $remark->remark != 'DECEASED' || str_contains($remark->remark, 'POSTPONE'))
                           @if($remark->serialized) 
                             @if(!$supp_publish_status) 
                               @if(!empty(unserialize($remark->serialized)['supp_exams'])) [{{ implode(', ',unserialize($remark->serialized)['supp_exams']) }}] @endif 
@@ -432,6 +433,7 @@
                             @else
                                 @if(!empty(unserialize($remark->supp_serialized)['carry_exams'])) [{{ implode(', ',unserialize($remark->supp_serialized)['carry_exams']) }}] @endif 
                             @endif
+                          @endif
                           @endif
                          </td>
                          <td>@if($remark->gpa) @if($remark->remark != 'PASS' && !$supp_publish_status) N/A @else {{ bcdiv($remark->gpa,1,1) }} @endif @else N/A @endif</td>
