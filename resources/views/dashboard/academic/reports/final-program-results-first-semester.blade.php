03027bf93680665236402f2526d198340cf90c22
lohicreativity <lohi.sanday@gmail.com>
2024-06-05 12:09:34 +0300

    Update final-program-results-first-semester.blade.php

--- resources/views/dashboard/academic/reports/final-program-results-first-semester.blade.php
+++ resources/views/dashboard/academic/reports/final-program-results-first-semester.blade.php
@@ -529,7 +529,131 @@
            </div><!-- end of table-responsive -->
           </div>
 
+          <div class="col-md-4" style='padding-right:10px;  float:left'>
+            <span class="ss-bold" style="font-size:7pt"> MODULE CODE/NAME </span> <br>
+                <div class="table-responsive">
+                   <table class="table table-condensed table-bordered">
+                      <tr>
+                        <td class="ss-bold ss-font-sm">Code</td>
+                        <td class="ss-bold ss-font-sm">Name</td>
+                      </tr>
+                      @foreach($modules as $modKey=>$mod)
+                      <tr>
+                        <td class="ss-font-sm">{{ $modKey }}</td>
+                        <td class="ss-font-sm">{{ $mod['name'] }}</td>
+                      </tr>
+                      @endforeach
+                   </table>
+                </div><!-- end of table-responsive -->
+          </div>
 
+          <div class="col-md-3">
+            <span class="ss-bold" style="font-size:7pt"> DISTRIBUTION OF RESULTS BY SEX </span> <br>
+              <div class="table-responsive">
+                <table class="table table-condensed table-bordered">
+                  <tr>
+                    <td class="ss-bold ss-font-sm">Class/Remark</td>
+                    <td class="ss-center ss-bold ss-font-sm">Male</td>
+                    <td class="ss-center ss-bold ss-font-sm">Female</td>
+                    <td class="ss-center ss-bold ss-font-sm">Total</td>
+                    <td class="ss-center ss-bold ss-font-sm">Percentage</td>
+                  </tr>
+                  @foreach($classifications as $class)
+                    <tr>
+                      @if(str_contains(strtolower($class), 'first'))
+                        <td class="ss-font-sm">{{ $class->name }}</td>
+                        <td class="ss-center ss-font-sm"> {{ $male_first_class_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ $female_first_class_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ $male_first_class_cases + $female_first_class_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ round((($male_first_class_cases + $female_first_class_cases)/count($students))*100,1) }}</td>
+                      @elseif(str_contains(strtolower($class), 'upper second'))
+                        <td class="ss-font-sm">{{ $class->name }}</td>
+                        <td class="ss-center ss-font-sm"> {{ $male_upsecond_class_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ $female_upsecond_class_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ $male_upsecond_class_cases + $female_upsecond_class_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ round((($male_upsecond_class_cases + $female_upsecond_class_cases)/count($students))*100,1) }}</td>
+                      @elseif(strtolower($class) == 'second class'))
+                        <td class="ss-font-sm">{{ $class->name }}</td>
+                        <td class="ss-center ss-font-sm"> {{ $male_second_class_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ $female_second_class_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ $male_second_class_cases + $female_second_class_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ round((($male_second_class_cases + $female_second_class_cases)/count($students))*100,1) }}</td>
+                      @elseif(str_contains(strtolower($class), 'lower'))
+                        <td class="ss-font-sm">{{ $class->name }}</td>
+                        <td class="ss-center ss-font-sm"> {{ $male_lwsecond_class_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ $female_lwsecond_class_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ $male_lwsecond_class_cases + $female_lwsecond_class_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ round((($male_lwsecond_class_cases + $female_lwsecond_class_cases)/count($students))*100,1) }}</td>
+                      @elseif(str_contains(strtolower($class), 'pass'))
+                        <td class="ss-font-sm">{{ $class->name }}</td>
+                        <td class="ss-center ss-font-sm"> {{ $male_pass_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ $female_pass_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ $male_pass_cases + $female_pass_cases }}</td>
+                        <td class="ss-center ss-font-sm">{{ round((($male_pass_cases + $female_pass_cases)/count($students))*100,1) }}</td>
+                      @endif
+                    </tr>
+                  @endforeach
+                    <tr>
+                      <td class="ss-font-sm">Supplementary</td>
+                      <td class="ss-center ss-font-sm">{{ $male_failed_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ $female_failed_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ $male_failed_cases + $female_failed_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ round((($male_failed_cases + $female_failed_cases)/count($students))*100,1) }}</td>
+                    </tr>
+                    <tr>
+                      <td class="ss-font-sm">Retake</td>
+                      <td class="ss-center ss-font-sm">{{ $male_retake_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ $female_retake_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ $male_retake_cases + $female_retake_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ round((($male_retake_cases + $female_retake_cases)/count($students))*100,1) }}</td>
+                    </tr>
+                    @if($student->applicant->program_level_id == 4)
+                    <tr>
+                      <td class="ss-font-sm">Carry</td>
+                      <td class="ss-center ss-font-sm">{{ $male_carry_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ $female_carry_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ $male_carry_cases + $female_carry_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ round((($male_carry_cases + $female_carry_cases)/count($students))*100,1) }}</td>
+                    </tr>
+                    @endif
+                    <tr>
+                      <td class="ss-font-sm">Incomplete</td>
+                      <td class="ss-center ss-font-sm">{{ $male_incomplete_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ $female_incomplete_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ $male_incomplete_cases + $female_incomplete_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ round((($male_incomplete_cases + $female_incomplete_cases)/count($students))*100,1) }}</td>
+                    </tr>
+                    <tr>
+                      <td class="ss-font-sm">Postponement</td>
+                      <td class="ss-center ss-font-sm">{{ $male_postponement_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ $female_postponement_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ $male_postponement_cases + $female_postponement_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ round((($male_postponement_cases + $female_postponement_cases)/count($students))*100,1) }}</td>
+                    </tr>
+                    <tr>
+                      <td class="ss-font-sm">Discoqualification</td>
+                      <td class="ss-center ss-font-sm">{{ $male_disco_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ $female_disco_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ $male_disco_cases + $female_disco_cases }}</td>
+                      <td class="ss-center ss-font-sm">{{ round((($male_disco_cases + $female_disco_cases)/count($students))*100,1) }}</td>
+                    </tr>
+                    <tr>
+                      <td class="ss-bold ss ss-font-sm">Total</td>
+                      <td class="ss-center ss-bold ss ss-font-sm">{{ ($male_disco_cases+$male_postponement_cases+$male_incomplete_cases+$male_carry_cases+$male_retake_cases+$male_failed_cases+$male_pass_cases+$male_lwsecond_class_cases+
+                                                            $male_second_class_cases+$male_upsecond_class_cases+$male_first_class_cases) }}</td>
+                      <td class="ss-center ss-bold ss ss-font-sm">{{ ($female_disco_cases+$female_postponement_cases+$female_incomplete_cases+$female_carry_cases+$female_retake_cases+$female_failed_cases+$female_pass_cases+$female_lwsecond_class_cases+
+                                                            $female_second_class_cases+$female_upsecond_class_cases+$female_first_class_cases) }}</td>
+                      <td class="ss-center ss-bold ss ss-font-sm">{{ ($male_disco_cases+$male_postponement_cases+$male_incomplete_cases+$male_carry_cases+$male_retake_cases+$male_failed_cases+$male_pass_cases+$male_lwsecond_class_cases+
+                                                            $male_second_class_cases+$male_upsecond_class_cases+$male_first_class_cases+$female_disco_cases+$female_postponement_cases+$female_incomplete_cases+$female_carry_cases+$female_retake_cases+$female_failed_cases+$female_pass_cases+$female_lwsecond_class_cases+
+                                                            $female_second_class_cases+$female_upsecond_class_cases+$female_first_class_cases) }}</td>
+                      <td class="ss-center ss-bold ss ss-font-sm">{{ round((($male_disco_cases+$male_postponement_cases+$male_incomplete_cases+$male_carry_cases+$male_retake_cases+$male_failed_cases+$male_pass_cases+$male_lwsecond_class_cases+
+                        $male_second_class_cases+$male_upsecond_class_cases+$male_first_class_cases+$female_disco_cases+$female_postponement_cases+$female_incomplete_cases+$female_carry_cases+$female_retake_cases+$female_failed_cases+$female_pass_cases+$female_lwsecond_class_cases+
+                        $female_second_class_cases+$female_upsecond_class_cases+$female_first_class_cases)/count($students))*100,1) }}</td>
+                    </tr>
+                </table>
+              </div><!-- end of table-responsive -->
+          </div>
+        </div>
         <div class="row">
           <div class="col-md-8">
               <div class="ss-left">
