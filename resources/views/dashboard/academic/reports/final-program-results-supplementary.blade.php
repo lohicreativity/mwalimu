75b9fa102d3160c51b84bbcea827553d7e45214d
lohicreativity <lohi.sanday@gmail.com>
2024-06-06 17:33:59 +0300

    Update final-program-results-supplementary.blade.php

--- resources/views/dashboard/academic/reports/final-program-results-supplementary.blade.php
+++ resources/views/dashboard/academic/reports/final-program-results-supplementary.blade.php
@@ -632,7 +632,7 @@
           </div><!-- end of col-md-12 -->
         </div><!-- end of row -->
         <div class="row">
-          <div class="col-md-4" style='padding-right:10px'>
+          <div class="col-md-3" style='padding-right:10px'>
             <span class="ss-bold" style="font-size:7pt"> KEYS </span> <br>
                 <div class="table-responsive">
                   <table class="table table-condensed table-bordered">
