34256f385e893a3427d5da78cd63a96788aba4aa
lohicreativity <lohi.sanday@gmail.com>
2024-06-07 09:52:42 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -1395,9 +1395,9 @@ class ExaminationResultController extends Controller
                                     ->where('semester_id',$semester_id)
                                     ->where('year_of_study',$year_of_study)
                                     ->first();
-                                    return $remark;
-         }
 
+         }
+         return $remark;
          $remark = SemesterRemark::where('student_id',$case)
          ->where('study_academic_year_id',$ac_yr_id)
          ->where('semester_id',$semester_id)
