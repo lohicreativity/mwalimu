ca97b8381d4ccd2e6079a46e8afbe37a27829c5a
lohicreativity <lohi.sanday@gmail.com>
2024-06-07 14:16:51 +0300

    Update final-student-annual-results.blade.php

--- resources/views/dashboard/academic/reports/final-student-annual-results.blade.php
+++ resources/views/dashboard/academic/reports/final-student-annual-results.blade.php
@@ -376,8 +376,9 @@
                       @foreach($semester->remarks as $remark)
 
                       <tr>
-                        <td>@if($remark->remark != 'PASS') <strong>{{ $remark->supp_remark }} </strong> @else <strong>{{ $remark->remark }}</strong> @endif dssdf
+                        <td>@if($remark->remark != 'PASS') <strong>{{ $remark->supp_remark }} </strong> @else <strong>{{ $remark->remark }}</strong> @endif
                           @if($remark->remark != 'REPEAT' && $remark->remark != 'FAIL&DISCO' && $remark->remark != 'DECEASED' && str_contains($remark->remark, 'POSTPONE'))
+                          fddfdfs
                             @if($remark->serialized) 
                               @if(empty($remark->supp_remark)) 
                                 @if(!empty(unserialize($remark->serialized)['supp_exams'])) [{{ implode(', ',unserialize($remark->serialized)['supp_exams']) }}] @endif 
