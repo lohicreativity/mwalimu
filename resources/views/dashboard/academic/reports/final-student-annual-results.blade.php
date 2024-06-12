fe442f90f16fe98b585141ff9b3ee18cd4f48d08
lohicreativity <lohi.sanday@gmail.com>
2024-06-12 19:10:42 +0300

    Update final-student-annual-results.blade.php

--- resources/views/dashboard/academic/reports/final-student-annual-results.blade.php
+++ resources/views/dashboard/academic/reports/final-student-annual-results.blade.php
@@ -253,7 +253,8 @@
                             @if($result->moduleAssignment->programModuleAssignment->semester_id == $semester->id && $result->moduleAssignment->programModuleAssignment->id == $program->id)
                               <tr>
                                   <td>{{ $count }}</td>
-                                  <td>@if($result->final_exam_remark != 'POSTPONED')<a href="{{ url('academic/results/'.$student->id.'/'.$study_academic_year->id.'/'.$result->moduleAssignment->programModuleAssignment->id.'/edit-student-results?year_of_study='.$year_of_study.'&exam_type='.$result->exam_type) }}">{{ $result->moduleAssignment->module->code }}</a> @else {{ $result->moduleAssignment->module->code }} @endif</td>
+                                  <td>@if($result->course_work_score != null || $result->final_score != null || $result->supp_score != null)
+                                    <a href="{{ url('academic/results/'.$student->id.'/'.$study_academic_year->id.'/'.$result->moduleAssignment->programModuleAssignment->id.'/edit-student-results?year_of_study='.$year_of_study.'&exam_type='.$result->exam_type) }}">{{ $result->moduleAssignment->module->code }}</a> @else {{ $result->moduleAssignment->module->code }} @endif</td>
                                   <td>{{ $result->moduleAssignment->module->name }}</td>
                                   <td>
                                     @if(!$supp_publish_status)
