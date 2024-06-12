ecf68816ad4c107d2cde4522c44942d72d7b24c7
lohicreativity <lohi.sanday@gmail.com>
2024-06-12 19:09:31 +0300

    Update final-student-annual-results.blade.php

--- resources/views/dashboard/academic/reports/final-student-annual-results.blade.php
+++ resources/views/dashboard/academic/reports/final-student-annual-results.blade.php
@@ -145,7 +145,7 @@
                               @endphp
                                 <tr>
                                   <td>{{ $count }}</td>
-                                  <td>@if($result->final_exam_remark != 'POSTPONED' && ($result->course_work_score != null || $result->final_score != null || $result->supp_score != null))
+                                  <td>@if($result->course_work_score != null && $result->final_score != null && $result->supp_score != null)
                                         <a href="{{ url('academic/results/'.$student->id.'/'.$study_academic_year->id.'/'.$result->moduleAssignment->programModuleAssignment->id.'/edit-student-results?year_of_study='.$year_of_study.'&exam_type='.$result->exam_type) }}">
                                           {{ $result->moduleAssignment->module->code }}
                                         </a> 
