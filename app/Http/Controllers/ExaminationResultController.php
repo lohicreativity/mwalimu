e2dbed890946e7a76de6077f9b76d3041a91d6ab
lohicreativity <lohi.sanday@gmail.com>
2024-06-12 08:00:21 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -1423,17 +1423,13 @@ class ExaminationResultController extends Controller
                   $results = ExaminationResult::where('student_id',$case)
                                              ->whereIn('module_assignment_id',$module_assignmentIDs)
                                              ->get();
-
-                                             if($case == 3061){
-                                                return $results;
-                                             }
                }
 
                foreach($results as $result){
                   $course_work_based = $module_assignment_buffer[$result->module_assignment_id]['course_work_based'];
                   $module_pass_mark = $module_assignment_buffer[$result->module_assignment_id]['module_pass_mark'];
 
-                  if(count($special_exam_status) > 0){
+                  if(count($special_exam_status) > 0 && $result->final_exam_remark == 'POSTPONED'){
                      foreach($special_exam_status as $special){
                         if($result->module_assignment_id == $special->module_assignment_id){
                            if($result->course_work_remark == 'INCOMPLETE' || $result->final_remark == 'INCOMPLETE' || $result->final_remark == 'POSTPONED'){
