9a53635c10e4570cf8b142dda65c0c48a6f9a250
lohicreativity <lohi.sanday@gmail.com>
2024-06-15 21:36:10 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -4303,9 +4303,9 @@ class ExaminationResultController extends Controller
       $pass_status = 'PASS'; 
       $supp_exams = $retake_exams = $carry_exams = [];
       foreach($student_results as $stu_result){
-         //if($stu_result->module_assignmnet_id == 452){
-            return $stu_result;
-        // }
+         if($stu_result->module_assignmnet_id == 452){
+            return $$stu_result->module_assignment_id ;
+         }
          if($stu_result->supp_remark == 'FAIL' || ($stu_result->final_remark == 'POSTPONED' && $stu_result->final_exam_remark == 'FAIL' && $stu_result->supp_uploaded_at != null)){
             $pass_status = 'SUPP'; 
             $supp_exams[] = $stu_result->moduleAssignment->module->code;
