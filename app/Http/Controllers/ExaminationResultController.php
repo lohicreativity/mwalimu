0e1a7c000de9e339857e4bf6aea0d329fc53478d
lohicreativity <lohi.sanday@gmail.com>
2024-06-17 13:56:15 +0300

    Update ExaminationResultController.php

--- app/Http/Controllers/ExaminationResultController.php
+++ app/Http/Controllers/ExaminationResultController.php
@@ -4249,7 +4249,7 @@ class ExaminationResultController extends Controller
          }
       }
 
-
+return $student_results;
       $pass_status = 'PASS'; 
       $supp_exams = $retake_exams = $carry_exams = [];
       foreach($student_results as $stu_result){
