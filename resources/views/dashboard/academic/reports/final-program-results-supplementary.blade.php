77625d9a94ebbdfb8b4f56dce6c56b9b2c1e3c45
lohicreativity <lohi.sanday@gmail.com>
2024-06-17 14:46:33 +0300

    Update final-program-results-supplementary.blade.php

--- resources/views/dashboard/academic/reports/final-program-results-supplementary.blade.php
+++ resources/views/dashboard/academic/reports/final-program-results-supplementary.blade.php
@@ -469,7 +469,7 @@
                         </tr>
                       </thead>
                       <tbody>
-                      @foreach($special_exam_students as $key=>$student)
+                      @foreach($special_exam_students->sortBy('student->registration_number') as $key=>$student)
                         @php $s_total_students++; @endphp
                       <tr>
                         <td class="ss-font-xs">{{ $count }}</td>
