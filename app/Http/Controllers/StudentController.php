ab4e81782d376f0258f09c5d5171ecb448d52f5b
lohicreativity <lohi.sanday@gmail.com>
2024-06-18 21:18:06 +0300

    Update StudentController.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1027,7 +1027,7 @@ class StudentController extends Controller
 
         DB::beginTransaction();
         $study_academic_year = StudyAcademicYear::find(session('active_academic_year_id'));
-
+return $student->applicant->intake_id.'/'.explode('/',$student->registration_number)[3].'/'.substr(explode('/',$study_academic_year->academicYear->year)[1],2);
         if($student->applicant->intake_id == 2 && explode('/',$student->registration_number)[3] == substr(explode('/',$study_academic_year->academicYear->year)[1],2)){return 1;
           $ac_yr_id = $study_academic_year->id + 1;
         }else{return 2;
