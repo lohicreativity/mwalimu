ddb9a90750e71ff62289e8313549b069a4bd2859
lohicreativity <lohi.sanday@gmail.com>
2024-06-21 18:04:33 +0300

    Update auth-header.blade.php

--- resources/views/layouts/auth-header.blade.php
+++ resources/views/layouts/auth-header.blade.php
@@ -38,13 +38,13 @@
           <a href="{{ url('staff/staff/'.$staff->id.'/show') }}" class="dropdown-item">
             <i class="fas fa-user mr-2"></i> Profile
           </a>
-          <a href="{{ url('change-password') }}" class="dropdown-item">
+          <a href="{{ url('staff-change-password') }}" class="dropdown-item">
             <i class="fas fa-lock mr-2"></i> Change Password
           </a>
           <div class="dropdown-divider"></div>
           @endif
-          @if(isset($staff))
-          <a href="{{ url('staff-change-password') }}" class="dropdown-item">
+          @if(!isset($staff))
+          <a href="{{ url('change-password') }}" class="dropdown-item">
             <i class="fas fa-lock mr-2"></i> Change Password
           </a>
           @endif
