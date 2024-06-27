c27e9a7b48b8b76543025b075ba6b530c3625c5c
lohicreativity <lohi.sanday@gmail.com>
2024-06-27 20:54:19 +0300

    dd

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -3803,10 +3803,6 @@ class ApplicationController extends Controller
                 'required',
                 'string',
                 'min:8',             // must be at least 10 characters in length
-                'regex:/[a-z]/',      // must contain at least one lowercase letter
-                'regex:/[A-Z]/',      // must contain at least one uppercase letter
-                'regex:/[0-9]/',      // must contain at least one digit
-                'regex:/[@$!%*#?&]/', // must contain a special character
             ],
 			'password_confirmation'=>'same:password'
         ]);
