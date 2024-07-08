9b00fc9d2f19044c66a0abd093c111c191e4f79e
lohicreativity <lohi.sanday@gmail.com>
2024-07-08 18:17:51 +0300

    Update GradingPolicyController.php

--- app/Http/Controllers/GradingPolicyController.php
+++ app/Http/Controllers/GradingPolicyController.php
@@ -158,7 +158,7 @@ class GradingPolicyController extends Controller
         	return redirect()->back()->with('error','Grading policy overlapping for this NTA level');
         }
 
-        if(CourseWorkResult::where('study_academic_year_id',$request->get('study_academic_year_id'))->first()){
+        if(CourseWorkResult::whereHas('moduleAssignment',function($query) use($request){$query->where('study_academic_year_id',$request->get('study_academic_year_id'));})->first()){
             return redirect()->back()->with('error','Cannot be changed because the grading policy has already been used');
         }
 
