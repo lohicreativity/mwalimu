a224bf9e7894de32ddbf91ed93b70624cc9bf240
lohicreativity <lohi.sanday@gmail.com>
2024-07-08 09:01:19 +0300

    Update entry-requirements.blade.php

--- resources/views/dashboard/application/entry-requirements.blade.php
+++ resources/views/dashboard/application/entry-requirements.blade.php
@@ -1311,7 +1311,7 @@
                                      <div class="card-body">
                                        
                                        <div class="row">
-                                        <div class="form-group col-3">
+                                        <div class="form-group col-4">
                                           {!! Form::label('','Programme') !!}
                                           <select name="campus_program_id" class="form-control" required>
                                             <option value="">Select Programme</option>
@@ -1321,7 +1321,7 @@
                                           </select>
                                         </div>
 
-                                        <div class="form-group col-3">
+                                        <div class="form-group col-4">
                                           {!! Form::label('','Certificate Majors') !!}
                                           <select name="equivalent_majors[]" class="form-control ss-select-tags" multiple="multiple">
                                              {{--
@@ -1380,7 +1380,7 @@
                                              </select>
                                         </div>
 
-                                        <div class="form-group col-3">
+                                        <div class="form-group col-4">
                                           {!! Form::label('','Principle Pass Points') !!}
                                           {!! Form::text('principle_pass_points',$requirement->principle_pass_points,$principle_pass_points) !!}
                                         </div>
