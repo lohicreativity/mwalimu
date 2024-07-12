137a3fc31e6786b20265201d797ad36b73167baa
lohicreativity <lohi.sanday@gmail.com>
2024-07-12 15:45:08 +0300

    Update select-programs.blade.php

--- resources/views/dashboard/application/select-programs.blade.php
+++ resources/views/dashboard/application/select-programs.blade.php
@@ -192,54 +192,51 @@
                       </div>
                     </div>
                   @else   
-
-                          <div class="card">
-                            <!-- /.card-header -->
-                            <div class="card-body">      
-                    <div class="card">
-                      <div class="card-header">
-                        <h3 class="card-title">Selections</h3>
-                      </div>
-                      <!-- /.card-header -->
-                      <div class="card-body">
-                        <table class="table table-bordered">
-                            <thead>
-                              <tr>
-                                <th>Choice</th>
-                                <th>Programme</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              <tr>
-                                <td>1</td>
-                                <td>@if(App\Domain\Application\Models\ApplicantProgramSelection::hasSelectedChoice($applicant->freshSelections,1)) 1st Choice Selected 
-                                    @else <a href="#" data-toggle="modal" data-target="#ss-first-choice">Select 1st Choice Programme</a> @endif</td>
-                              </tr>
-                              @if(App\Domain\Application\Models\ApplicantProgramSelection::hasSelectedChoice($applicant->freshSelections,1))
-                                <tr>
-                                  <td>2</td>
-                                  <td>@if(App\Domain\Application\Models\ApplicantProgramSelection::hasSelectedChoice($applicant->freshSelections,2)) 2nd Choice Selected 
-                                      @else <a href="#" data-toggle="modal" data-target="#ss-second-choice">Select 2nd Choice Programme</a>@endif</td>
-                                </tr>
-                              @endif
-                              @if(App\Domain\Application\Models\ApplicantProgramSelection::hasSelectedChoice($applicant->freshSelections,2))
-                                <tr>
-                                  <td>3</td>
-                                  <td>@if(App\Domain\Application\Models\ApplicantProgramSelection::hasSelectedChoice($applicant->freshSelections,3)) 3rd Choice Selected 
-                                      @else <a href="#" data-toggle="modal" data-target="#ss-third-choice">Select 3rd Choice Programme</a>@endif</td>
-                                </tr>
-                              @endif
-                              @if(App\Domain\Application\Models\ApplicantProgramSelection::hasSelectedChoice($applicant->freshSelections,3))
-                                <tr>
-                                  <td>4</td>
-                                  <td>@if(App\Domain\Application\Models\ApplicantProgramSelection::hasSelectedChoice($applicant->freshSelections,4)) 4th Choice Selected 
-                                      @else <a href="#" data-toggle="modal" data-target="#ss-forth-choice">Select 4th Choice Programme</a>@endif</td>
-                                </tr>
-                              @endif
-                            </tbody>
-                        </table>
-                      </div>
-                    </div></div></div>
+                      <div class="col-12">      
+                        <div class="card">
+                          <div class="card-header">
+                            <h3 class="card-title">Selections</h3>
+                          </div>
+                          <!-- /.card-header -->
+                          <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                  <tr>
+                                    <th>Choice</th>
+                                    <th>Programme</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <td>1</td>
+                                    <td>@if(App\Domain\Application\Models\ApplicantProgramSelection::hasSelectedChoice($applicant->freshSelections,1)) 1st Choice Selected 
+                                        @else <a href="#" data-toggle="modal" data-target="#ss-first-choice">Select 1st Choice Programme</a> @endif</td>
+                                  </tr>
+                                  @if(App\Domain\Application\Models\ApplicantProgramSelection::hasSelectedChoice($applicant->freshSelections,1))
+                                    <tr>
+                                      <td>2</td>
+                                      <td>@if(App\Domain\Application\Models\ApplicantProgramSelection::hasSelectedChoice($applicant->freshSelections,2)) 2nd Choice Selected 
+                                          @else <a href="#" data-toggle="modal" data-target="#ss-second-choice">Select 2nd Choice Programme</a>@endif</td>
+                                    </tr>
+                                  @endif
+                                  @if(App\Domain\Application\Models\ApplicantProgramSelection::hasSelectedChoice($applicant->freshSelections,2))
+                                    <tr>
+                                      <td>3</td>
+                                      <td>@if(App\Domain\Application\Models\ApplicantProgramSelection::hasSelectedChoice($applicant->freshSelections,3)) 3rd Choice Selected 
+                                          @else <a href="#" data-toggle="modal" data-target="#ss-third-choice">Select 3rd Choice Programme</a>@endif</td>
+                                    </tr>
+                                  @endif
+                                  @if(App\Domain\Application\Models\ApplicantProgramSelection::hasSelectedChoice($applicant->freshSelections,3))
+                                    <tr>
+                                      <td>4</td>
+                                      <td>@if(App\Domain\Application\Models\ApplicantProgramSelection::hasSelectedChoice($applicant->freshSelections,4)) 4th Choice Selected 
+                                          @else <a href="#" data-toggle="modal" data-target="#ss-forth-choice">Select 4th Choice Programme</a>@endif</td>
+                                    </tr>
+                                  @endif
+                                </tbody>
+                            </table>
+                          </div>
+                        </div></div>
                   @endif
                 @endif
               @endif
