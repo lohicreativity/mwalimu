15fa5017354d449d6ce683aaac82a6d26ed1f997
lohicreativity <lohi.sanday@gmail.com>
2024-07-12 15:33:45 +0300

    Update select-programs.blade.php

--- resources/views/dashboard/application/select-programs.blade.php
+++ resources/views/dashboard/application/select-programs.blade.php
@@ -641,9 +641,6 @@
         </div>
         @if(count($applicant->freshSelections) > 0) 
         <div class="card">
-          <div class="card-header">
-            <h3 class="card-title">Selections</h3>
-          </div>
           <!-- /.card-header -->
           <div class="card-body">
             <div class="card">
